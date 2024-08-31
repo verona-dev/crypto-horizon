ddb28b05d64465a0ee48b4f4cc4c593263545f3b
verona-hub <68257343+verona-hub@users.noreply.github.com>
2024-08-31 18:29:00 +0200

    CR-1 Install Modules fixed nuxt server warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,19 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+   devtools: { enabled: true },
+
    css: [
       '@/assets/scss/main.scss',
       '@/assets/css/main.css',
    ],
 
-    devtools: { enabled: true },
 
-    modules: [['@nuxtjs/google-fonts', {
-        families: {
-            'Poppins': [400, 500, 600, 700]
-        }
-    }], 'nuxt-quasar-ui', 'nuxt-icon', '@nuxt/ui', '@pinia/nuxt', "@nuxt/icon"],
+   modules: [['@nuxtjs/google-fonts', {
+      families: {
+         'Poppins': [400, 500, 600, 700]
+      }
+   }], 'nuxt-quasar-ui', 'nuxt-icon', '@nuxt/ui', '@pinia/nuxt', "@nuxt/icon"],
 
    vite: {
       css: {
