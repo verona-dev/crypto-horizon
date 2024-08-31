dbb7ffcab590b126ddfacbe227c82fd32b0a8c98
verona-hub <68257343+verona-hub@users.noreply.github.com>
2024-08-31 18:14:38 +0200

    CR-1 Install Modules adjusted css and scss files
    installed nuxt-icon
    installed mdi-fonr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,26 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+   css: [
+      '@/assets/scss/main.scss',
+      '@/assets/css/main.css',
+   ],
+
     devtools: { enabled: true },
-    modules: [
-        ['@nuxtjs/google-fonts', {
-            families: {
-                'Poppins': [400, 500, 600, 700]
-            }
-        }],
-        'nuxt-quasar-ui',
-        'nuxt-icon',
-        '@nuxt/ui',
-        '@pinia/nuxt',
-    ]
+
+    modules: [['@nuxtjs/google-fonts', {
+        families: {
+            'Poppins': [400, 500, 600, 700]
+        }
+    }], 'nuxt-quasar-ui', 'nuxt-icon', '@nuxt/ui', '@pinia/nuxt', "@nuxt/icon"],
+
+   vite: {
+      css: {
+         preprocessorOptions: {
+            scss: {
+               additionalData: `@import "@/assets/scss/_colors.scss"; @import "@/assets/scss/_variables.scss";`,
+            },
+         },
+      },
+   },
 })
