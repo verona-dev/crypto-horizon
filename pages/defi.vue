88614c5ef4943d1436b52f1a66cf1e00eba973f9
verona-hub <68257343+verona-hub@users.noreply.github.com>
2024-09-08 19:15:51 +0200

    CR-2 Create a homepage created /market and /defi pages

---
+++ pages/defi.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='defi component'>
+        <h1>Coming Soon...</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang='scss'>
+    .defi {
+        h1 {
+            font-size: 3em;
+            animation: 3000ms fadeIn forwards;
+            opacity: 0;
+        }
+    }
+</style>
