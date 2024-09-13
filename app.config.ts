c42aae05dc71eea64236b1cb62d6a854257acf32
verona-hub <68257343+verona-hub@users.noreply.github.com>
2024-09-13 23:55:12 +0200

    CR-5 Populate /market page /market page added Card and stylings

--- app.config.ts
+++ app.config.ts
@@ -2,49 +2,65 @@ export default defineAppConfig({
    ui: {
       primary: 'customPrimary',
       gray: 'customGray',
-      // primary: 'lime',
-      // gray: 'cool',
-      success: 'green',
-      aqua: '02D0BD',
 
-      /*
+      card: {
+         base: '',
+         background: 'bg-grey dark:bg-gray-900',
+         divide: 'divide-y divide-gray-200 dark:divide-gray-800',
+         ring: 'ring-1 ring-gray-200 dark:ring-gray-800',
+         rounded: 'rounded-lg',
+         shadow: 'shadow',
+         body: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-5 sm:p-6',
+         },
+         header: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-5 sm:px-6',
+         },
+         footer: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-4 sm:px-6',
+         },
+      },
+
       table: {
-         default: {
-            thead: 'relative divide-yellow-200 dark:divide-yellow-800',
-            tbody: 'divide-y divide-yellow-200 dark:divide-yellow-800',
-            tr: {
-               base: '',
-               selected: 'bg-red-50 dark:bg-gray-800/50',
-               active: 'hover:bg-red-50 dark:hover:bg-gray-800/50 cursor-pointer',
-            },
-            th: {
-               base: 'text-left rtl:text-right',
-               padding: 'px-4 py-3.5',
-               color: 'text-red-900 dark:text-white',
-               font: 'font-semibold',
-               size: 'text-sm',
-            },
-            td: {
-               base: 'whitespace-nowrap',
-               padding: 'px-4 py-4',
-               color: 'text-red-500 dark:text-gray-400',
-               font: '',
-               size: 'text-sm',
-            },
+         thead: 'relative divide-yellow-200 dark:divide-yellow-800',
+         tbody: 'divide-y divide-yellow-200 dark:divide-yellow-800',
+         tr: {
+            base: '',
+            selected: 'bg-red-50 dark:bg-gray-800/50',
+            active: 'hover:bg-red-50 dark:hover:bg-gray-800/50 cursor-pointer',
+         },
+         th: {
+            base: 'text-left rtl:text-right',
+            padding: 'px-4 py-3.5',
+            color: 'text-red-900 dark:text-white',
+            font: 'font-semibold',
+            size: 'text-sm',
+         },
+         td: {
+            base: 'whitespace-nowrap',
+            padding: 'px-4 py-4',
+            color: 'text-red-500 dark:text-gray-400',
+            font: '',
+            size: 'text-sm',
+         },
 
-            sortAscIcon: 'octicon:sort-asc',
-            sortDescIcon: 'octicon:sort-desc',
-            sortButton: {
-               icon: 'octicon:arrow-switch'
-            },
-            loadingState: {
-               icon: 'octicon:sync'
-            },
-            emptyState: {
-               icon: 'octicon:database'
-            }
+         sortAscIcon: 'octicon:sort-asc',
+         sortDescIcon: 'octicon:sort-desc',
+         sortButton: {
+            icon: 'octicon:arrow-switch'
+         },
+         loadingState: {
+            icon: 'octicon:sync'
+         },
+         emptyState: {
+            icon: 'octicon:database'
          }
       }
-      */
    }
 })
--- pages/market.vue
+++ pages/market.vue
@@ -1,49 +1,71 @@
 <template>
     <div class='market component'>
-        Market
-        
-<!--
-        <Icon :name='tokenIcon' />
-        {{coins[0]?.id}}
-        -->
-        
-        <UTable
-            :rows='rows'
-            :columns='columns'
-            :loading='loading'
-        >
-            <template #row='props'>
-                <tr>
-                    <td>{{ props.row.rank }}</td>
-                    <td>{{ props.row.name }}</td>
-                    <td>
-                        <Icon :name='getIcon(props.row.symbol)' />
-                        {{ props.row.symbol }}
-                    </td>
-                    <td>{{ props.row.priceUsd }}</td>
-                    <td>{{ props.row.changePercent24Hr }}</td>
-                    <td>{{ props.row.marketCapUsd }}</td>
-                    <td>{{ props.row.volumeUsd24Hr }}</td>
-                </tr>
-                
-<!--                <div class='flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700'>-->
-<!--                    <UPagination v-model='page' :page-count='pageCount' :total='coins.length' />-->
-<!--                </div>-->
-            </template>
+        <UCard class='card'>
+            <div class='flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700'>
+                <UInput v-model='filter' placeholder='Filter...' />
+            </div>
             
-            <template>
+            <UTable
+                :rows='filteredRows'
+                :columns='columns'
+                :filter='filter'
+                :sort='sort'
+                :loading='loading'
+                class='table w-full'
+                :ui="{
+                    th: { base: 'sticky top-0' },
+                    wrapper: { base: '' },
+                }"
+            >
+                <template #loading-state>
+                    <div class="flex items-center justify-center h-32">
+                        <i class="loader --6" />
+                    </div>
+                </template>
+                
+                <template #row='props'>
+                    <tr>
+                        <td>{{ props.row.rank }}</td>
+                        <td>{{ props.row.name }}</td>
+                        <td>
+                            <Icon :name='getIcon(props.row.symbol)' />
+                            {{ props.row.symbol }}
+                        </td>
+                        <td>{{ props.row.priceUsd }}</td>
+                        <td>{{ props.row.changePercent24Hr }}</td>
+                        <td>{{ props.row.marketCapUsd }}</td>
+                        <td>{{ props.row.volumeUsd24Hr }}</td>
+                    </tr>
+                </template>
+            </UTable>
+            <div class='table-footer'>
+                <div class='results-info'>
+                    <span class="text-sm leading-5">
+                        Showing {{ pageFrom }} to {{ pageTo }} of {{ pageTotal }}
+                    </span>
+                </div>
+                
                 <UPagination
                     v-model='page'
                     :page-count='pageCount'
-                    :total='coins.length'
+                    :total='pageTotal'
+                    :ui="{
+                        wrapper: 'flex items-center gap-1',
+                        rounded: '!rounded-full min-w-[32px] justify-center',
+                        default: {
+                           activeButton: {
+                              variant: 'outline'
+                           }
+                     }}"
+                    class='pagination'
                 />
-            </template>
-        </UTable>
+            </div>
+        </UCard>
     </div>
 
 </template>
 
-<script setup>
+<script setup lang='ts'>
     import { ref } from 'vue';
     // CoinsStore
     import { storeToRefs } from 'pinia';
@@ -53,44 +75,63 @@
     // State
     const { loading, coins } = storeToRefs(CoinsStore);
     const page = ref(1);
-    const pageCount = 5;
+    const pageCount = ref(10);
+    const pageTotal = computed(() => coins.value?.length);
+    const pageFrom = computed(() => (page.value - 1) * pageCount.value + 1);
+    const pageTo = computed(() => Math.min(page.value * pageCount.value, pageTotal.value))
+    
     const rows = computed(() => {
-        return coins.value?.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+        return coins.value?.slice((page.value - 1) * pageCount.value, (page.value) * pageCount.value)
     });
-    const total = computed(() => coins.value?.length);
     
     const tokenIcon = computed(() => `cryptocurrency-color:${coins?.value[0]?.symbol.toLowerCase()}`);
     
     const columns = [
         {
             key: 'rank',
-            label: 'Rank'
+            label: 'Rank',
+            class: 'bg-red-500/50 dark:bg-red-400/50 animate-pulse'
         },
         {
             key: 'name',
-            label: 'Name'
+            label: 'Name',
         },
         {
             key: 'symbol',
-            label: 'Symbol'
+            label: 'Symbol',
         },
         {
             key: 'priceUsd',
-            label: 'Price (USD)'
+            label: 'Price (USD)',
         },
         {
             key: 'changePercent24Hr',
-            label: 'Change % (24Hr)'
+            label: 'Change % (24Hr)',
         },
         {
             key: 'marketCapUsd',
-            label: 'Market Cap (USD)'
+            label: 'Market Cap (USD)',
         },
         {
             key: 'volumeUsd24Hr',
-            label: 'Volume (24Hr)'
+            label: 'Volume (24Hr)',
         },
     ]
+    
+    // Filter
+    const filter = ref('');
+    const filteredRows = computed(() => {
+        return rows.value.filter(row => {
+            return row.name.toLowerCase().includes(filter.value.toLowerCase()) ||
+                row.symbol?.toLowerCase().includes(filter.value.toLowerCase());
+        })
+    });
+    
+    // Sort
+    const sort = ref({
+        column: '' as const,
+        direction: 'desc' as const
+    })
     
     // const rows = [
     //     {
@@ -138,15 +179,40 @@
 
 <style scoped lang='scss'>
     .market {
-        table {
-            thead {
-                color: red !important;
-                th {
-                    color: red !important;
+        margin: 0 auto;
+        
+        .card {
+            background-color: transparent;
+            
+            .table {
+                
+                table {
+                    //height: 600px;
+                    
+                    thead {
+                        color: red !important;
+                        th {
+                            color: red !important;
+                        }
+                    }
                 }
             }
-            tbody {}
             
+            .table-footer {
+                align-items: center;
+                border: 1px solid var(--color-pink_french);
+                display: flex;
+                justify-content: space-between;
+                padding: 20px;
+                width: 100%;
+                
+                .results-info {
+                    color: var(--color-pink_french);
+                }
+                
+                .pagination {
+                }
+            }
         }
     }
 </style>
