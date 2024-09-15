1537260daba4e5fd2c92357482e86f188c600b60
verona-hub <68257343+verona-hub@users.noreply.github.com>
2024-09-15 17:45:14 +0200

    CR-11 Implement theme mode created ThemeSelector component

---
+++ components/ThemeSelector.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class='theme-selector'>
+        <select v-model='$colorMode.preference'>
+            <option value='light'>Light</option>
+            <option value='dark'>Dark</option>
+        </select>
+    </section>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const colorMode = useColorMode()
+    
+    const enabled = ref(false);
+    
+    watch(enabled, () => {
+        if (enabled.value) {
+            colorMode.preference = 'dark';
+        } else {
+            colorMode.preference = 'light';
+        }
+        checkTheme();
+    });
+    
+    const checkTheme = () => {
+        if (colorMode.preference === 'dark') {
+            enabled.value = true;
+        } else {
+            enabled.value = false;
+        }
+    };
+    
+    onMounted(() => {
+        console.log(colorMode.preference);
+    });
+</script>
+
+<style scoped lang='scss'>
+    .theme-selector {
+    }
+</style>
--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -8,13 +8,7 @@
             <ul>
                 <li>
                     <div class='ml-10'>
-                        <span class='mr-5'>Color mode</span>
-                        <select v-model='$colorMode.preference'>
-                            <option value='system'>System</option>
-                            <option value='light'>Light</option>
-                            <option value='dark'>Dark</option>
-                            <option value='sepia'>Sepia</option>
-                        </select>
+                        <ThemeSelector />
                     </div>
                 </li>
                 <li>
@@ -56,3 +50,6 @@
         }
     }
 </style>
+<script setup>
+    import ThemeSelector from '~/components/ThemeSelector.vue';
+</script>
