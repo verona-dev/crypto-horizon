74cb54f2ed8dcc5f1906c583b74c8f4fef80d09d
verona-hub <veronadev@tuta.io>
2024-10-21 01:27:18 +0200

    CR-20 [MARKETS] add single coin page add single coin page add formatUtils and styleUtils

--- stores/CoinsStore.js
+++ stores/CoinsStore.js
@@ -5,6 +5,7 @@ export const useCoinsStore = defineStore('CoinsStore', {
     state: () => ({
         loading: false,
         coins: [],
+        activeCoin: null,
     }),
     
     actions: {
@@ -14,6 +15,22 @@ export const useCoinsStore = defineStore('CoinsStore', {
             try {
                 const { data }  = await useCoincapApiFetch('/assets');
                 this.coins = data;
+            }
+            catch(error) {
+                console.log(error);
+            }
+            finally {
+                this.loading = false;
+            }
+        },
+        
+        async setActiveCoin(symbol) {
+            this.loading = true;
+            
+            try {
+                this.activeCoin = symbol;
+                // const { data }  = await useCoincapApiFetch(`/assets/${symbol}`);
+                // this.activeCoin = data;
             }
             catch(error) {
                 console.log(error);
