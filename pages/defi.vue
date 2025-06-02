1a3c00217d97a5e0180b273247e50388387edf1f
verona-dev <veronadev@tuta.io>
2025-06-02 20:07:29 +0200

    CR-26 upgrade project tailwind fixes

--- pages/defi.vue
+++ pages/defi.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='defi page'>
-        <h1>Coming Soon...</h1>
+        <h1 class='text-red-500 text-7xl font-bold'>Coming Soon...</h1>
     </div>
 </template>
 
@@ -11,9 +11,9 @@
 <style scoped lang='scss'>
     .defi {
         h1 {
-            font-size: 3em;
-            animation: 3000ms fadeIn forwards;
-            opacity: 0;
+            //font-size: 3em;
+            //animation: 3000ms fadeIn forwards;
+            //opacity: 0;
         }
     }
 </style>
