0f073fda01c521c4c49bc7f89bc11ecf304e28ae
verona-dev <veronadev@tuta.io>
2025-06-04 19:28:27 +0200

    CR-23 [DESIGN] Add main navigation code cleanup

--- pages/defi.vue
+++ pages/defi.vue
@@ -2,7 +2,7 @@
     <div class='defi page'>
         <h1 class='text-pink-500'>Coming Soon...</h1>
         
-        <MazBtn class='mt-8'>Click me!</MazBtn>
+<!--        <MazBtn class='mt-8'>Click me!</MazBtn>-->
     </div>
 </template>
 
