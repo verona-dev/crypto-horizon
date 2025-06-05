83d66ca02f230aa20c6e56d829b80d03a6f9593d
verona-dev <veronadev@tuta.io>
2025-06-05 13:51:51 +0200

    CR-14 [Markets] adjust table remove useCoincapApi from apiFetch

--- composables/apiFetch.js
+++ composables/apiFetch.js
@@ -1,18 +1,4 @@
 const { coingecko_api_key } = useRuntimeConfig().public;
-const { coincap_api_key } = useRuntimeConfig().public;
-
-export const useCoincapApi = (route) => {
-    return $fetch(route, {
-        baseURL: 'https://api.coincap.io/v3',
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + coincap_api_key
-        },
-        redirect: 'follow',
-    });
-}
 
 export const useCoinGeckoApi = (route) => {
     return $fetch(route, {
