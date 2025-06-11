60782e22515c6663cef8d5826322b378de81252d
verona-dev <veronadev@tuta.io>
2025-06-11 15:03:55 +0200

    CR-20 [MARKETS] add single coin page add livecoinwatch_api_key to runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,6 +66,7 @@ export default defineNuxtConfig({
       public: {
          coingecko_api_key: process.env.COINGECKO_API_KEY,
          coincap_api_key: process.env.COINCAP_API_KEY,
+         livecoinwatch_api_key: process.env.LIVECOINWATCH_API_KEY,
       },
    },
 
