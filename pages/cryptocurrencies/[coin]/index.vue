c57314c838d83bb8f9c8fc313560dbb976621d1f
verona-dev <veronadev@tuta.io>
2025-06-11 14:49:47 +0200

    CR-20 [MARKETS] add single coin page single coin page ready for data fetch

--- pages/cryptocurrencies/[coin]/index.vue
+++ pages/cryptocurrencies/[coin]/index.vue
@@ -1,56 +1,56 @@
 <template>
-    <div class='single-coin'>
+    <div class='single-coin custom-flex'>
         This is single coin page
-<!--
-        <UCard
-            v-if='coin'
-            class='coin-card'
-        >
-            <div class='coin-header'>
-                <div class='name'>
-                    <Icon
-                        v-if='coin.icon'
-                        :name='coin.icon'
-                        size='25'
-                    />
-                    <h1>{{ coin.name }}</h1>
-                </div>
-                
-                <div class='price'>
-                    <h1>{{ coin.price }}</h1>
-                </div>
-            </div>
-            
-            <div class='coin-stats'>
-                <UCard class='card'>
-                    <h3>Market Cap</h3>
-                    {{ coin?.marketCap }}
+        <!--
+                <UCard
+                    v-if='coin'
+                    class='coin-card'
+                >
+                    <div class='coin-header'>
+                        <div class='name'>
+                            <Icon
+                                v-if='coin.icon'
+                                :name='coin.icon'
+                                size='25'
+                            />
+                            <h1>{{ coin.name }}</h1>
+                        </div>
+                        
+                        <div class='price'>
+                            <h1>{{ coin.price }}</h1>
+                        </div>
+                    </div>
+                    
+                    <div class='coin-stats'>
+                        <UCard class='card'>
+                            <h3>Market Cap</h3>
+                            {{ coin?.marketCap }}
+                        </UCard>
+                        
+                        <UCard class='card'>
+                            <h3>Volume</h3>
+                            {{ coin?.volume }}
+                        </UCard>
+                        
+                        <UCard v-if='coin.supply' class='card'>
+                            <h3>Total Supply</h3>
+                            {{ coin?.supply }}
+                        </UCard>
+                        
+                        <UCard v-if='coin.explorer' class='card'>
+                            <h3>Explorer</h3>
+                            <NuxtLink
+                                :href='coin?.explorer'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                class='text-indigo-400 hover:text-indigo-500'
+                            >
+                                {{ coin?.explorer }}
+                            </NuxtLink>
+                        </UCard>
+                    </div>
                 </UCard>
-                
-                <UCard class='card'>
-                    <h3>Volume</h3>
-                    {{ coin?.volume }}
-                </UCard>
-                
-                <UCard v-if='coin.supply' class='card'>
-                    <h3>Total Supply</h3>
-                    {{ coin?.supply }}
-                </UCard>
-                
-                <UCard v-if='coin.explorer' class='card'>
-                    <h3>Explorer</h3>
-                    <NuxtLink
-                        :href='coin?.explorer'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        class='text-indigo-400 hover:text-indigo-500'
-                    >
-                        {{ coin?.explorer }}
-                    </NuxtLink>
-                </UCard>
-            </div>
-        </UCard>
-        -->
+                -->
     </div>
 </template>
 
@@ -61,15 +61,21 @@
     // CryptocurrenciesStore
     import {storeToRefs} from 'pinia';
     import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
-    // const CoinsStore = useCryptocurrenciesStore();
+    const CryptocurrenciesStore = useCryptocurrenciesStore();
     
     // State
-    // const { coin } = storeToRefs(CryptocurrenciesStore);
+    const { coin } = storeToRefs(CryptocurrenciesStore);
+    // Methods
+    const { setActiveCoin } = CryptocurrenciesStore;
+    
+    const fetchCoin = async() => {
+        console.log(route.params.coin)
+        await setActiveCoin(route.params.coin);
+    }
     
     
-    onMounted(async() => {
-        // await fetchCoincapCoin(route.params.coin)
-        // await fetchCoingeckoHistoricalChartData(route.params.coin, 1)
+    onMounted(() => {
+        fetchCoin();
     });
 </script>
 
