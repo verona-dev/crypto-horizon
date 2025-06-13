b0651ff44496bddcbb317bb5754d8bff2f14b8a8
verona-dev <veronadev@tuta.io>
2025-06-13 12:11:50 +0200

    CR-20 [MARKETS] add single coin page cc table change header labels

--- components/markets/CryptocurrenciesTable.vue
+++ components/markets/CryptocurrenciesTable.vue
@@ -5,7 +5,7 @@
           { label: "Name", key: "name", sortable: false, classes: "", },
           { label: "Price", key: "price", align: "center", sortable: false, classes: "",},
           { label: "Market Cap", key: "marketCap", align: "center", sortable: false, classes: "", },
-          { label: "Volume (24h)", key: "volume", align: "center", sortable: false, classes: "", },
+          { label: "24h Volume", key: "volume", align: "center", sortable: false, classes: "", },
           { label: "Circ. Supply", key: "c_supply", align: "center", sortable: false, classes: "", },
           { label: "24h %", key: "changePercent24Hr", align: "center", sortable: false, classes: "", },
         ]'
