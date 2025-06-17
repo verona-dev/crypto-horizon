dd72f55386f0868c6ac8166d96bdb4395fa96db9
verona-dev <veronadev@tuta.io>
2025-06-17 15:05:49 +0200

    CR-20 [MARKETS] add single coin page create Stacked Bar chart for coin supply

---
+++ components/markets/cryptocurrencies/coin/CoinSupplyStackedChart.vue
@@ -0,0 +1,58 @@
+
+<template>
+    <div class='my-20'>
+        <Bar
+            :data='chartData'
+            :options='chartOptions'
+        />
+    </div>
+</template>
+
+<script lang='ts' setup>
+    import { Bar, Doughnut } from 'vue-chartjs';
+    
+    const props = defineProps({
+        totalSupply: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+        maxSupply: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+    });
+    
+    const { totalSupply, maxSupply } = toRefs(props);
+    const remainingSupply = computed(() => maxSupply.value - totalSupply.value);
+    
+    const chartData = ref({
+        labels: ['Max Supply', 'Total Supply', 'Remaining Supply', ],
+        datasets: [
+            {
+                label: 'Supply',
+                backgroundColor: ['#ccb7a8',  '#e18037', '#41B883'],
+                data: [maxSupply.value, totalSupply.value, remainingSupply.value]
+            }
+        ]
+    });
+    
+    const chartOptions = ref({
+        indexAxis: 'y',
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: true,
+            }
+        }
+    });
+</script>
