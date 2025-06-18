0bb2777a43ff69e4a5dc5a5889f3ac5876c0ce0c
verona-dev <veronadev@tuta.io>
2025-06-18 18:11:56 +0200

    CR-20 [MARKETS] add single coin page create CoinSupplyProgressBar component

---
+++ components/markets/cryptocurrencies/coin/CoinSupplyProgressBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <MazAnimatedElement
+        direction='up'
+        :duration='2000'
+        class='my-10 flex items-center justify-center'
+    >
+        <MazCircularProgressBar
+            :percentage='remainingSupplyPercentage'
+            :duration='3000'
+            suffix='%'
+            auto-color
+        />
+        
+        <div class='flex flex-col'>
+            <div class='mb-6'>
+                <h4 class='ml-6'>Max Supply</h4>
+                <p class='ml-6 mt-2'>{{ coin.maxSupplyChart?.toLocaleString() }} {{ coin.symbol }}</p>
+            </div>
+            <div>
+                <h4 class='ml-6'>Total Supply</h4>
+                <p class='ml-6 mt-2'>{{ coin.totalSupplyChart?.toLocaleString() }} {{ coin.symbol }}</p>
+            </div>
+        </div>
+        
+        
+    </MazAnimatedElement>
+    
+    
+</template>
+
+<script setup>
+    const props = defineProps({
+        coin: {
+            type: Object,
+            default: (() => {}),
+            required: true,
+        }
+    });
+    
+    const { coin } = toRefs(props);
+    
+    const remainingSupplyPercentage = computed(() => (coin.value.totalSupplyChart / coin.value.maxSupplyChart) * 100);
+</script>
