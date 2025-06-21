3b846eb77dfc97cb9a70b97f2c59eea19eefbc2b
verona-dev <veronadev@tuta.io>
2025-06-21 17:57:15 +0200

    CR-20 [MARKETS] add single coin page coin overview add hover card to pairs

--- components/markets/cryptocurrencies/coin/CoinOverview.vue
+++ components/markets/cryptocurrencies/coin/CoinOverview.vue
@@ -2,14 +2,19 @@
     <div v-if='coin' class='coin-overview w-96'>
         <Table class='text-lg'>
             <TableBody>
+                <!--  Created  -->
                 <TableRow>
                     <TableCell>Created</TableCell>
                     <TableCell>{{ coin.age }} days ago</TableCell>
                 </TableRow>
+                
+                <!--  ATH  -->
                 <TableRow>
                     <TableCell>All-time high</TableCell>
                     <TableCell>{{ coin.allTimeHighUSDFormatted }}</TableCell>
                 </TableRow>
+                
+                <!--  Markets  -->
                 <TableRow>
                     <TableCell class='flex items-center'>
                         Markets
@@ -23,16 +28,35 @@
                                 />
                             </HoverCardTrigger>
                             <HoverCardContent class='hover-card-content'>
-                                <span class='text-sm'>The number of markets coin is present at.</span>
+                                <span class='text-sm'>The number of markets the coin is present at.</span>
                             </HoverCardContent>
                         </HoverCard>
                     </TableCell>
                     <TableCell>{{ coin.markets }}</TableCell>
                 </TableRow>
+                
+                <!--  Pairs  -->
                 <TableRow>
-                    <TableCell>Pairs</TableCell>
+                    <TableCell class='flex items-center'>
+                        Pairs
+                        
+                        <HoverCard openDelay='200'>
+                            <HoverCardTrigger class='info-icon'>
+                                <NuxtIcon
+                                    name='radix-icons:info-circled'
+                                    size='25'
+                                    class='flex ml-2 mt-1'
+                                />
+                            </HoverCardTrigger>
+                            <HoverCardContent class='hover-card-content'>
+                                <span class='text-sm'>The number of unique markets the coin is present at.</span>
+                            </HoverCardContent>
+                        </HoverCard>
+                    </TableCell>
                     <TableCell>{{ coin.pairs }}</TableCell>
                 </TableRow>
+                
+                <!--  Exchanges  -->
                 <TableRow>
                     <TableCell>Exchanges</TableCell>
                     <TableCell>{{ coin.exchanges }}</TableCell>
@@ -60,3 +84,10 @@
     });
     const { coin } = toRefs(props);
 </script>
+
+<style scoped>
+    td {
+        //display: flex;
+        //align-items: center;
+    }
+</style>
