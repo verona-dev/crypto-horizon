daa8758dc43659bb84fbec992096a05b84710f9a
verona-dev <veronadev@tuta.io>
2025-06-26 18:14:47 +0200

    CR-20 [MARKETS] add single coin page add CoinDeltaItem to CoinDelta

--- components/markets/cryptocurrencies/coin/CoinDelta.vue
+++ components/markets/cryptocurrencies/coin/CoinDelta.vue
@@ -2,41 +2,19 @@
     <CardContent class='px-20 my-10 flex justify-around'>
         This is coin delta
         
+        <!-- 1h  -->
+        <CoinDeltaItem
+            :delta='delta1h'
+            title='1h'
+            description='Rate of change in the last hour.'
+        />
+        
         <!-- 24h  -->
-        <Card class='text-card-foreground gap-1 py-5 px-0'>
-            
-            <CardTitle class='flex flex-col items-center'>
-                <NuxtIcon
-                    name='radix-icons:bar-chart'
-                    size='40'
-                    :class='getTrendColor(delta24h)'
-                />
-            </CardTitle>
-            
-            <CardContent class='flex justify-center'>
-                <p>24h</p>
-                
-                <HoverCard
-                    openDelay='200'
-                    class='flex'
-                >
-                    <HoverCardTrigger class='info-icon'>
-                        <NuxtIcon
-                            name='radix-icons:info-circled'
-                            size='25'
-                            class='flex ml-2'
-                        />
-                    </HoverCardTrigger>
-                    <HoverCardContent class='hover-card-content'>
-                        <span class='text-sm'>Rate of change in the last 24 hours.</span>
-                    </HoverCardContent>
-                </HoverCard>
-            </CardContent>
-            
-            <CardFooter :class='getTrendColor(delta24h)'>
-                <h6>{{ delta24h.toFixed(2) }}&#37;</h6>
-            </CardFooter>
-        </Card>
+        <CoinDeltaItem
+            :delta='delta24h'
+            title='24h'
+            description='Rate of change in the last 24 hours.'
+        />
     </CardContent>
     
     <Separator class='my-4' />
@@ -45,7 +23,7 @@
 <script setup>
     import { getDeltaPercentage } from '~/utils/formatUtils.js';
     import { getTrendColor } from '~/utils/styleUtils.js';
-    import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card/index.js';
+    import CoinDeltaItem from '~/components/markets/cryptocurrencies/coin/CoinDeltaItem.vue';
     
     const props = defineProps({
         coin: {
@@ -56,7 +34,6 @@
     });
     
     const { coin } = toRefs(props);
-    const delta24h = computed(() => {
-        return getDeltaPercentage(coin.value.delta.day, coin.value.rate);
-    });
+    const delta1h = computed(() => getDeltaPercentage(coin.value?.delta.hour, coin.value?.rate))
+    const delta24h = computed(() => getDeltaPercentage(coin.value?.delta.day, coin.value?.rate));
 </script>
