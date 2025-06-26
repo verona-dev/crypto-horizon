eabae4fc5edac552206c699dd29f249ded315ff0
verona-dev <veronadev@tuta.io>
2025-06-26 18:14:28 +0200

    CR-20 [MARKETS] add single coin page create CoinDeltaItem

---
+++ components/markets/cryptocurrencies/coin/CoinDeltaItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <HoverCard
+        openDelay='200'
+        class='flex'
+    >
+        <HoverCardTrigger class='info-icon'>
+            <Card
+                class='text-card-foreground gap-1 py-5 px-0'
+                :class='getTrendColor(delta)'
+            >
+                <CardTitle class='flex flex-col items-center'>
+                    <p>1h</p>
+                </CardTitle>
+                
+                <CardContent class='flex justify-center'>
+                    <NuxtIcon
+                        name='radix-icons:bar-chart'
+                        size='40'
+                    />
+                </CardContent>
+                
+                <CardFooter>
+                    <h6>{{ delta.toFixed(2) }}&#37;</h6>
+                </CardFooter>
+            </Card>
+        </HoverCardTrigger>
+        
+        <HoverCardContent class='hover-card-content'>
+            <span class='text-sm'>{{ description }}</span>
+        </HoverCardContent>
+    </HoverCard>
+</template>
+
+<script setup>
+    const props = defineProps({
+        delta: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        description: {
+            type: String,
+            required: true,
+            default: '-',
+        },
+    });
+    
+    const { delta, description } = toRefs(props);
+</script>
