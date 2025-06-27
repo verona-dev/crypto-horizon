e446326b181a4b2d9cfa0d38477155682aff23b2
verona-dev <veronadev@tuta.io>
2025-06-27 17:07:05 +0200

    CR-20 [MARKETS] add single coin page single coin stacked bars add conditionals

--- components/markets/cryptocurrencies/coin/CoinSupplyStackedBars.vue
+++ components/markets/cryptocurrencies/coin/CoinSupplyStackedBars.vue
@@ -23,13 +23,35 @@
     const remainingSupply = computed(() => coin.value.maxSupply - coin.value.totalSupply);
     
     const chartContent = computed(() => {
+        const labels: string[] = [];
+        const data: number[] = [];
+        const backgroundColor: string[] = [];
+        
         // If coin has max supply
         if (coin.value.maxSupply) {
-            return {
-                labels: ['Max Supply', 'Total Supply', 'Circulating Supply', 'Remaining Supply'],
-                data: [coin.value.maxSupply, coin.value.totalSupply, coin.value.circulatingSupply, remainingSupply.value],
-                backgroundColor: ['#00b1f5',  '#fef0ca', '#e787c0', '#41B883'],
-            };
+            labels.push('Max Supply');
+            data.push(coin.value.maxSupply);
+            backgroundColor.push('#00b1f5');
+            
+            if(coin.value.totalSupply) {
+                labels.push('Total Supply');
+                data.push(coin.value.totalSupply);
+                backgroundColor.push('#fef0ca');
+            }
+            
+            if(coin.value.circulatingSupply) {
+                labels.push('Circulating Supply');
+                data.push(coin.value.circulatingSupply);
+                backgroundColor.push('#e787c0');
+            }
+            
+            if(remainingSupply.value) {
+                labels.push('Remaining Supply');
+                data.push(remainingSupply.value);
+                backgroundColor.push('#41B883');
+            }
+            
+            return { labels, data, backgroundColor };
         } else {
             // If coin does not have max supply
             return {
