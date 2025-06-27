5cad02073b18343396d93fa6e825d256ac1a87b6
verona-dev <veronadev@tuta.io>
2025-06-27 16:13:38 +0200

    CR-20 [MARKETS] add single coin page create coingecko api route

--- composables/apiCoingecko.js
+++ composables/apiCoingecko.js
@@ -1,10 +1,22 @@
-import { useCoinGeckoApi } from '~/composables/apiFetch';
 
+export async function useFetchCoingecko()  {
+    try {
+        return await $fetch('/api/coingecko/coins/markets', {
+            query: { vs_currency: 'usd' }
+        });
+    } catch(error) {
+        console.error('Error fetching CoinLore data', error);
+    }
+}
+
+
+
+/*
 export const useCoingeckoHistoricalChartData = (coin, days) => {
     const params = new URLSearchParams();
     params.append('vs_currency', 'usd');
     params.append('interval', 'daily');
     days && params.append('days', days);
     const chartData = `coins/${coin}/market_chart?${params}`;
-    return useCoinGeckoApi(chartData);
 }
+*/
---
+++ server/api/coingecko/[...route].js
@@ -0,0 +1,36 @@
+import { createError, defineEventHandler, getQuery } from 'h3';
+
+export default defineEventHandler(async (event) => {
+    const { coingeckoApiKey } = useRuntimeConfig().public;
+    
+    const route = event.context.params?.route || '';
+    const queryEvent = getQuery(event);
+    const query = new URLSearchParams(queryEvent).toString();
+    const apiUrl = `https://api.coingecko.com/api/v3/${route}?${query}`;
+    
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-cg-demo-api-key': coingeckoApiKey,
+            },
+            redirect: 'follow',
+        });
+        
+        if (!response.ok) {
+            throw createError({
+                statusCode: response.status,
+                statusMessage: response.statusText,
+            });
+        }
+        
+        return await response.json();
+    } catch(error) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Failed to fetch CoinGecko data',
+        });
+    }
+});
