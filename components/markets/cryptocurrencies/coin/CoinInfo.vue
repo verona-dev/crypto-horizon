833e9ae7cce49a35b00628ad343a9bf28a41dbbf
verona-dev <veronadev@tuta.io>
2025-07-01 16:33:17 +0200

    CR-20 [MARKETS] add single coin page coin cleanup

--- components/markets/cryptocurrencies/coin/CoinInfo.vue
+++ components/markets/cryptocurrencies/coin/CoinInfo.vue
@@ -1,30 +1,36 @@
 <template>
     <CardContent v-if='coingecko' class='px-20 my-10 flex flex-col'>
         <!--  Description  -->
-        <div v-if='coingecko.description' class='bg-accent-foreground'>
-            <Accordion type='single' collapsible class='py-2 px-8'>
+        <div v-if='coingecko.description' class='bg-accent-foreground rounded-t-md'>
+            <Accordion type='single' collapsible class='py-3 px-6'>
                 <AccordionItem value='item-1' class=''>
                     <AccordionTrigger class=''>
                         <p>What is {{ coingecko.name }}</p>
                     </AccordionTrigger>
-                    <AccordionContent v-html='coingecko.description.en'></AccordionContent>
+                    
+                    <AccordionContent class='py-6'>
+                        {{ coingecko.description.en }}
+                    </AccordionContent>
                 </AccordionItem>
             </Accordion>
         </div>
         
+        <Separator class='custom-separator'/>
+        
         <!--  Categories  -->
-        <div v-if='coingecko.categories' class='bg-accent-foreground'>
-            <Accordion type='single' collapsible class='py-2 px-8'>
+        <div v-if='coingecko.categories' class='bg-accent-foreground rounded-b-md'>
+            <Accordion type='single' collapsible class='py-3 px-6'>
                 <AccordionItem value='item-1'>
                     <AccordionTrigger class=''>
-                        <p class=''>Categories</p>
+                        <p>Categories</p>
                     </AccordionTrigger>
-                    <AccordionContent>
+                    
+                    <AccordionContent class='py-6'>
                         <MazBadge
                             v-for='category in coingecko.categories'
                             :key='category'
                             color='gray'
-                            class='m-1.5'
+                            class='m-2'
                             outline
                         >
                             {{ category }}
@@ -51,3 +57,9 @@
     const { coin } = toRefs(props);
     const coingecko = toRef(coin.value?.coingecko);
 </script>
+
+<style scoped>
+    .custom-separator {
+        background: var(--card-border);
+    }
+</style>
