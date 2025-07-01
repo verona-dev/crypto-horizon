ee93a4471651560d251e2112dff1a004db4ccf06
verona-dev <veronadev@tuta.io>
2025-07-01 15:15:01 +0200

    CR-20 [MARKETS] add single coin page coin info add categories

--- components/markets/cryptocurrencies/coin/CoinInfo.vue
+++ components/markets/cryptocurrencies/coin/CoinInfo.vue
@@ -1,15 +1,39 @@
 <template>
-    <div
-        v-if='coingecko.description'
-        class='w-[500px] self-center'
-    >
-        <Accordion type='single' collapsible>
-            <AccordionItem value='item-1'>
-                <AccordionTrigger>What is {{ coingecko.name }}</AccordionTrigger>
-                <AccordionContent>{{ coingecko.description.en }}</AccordionContent>
-            </AccordionItem>
-        </Accordion>
-    </div>
+    <CardContent v-if='coingecko' class='px-20 my-10 flex flex-col'>
+        <!--  Description  -->
+        <div v-if='coingecko.description' class=''>
+            <Accordion type='single' collapsible class=''>
+                <AccordionItem value='item-1'>
+                    <AccordionTrigger class='w-full'>
+                        <p>What is {{ coingecko.name }}</p>
+                    </AccordionTrigger>
+                    <AccordionContent v-html='coingecko.description.en'></AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+        
+        <!--  Categories  -->
+        <div v-if='coingecko.categories' class='categories'>
+            <Accordion type='single' collapsible class=''>
+                <AccordionItem value='item-1'>
+                    <AccordionTrigger class='w-full'>
+                        <p class=''>Categories</p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        <MazBadge
+                            v-for='category in coingecko.categories'
+                            :key='category'
+                            color='gray'
+                            class='m-1.5'
+                            outline
+                        >
+                            {{ category }}
+                        </MazBadge>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    </CardContent>
     
     <Separator class='my-4' />
 </template>
