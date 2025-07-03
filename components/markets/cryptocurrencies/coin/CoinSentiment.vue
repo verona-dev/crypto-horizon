cad4835a8e6bf24b51858a251828a944d468f3a5
verona-dev <veronadev@tuta.io>
2025-07-03 19:01:18 +0200

    CR-20 [MARKETS] add single coin page coin sentiment final adjustments

--- components/markets/cryptocurrencies/coin/CoinSentiment.vue
+++ components/markets/cryptocurrencies/coin/CoinSentiment.vue
@@ -1,16 +1,38 @@
 <template>
     <section class='my-20'>
-        <h6>Community sentiment</h6>
+        <div class='flex items-center'>
+            <NuxtIcon
+                name='iconoir:community'
+                size='45'
+                class='mr-4'
+            />
+            <h6>Community sentiment</h6>
+        </div>
         
         <div class='flex items-center w-96 mt-5'>
-            <p>{{ Math.floor(sentimentUp) }}&#37;</p>
+            <div class='flex items-center text-chart-2'>
+                <NuxtIcon
+                    name='iconoir:stat-up'
+                    size='35'
+                    class='mr-2'
+                />
+                <p>{{ Math.floor(sentimentUp) }}&#37;</p>
+            </div>
+            
             <Progress
                 v-model='sentiment'
                 class='mx-4'
             />
-            <p>{{ Math.ceil(sentimentDown) }}&#37;</p>
+            
+            <div class='flex items-center text-chart-5'>
+                <p>{{ Math.ceil(sentimentDown) }}&#37;</p>
+                <NuxtIcon
+                    name='iconoir:stat-down'
+                    size='35'
+                    class='ml-2'
+                />
+            </div>
         </div>
-        
     </section>
 </template>
 
