9c21077cc8a72b318ae22edc9bfe2045c7f1078a
verona-dev <veronadev@tuta.io>
2025-07-03 18:15:46 +0200

    CR-20 [MARKETS] add single coin page refactor shadcn progress component

--- components/ui/progress/Progress.vue
+++ components/ui/progress/Progress.vue
@@ -24,14 +24,14 @@ const delegatedProps = reactiveOmit(props, 'class')
     v-bind="delegatedProps"
     :class="
       cn(
-        'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
+        'bg-chart-5 relative h-2 w-full overflow-hidden rounded-full',
         props.class,
       )
     "
   >
     <ProgressIndicator
       data-slot="progress-indicator"
-      class="bg-primary h-full w-full flex-1 transition-all"
+      class="bg-chart-2 h-full w-full flex-1 transition-all"
       :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
     />
   </ProgressRoot>
