60b770693fe5de3b3c55e95c7b3440708a826cc1
verona-dev <veronadev@tuta.io>
2025-07-04 20:05:44 +0200

    CR-20 [MARKETS] add single coin page create CoinChart component

---
+++ components/markets/cryptocurrencies/coin/CoinChart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class='coin-chart'>
+        This is coin chart
+        {{ coin.coingecko.name}}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    coin: {
+        type: Object,
+        required: true
+    }
+});
+const { coin } = toRefs(props);
+</script>
