c7ce2b99d5dc8bdd3e9fae76fda5067a6853f4e0
verona-dev <veronadev@tuta.io>
2025-07-04 14:38:23 +0200

    CR-20 [MARKETS] add single coin page coin converter switch data from livecoinwatch api to coingecko

--- components/markets/cryptocurrencies/coin/CoinConverter.vue
+++ components/markets/cryptocurrencies/coin/CoinConverter.vue
@@ -43,7 +43,7 @@
     
     const { coin } = toRefs(props);
     const coinPrice = computed(() => {
-        const rate = coin.value?.livecoinwatch?.rate;
+        const rate = coin.value?.coingecko.market_data.current_price.usd;
         if (rate == null) return 0;
         return Math.round(rate * 100) / 100;
     });
