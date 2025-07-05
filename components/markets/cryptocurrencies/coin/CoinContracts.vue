176969acb73054abd74ff1931971be7566ca6c86
verona-dev <veronadev@tuta.io>
2025-07-05 13:24:48 +0200

    CR-20 [MARKETS] add single coin page create CoinContracts

---
+++ components/markets/cryptocurrencies/coin/CoinContracts.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        v-if='platforms'
+        class='coin-contracts'
+    >
+        <h6>Contracts</h6>
+        
+        <div class='flex items-center'>
+            <!--  First Contract  -->
+            <MazBadge
+                class='w-full'
+                color='info'
+            >
+                <div class='py-1.5 pr-4 flex items-center'>
+                    <p>{{ platforms[0].chain }}</p>
+                </div>
+            </MazBadge>
+            
+            <div class='vertical-separator'></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        coin: Object,
+        required: true,
+    });
+    
+    const { coin } = toRefs(props);
+    const platforms = Object.entries(coin.value.platforms).map(([key, value]) => ({
+        'chain': key,
+        'contract': value
+    }));
+</script>
+
+<style scoped>
+    .coin-contracts {}
+</style>
