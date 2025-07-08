f2fc362179dbbd4994c727c5cd04d52e29804421
verona-dev <veronadev@tuta.io>
2025-07-08 18:34:31 +0200

    CR-20 [MARKETS] add single coin page add sonner to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
     <NuxtLoadingIndicator />
+    <Toaster />
     
     <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script>
+    import { Toaster } from '@/components/ui/sonner';
+    import 'vue-sonner/style.css'
+</script>
