5deea62e661fce5120b0917df0a4597e1ba700f5
verona-dev <veronadev@tuta.io>
2025-07-09 18:27:45 +0200

    CR-20 [MARKETS] add single coin page coin public notice final adjustment

---
+++ components/markets/cryptocurrencies/coin/CoinPublicNotice.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--  Public Notice  -->
+    <Alert
+        v-if='publicNotice'
+        class='my-10 py-0 w-3/4 flex items-center border border-warning'
+    >
+        <div class='mr-6'>
+            <NuxtIcon
+                name='iconoir:megaphone'
+                size='50'
+            />
+        </div>
+        
+        <div class='notice-content border-l border-warning p-6'>
+            <AlertTitle>Public notice:</AlertTitle>
+            <AlertDescription class='inline' v-html='publicNoticeFormatted' />
+        </div>
+    </Alert>
+</template>
+
+<script setup>
+    import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert/index.js';
+    
+    const props = defineProps({
+        publicNotice: {
+            type: String,
+            required: true,
+        }
+    });
+    
+    const { publicNotice } = toRefs(props);
+    
+    const publicNoticeFormatted = computed(() => {
+        let text = publicNotice.value;
+        if (text.endsWith('.')) {
+            text = text.slice(0, -1);
+        }
+        return text.replace(/<a /g, '<a class="underline" ');
+    });
+</script>
