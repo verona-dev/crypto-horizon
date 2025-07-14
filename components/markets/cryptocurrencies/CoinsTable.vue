e64c162bc6dd675837c67d5e64b8b9022a6622ae
verona-dev <veronadev@tuta.io>
2025-07-14 16:14:44 +0200

    CR-20 [MARKETS] add single coin page coins table fix error: Uncaught (in promise) DOMException: Node.insertBefore: Child to insert before is not a child of this node

--- components/markets/cryptocurrencies/CoinsTable.vue
+++ components/markets/cryptocurrencies/CoinsTable.vue
@@ -1,5 +1,6 @@
 <template>
     <MazTable
+        v-if='!loading'
         :headers='[
           { label: "#", key: "rank", align: "center", sortable: false, classes: "w-20", },
           { label: "Name", key: "name", sortable: false, classes: "w-92", },
@@ -11,7 +12,6 @@
         ]'
         :rows='coins'
         class='my-20 px-6'
-        :loading='loading'
         color='secondary'
         hoverable
         background-even
