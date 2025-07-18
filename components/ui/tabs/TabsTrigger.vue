9c328fda0625cbb88ad424a070ee7663a2642f96
verona-dev <veronadev@tuta.io>
2025-07-18 15:56:10 +0200

    CR-20 [MARKETS] add single coin page adjust shadcn default tabs styles

--- components/ui/tabs/TabsTrigger.vue
+++ components/ui/tabs/TabsTrigger.vue
@@ -16,7 +16,13 @@ const forwardedProps = useForwardProps(delegatedProps)
     data-slot="tabs-trigger"
     v-bind="forwardedProps"
     :class="cn(
-      `py-5 data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-card text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
+      `py-5 data-[state=active]:bg-background dark:data-[state=active]:bg-chart-2
+      text-foreground dark:text-muted-foreground dark:data-[state=active]:text-foreground
+      focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring
+      inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 text-sm font-medium
+      whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px]
+      focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm
+      [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
       props.class,
     )"
   >
