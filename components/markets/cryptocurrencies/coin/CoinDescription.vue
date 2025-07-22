1cf470df6ff0b63ef8c9e8bf8a674611aa237f98
verona-dev <veronadev@tuta.io>
2025-07-22 17:47:15 +0200

    CR-20 [MARKETS] add single coin page coin description adjust style

--- components/markets/cryptocurrencies/coin/CoinDescription.vue
+++ components/markets/cryptocurrencies/coin/CoinDescription.vue
@@ -1,46 +1,48 @@
 <template>
-    <section v-if='coingecko' class='flex lg:flex-col items-stretch px-20'>
+    <div v-if='coingecko' class='flex lg:flex-col items-stretch px-20 w-full'>
         <h5 class='mb-6'>About
             <span class='capitalize'>{{ coingecko.id }}</span>
             <span class='ml-2'>&#40;{{ coin.symbol }}&#41;</span>
         </h5>
         
         <!--  Description  -->
-        <div v-show='description' class='bg-accent-foreground rounded-t-md'>
-            <Accordion type='single' collapsible class='py-3 px-6'>
-                <AccordionItem value='item-1' class=''>
-                    <AccordionTrigger>
-                        <p>What is {{ coingecko.name }}</p>
-                    </AccordionTrigger>
-                    
-                    <AccordionContent class='py-6'>{{ description }}</AccordionContent>
-                </AccordionItem>
-            </Accordion>
-        </div>
+        <Accordion
+            v-show='description'
+            type='single'
+            collapsible class='py-3 px-6 bg-muted rounded-t-md'
+        >
+            <AccordionItem value='item-1' class=''>
+                <AccordionTrigger>
+                    <p>What is {{ coingecko.name }}</p>
+                </AccordionTrigger>
+                
+                <AccordionContent class='py-6'>{{ description }}</AccordionContent>
+            </AccordionItem>
+        </Accordion>
+        
+        <Separator class='bg-ring' />
         
         <!--  Categories  -->
-        <div v-if='coingecko.categories' class='bg-accent-foreground rounded-b-md'>
-            <Accordion type='single' collapsible class='py-3 px-6'>
-                <AccordionItem value='item-1'>
-                    <AccordionTrigger>
-                        <p>Categories</p>
-                    </AccordionTrigger>
-                    
-                    <AccordionContent class='py-6'>
-                        <MazBadge
-                            v-for='category in coingecko.categories'
-                            :key='category'
-                            color='gray'
-                            class='m-2'
-                            outline
-                        >
-                            {{ category }}
-                        </MazBadge>
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
-        </div>
-    </section>
+        <Accordion v-if='coingecko.categories' type='single' collapsible class='py-3 px-6 bg-muted rounded-b-md'>
+            <AccordionItem value='item-1'>
+                <AccordionTrigger>
+                    <p>Categories</p>
+                </AccordionTrigger>
+                
+                <AccordionContent class='py-6'>
+                    <MazBadge
+                        v-for='category in coingecko.categories'
+                        :key='category'
+                        color='gray'
+                        class='m-2'
+                        outline
+                    >
+                        {{ category }}
+                    </MazBadge>
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    </div>
 </template>
 
 <script setup>
