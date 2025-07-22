9657ff394d7d7deed5b7e83e37de16a171c7fb56
verona-dev <veronadev@tuta.io>
2025-07-22 15:26:34 +0200

    CR-20 [MARKETS] add single coin page coin links refactor layout

--- components/markets/cryptocurrencies/coin/CoinLinks.vue
+++ components/markets/cryptocurrencies/coin/CoinLinks.vue
@@ -1,163 +1,101 @@
 <template>
-    <section class='coin-links flex flex-col xl:flex-row gap-y-20 xl:gap-0 xl:items-baseline'>
-        <!--  Websites + Chat  -->
-        <div class='flex xl:flex-col flex-wrap justify-evenly w-full xl:w-auto'>
-            <!--  Websites  -->
-            <div class='websites flex flex-col justify-center items-start lg:justify-start'>
-                <h4>Websites</h4>
-                
-                <!--  Homepage  -->
-                <NuxtLink
-                    v-if='livecoinwatchLinks.website'
-                    :to='livecoinwatchLinks.website'
-                    external
-                    target='_blank'
-                    class='flex'
-                >
-                    <div class='link-item flex items-center'>
-                        <NuxtIcon
-                            name='radix-icons:home'
-                            size='25'
-                            class='w-[50px]'
-                        />
-                        <p>Homepage</p>
-                    </div>
-                </NuxtLink>
-                
-                <!--  Whitepaper  -->
-                <NuxtLink
-                    v-if='livecoinwatchLinks.whitepaper'
-                    :to='livecoinwatchLinks.whitepaper'
-                    external
-                    target='_blank'
-                    class='flex mt-4'
-                >
-                    <div class='link-item flex items-center'>
-                        <NuxtIcon
-                            name='iconoir:open-book'
-                            size='25'
-                            class='w-[50px]'
-                        />
-                        <p>Whitepaper</p>
-                    </div>
-                </NuxtLink>
-                
-                <!--  Forum  -->
-                <div
-                    v-for='(link, name) in coingeckoLinks.official_forum_url'
-                    :key='name'
-                    class='flex items-center justify-start'
-                >
-                    <NuxtLink
-                        v-if='link'
-                        :to='link'
-                        external
-                        target='_blank'
-                        class='flex mt-4'
-                    >
-                        <div class='link-item flex items-center'>
-                            <NuxtIcon
-                                name='radix-icons:chat-bubble'
-                                size='25'
-                                class='w-[50px]'
-                            />
-                            <p>Official Forum</p>
-                        </div>
-                    </NuxtLink>
-                </div>
-            </div>
+    <section class='coin-links flex flex-wrap gap-10'>
+        <!-- Websites -->
+        <div class='group flex flex-col'>
+            <h4 class='mb-4'>Websites</h4>
             
-            <!--  Chat  -->
-            <div v-if='coingeckoLinks.chat_url.length' class='chat xl:mt-20'>
-                <h4>Chat</h4>
-                
-                <div
-                    v-for='chat in coingeckoLinks.chat_url'
-                    :key='chat'
-                    class='flex items-center'
+            <NuxtLink
+                v-if='livecoinwatchLinks.website'
+                :to='livecoinwatchLinks.website'
+                external
+                target='_blank'
+                class='flex items-center mb-3 link-item'
+            >
+                <NuxtIcon name='radix-icons:home' size='25' class='w-[50px]' />
+                <p>Homepage</p>
+            </NuxtLink>
+            
+            <NuxtLink
+                v-if='livecoinwatchLinks.whitepaper'
+                :to='livecoinwatchLinks.whitepaper'
+                external
+                target='_blank'
+                class='flex items-center mb-3 link-item'
+            >
+                <NuxtIcon name='iconoir:open-book' size='25' class='w-[50px]' />
+                <p>Whitepaper</p>
+            </NuxtLink>
+            
+            <div v-for='(link, name) in coingeckoLinks.official_forum_url' :key='name'>
+                <NuxtLink
+                    v-if='link'
+                    :to='link'
+                    external
+                    target='_blank'
+                    class='flex items-center mb-3 link-item'
                 >
-                    <NuxtLink
-                        v-if='chat'
-                        :to='chat'
-                        external
-                        target='_blank'
-                        class='flex mt-4'
-                    >
-                        <div class='link-item flex items-center'>
-                            <NuxtIcon
-                                name='iconoir:chat-lines'
-                                size='25'
-                                class='w-[50px]'
-                            />
-                            <p class='text-sm'>{{ chat }}</p>
-                        </div>
-                    </NuxtLink>
-                </div>
+                    <NuxtIcon name='radix-icons:chat-bubble' size='25' class='w-[50px]' />
+                    <p>Official Forum</p>
+                </NuxtLink>
             </div>
         </div>
         
-        <div class='flex xl:flex-col flex-wrap justify-evenly w-full xl:w-auto'>
-            <!--  Community  -->
-            <div v-if='livecoinwatchLinks?.socials && Object.keys(livecoinwatchLinks.socials).length' class='flex flex-col justify-center items-start lg:justify-start'>
-                <h4>Community</h4>
-                
-                <div
-                    v-for='(link, name) in livecoinwatchLinks?.socials'
-                    :key='name'
-                    class='flex items-center justify-start'
-                >
-                    <NuxtLink
-                        v-if='link && name'
-                        :to='link'
-                        external
-                        target='_blank'
-                    >
-                        <div class='link-item flex items-center py-3'>
-                            <NuxtIcon
-                                v-if='name'
-                                :key='name'
-                                :name="`fa-brands:${name}`"
-                                size='25'
-                                class='w-[50px]'
-                            />
-                            
-                            <p>{{ name }}</p>
-                        </div>
-                    
-                    </NuxtLink>
-                </div>
-            </div>
+        <!-- Chat -->
+        <div v-if='coingeckoLinks.chat_url.length' class='group flex flex-col '>
+            <h4 class='mb-4'>Chat</h4>
             
-            <!--  Github  -->
-            <div v-if='coingeckoLinks.repos_url?.github.length' class='xl:mt-20 flex flex-col items-start lg:justify-start'>
-                <h4>Github</h4>
-                
-                <div
-                    v-for='link in coingeckoLinks.repos_url.github'
-                    :key='link'
-                    class='flex items-center justify-start'
+            <div v-for='chat in coingeckoLinks.chat_url' :key='chat'>
+                <NuxtLink
+                    v-if='chat'
+                    :to='chat'
+                    external
+                    target='_blank'
+                    class='flex items-center mb-3 link-item'
                 >
-                    <NuxtLink
-                        v-if='link'
-                        :to='link'
-                        external
-                        target='_blank'
-                    >
-                        <div class='link-item flex items-center py-3'>
-                            <NuxtIcon
-                                name='fa-brands:github'
-                                size='25'
-                                class='w-[50px]'
-                            />
-                            
-                            <p class='text-sm'>{{ link }}</p>
-                        </div>
-                    
-                    </NuxtLink>
-                </div>
+                    <NuxtIcon name='iconoir:chat-lines' size='25' class='w-[50px]' />
+                    <p class='text-sm'>{{ chat }}</p>
+                </NuxtLink>
             </div>
         </div>
-
+        
+        <!-- Community -->
+        <div v-if='livecoinwatchLinks?.socials && Object.keys(livecoinwatchLinks.socials).length' class='group flex flex-col'>
+            <h4 class='mb-4'>Community</h4>
+            
+            <div
+                v-for='(link, name) in livecoinwatchLinks.socials'
+                :key='name'
+            >
+                <NuxtLink
+                    v-if='link && name'
+                    :to='link'
+                    external
+                    target='_blank'
+                    class='flex items-center py-2 link-item'
+                >
+                    <NuxtIcon :name='`fa-brands:${name}`' size='25' class='w-[50px]' />
+                    <p>{{ name }}</p>
+                </NuxtLink>
+            </div>
+        </div>
+        
+        <!-- Github -->
+        <div v-if='coingeckoLinks.repos_url?.github.length' class='group flex flex-col'>
+            <h4 class='mb-4'>Github</h4>
+            
+            <div v-for='link in coingeckoLinks.repos_url.github' :key='link'>
+                <NuxtLink
+                    v-if='link'
+                    :to='link'
+                    external
+                    target='_blank'
+                    class='flex items-center py-2 link-item'
+                >
+                    <NuxtIcon name='fa-brands:github' size='25' class='w-[50px]' />
+                    <p class='text-sm'>{{ link }}</p>
+                </NuxtLink>
+            </div>
+        </div>
     </section>
     
     <Separator class='my-4' />
@@ -181,6 +119,11 @@
 
 <style>
     .coin-links {
+        .group {
+            border: 1px solid yellow;
+            min-width: 400px;
+        }
+        
         .link-item {
             color: rgb(156 163 175 / var(--maz-tw-text-opacity, 1));
             
