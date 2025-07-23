76fdba13337e5c27bb28be441b3d7f9795d99e0f
verona-dev <veronadev@tuta.io>
2025-07-23 15:48:53 +0200

    CR-20 [MARKETS] add single coin page coin supply add trigger icon

--- components/markets/cryptocurrencies/coin/CoinSupply.vue
+++ components/markets/cryptocurrencies/coin/CoinSupply.vue
@@ -1,46 +1,48 @@
 <template>
-    <div class='my-10 flex flex-col lg:flex-row flex-wrap justify-center'>
-        <Drawer>
-            <!--  Open Drawer  -->
-            <DrawerTrigger>Supply Charts</DrawerTrigger>
+    <Drawer>
+        <!--  Open Drawer  -->
+        <DrawerTrigger>
+            <NuxtIcon
+                name='bitcoin-icons:pie-chart-outline'
+                size='75'
+                class='bg-yellow-500'
+            />
+        </DrawerTrigger>
+        
+        <DrawerContent>
+            <DrawerHeader>
+                <DrawerTitle>Title</DrawerTitle>
+                <DrawerDescription>Subtitle.</DrawerDescription>
+            </DrawerHeader>
             
-            <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle>Title</DrawerTitle>
-                    <DrawerDescription>Subtitle.</DrawerDescription>
-                </DrawerHeader>
+            <DrawerFooter>
+                <CoinSupplyProgressBars :coin='coin' />
                 
-                <DrawerFooter>
-                    <CoinSupplyProgressBars :coin='coin' />
+                <Tabs
+                    default-value='doughnut-chart'
+                    class='my-10 self-center w-full xl:w-1/2'
+                >
+                    <TabsList>
+                        <TabsTrigger value='doughnut-chart'>Doughnut Chart</TabsTrigger>
+                        <TabsTrigger value='stacked-bars'>Stacked Bars</TabsTrigger>
+                    </TabsList>
                     
-                    <Tabs
-                        default-value='doughnut-chart'
-                        class='my-10 self-center w-full xl:w-1/2'
-                    >
-                        <TabsList>
-                            <TabsTrigger value='doughnut-chart'>Doughnut Chart</TabsTrigger>
-                            <TabsTrigger value='stacked-bars'>Stacked Bars</TabsTrigger>
-                        </TabsList>
-                        
-                        <TabsContent value='doughnut-chart' class='min-h-[500px] xl:w-1/2 mx-auto'>
-                            <CoinSupplyDoughnutChart :coin='coin' />
-                        </TabsContent>
-                        
-                        <TabsContent value='stacked-bars' class='min-h-[500px] xl:w-11/12 mx-auto'>
-                            <CoinSupplyStackedBars :coin='coin' />
-                        </TabsContent>
-                    </Tabs>
+                    <TabsContent value='doughnut-chart' class='min-h-[500px] xl:w-1/2 mx-auto'>
+                        <CoinSupplyDoughnutChart :coin='coin' />
+                    </TabsContent>
                     
-                    <!--  Close Drawer  -->
-                    <DrawerClose>
-                        <Button variant="outline">
-                            Close
-                        </Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
-    </div>
+                    <TabsContent value='stacked-bars' class='min-h-[500px] xl:w-11/12 mx-auto'>
+                        <CoinSupplyStackedBars :coin='coin' />
+                    </TabsContent>
+                </Tabs>
+                
+                <!--  Close Drawer  -->
+                <DrawerClose>
+                        Close
+                </DrawerClose>
+            </DrawerFooter>
+        </DrawerContent>
+    </Drawer>
 </template>
 
 <script setup>
