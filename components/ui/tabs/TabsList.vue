e9205ff7ef4cda36eaec02f1ef64f093b73d814a
verona-dev <veronadev@tuta.io>
2025-07-23 19:16:05 +0200

    CR-20 [MARKETS] add single coin page adjust tabs default styles

--- components/ui/tabs/TabsList.vue
+++ components/ui/tabs/TabsList.vue
@@ -14,7 +14,7 @@ const delegatedProps = reactiveOmit(props, 'class')
     data-slot="tabs-list"
     v-bind="delegatedProps"
     :class="cn(
-      'bg-muted text-muted inline-flex h-9 items-center justify-center rounded-lg p-[3px] mx-auto my-10 py-6 px-1 w-3/5',
+      'bg-muted text-muted inline-flex h-9 items-center justify-center rounded-lg p-[3px] mx-auto my-10 py-8 px-1 w-3/5',
       props.class,
     )"
   >
