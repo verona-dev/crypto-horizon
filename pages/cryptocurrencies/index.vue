7e5e3a8c4972671fbf269015af45336ff0f29fc7
verona-dev <veronadev@tuta.io>
2025-07-24 20:18:06 +0200

    CR-20 [MARKETS] add single coin page fetch news on cc page

--- pages/cryptocurrencies/index.vue
+++ pages/cryptocurrencies/index.vue
@@ -8,4 +8,14 @@
 <script setup>
     import GlobalCrypto from '~/components/markets/cryptocurrencies/GlobalCrypto.vue';
     import CoinsTable from '~/components/markets/cryptocurrencies/CoinsTable.vue';
+    
+    // CryptocurrenciesStore
+    import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
+    const CryptocurrenciesStore = useCryptocurrenciesStore();
+    
+    const { fetchCoindeskNews } = CryptocurrenciesStore;
+    
+    onMounted(async() => {
+        await fetchCoindeskNews();
+    });
 </script>
