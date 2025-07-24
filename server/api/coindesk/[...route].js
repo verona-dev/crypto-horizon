03c3fd474a0d7a3fa8c6528b9e6b2f19a9985704
verona-dev <veronadev@tuta.io>
2025-07-24 19:48:14 +0200

    CR-20 [MARKETS] add single coin page coindesk server route complete request with method and headers

--- server/api/coindesk/[...route].js
+++ server/api/coindesk/[...route].js
@@ -7,7 +7,23 @@ export default defineEventHandler(async(event) => {
     const apiUrl = `https://data-api.coindesk.com/${route}`;
     
     try {
-    
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-api-key': coindesk_api,
+            },
+        });
+        
+        if (!response.ok) {
+            throw createError({
+                statusCode: response.status,
+                statusMessage: response.statusText,
+            });
+        }
+        
+        return await response.json();
     } catch(error) {
         throw createError({
             statusCode: 500,
