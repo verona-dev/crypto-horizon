7eb613882007d902bd6f8672a63337e048b58fa9
verona-dev <veronadev@tuta.io>
2025-07-25 19:00:32 +0200

    CR-8 [PAGE] Create /news remove news fetching from /cryptocurrencies

--- pages/cryptocurrencies/index.vue
+++ pages/cryptocurrencies/index.vue
@@ -8,14 +8,4 @@
 <script setup>
     import GlobalCrypto from '~/components/markets/cryptocurrencies/GlobalCrypto.vue';
     import CoinsTable from '~/components/markets/cryptocurrencies/CoinsTable.vue';
-    
-    // CryptocurrenciesStore
-    import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
-    const CryptocurrenciesStore = useCryptocurrenciesStore();
-    
-    const { getCoindeskNews } = CryptocurrenciesStore;
-    
-    onMounted(async() => {
-        await getCoindeskNews();
-    });
 </script>
