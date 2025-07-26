95842ccf3a43afb4026acab9371cec755a6e0a26
verona-dev <veronadev@tuta.io>
2025-07-26 16:46:20 +0200

    CR-28 [PERFORMANCE] move svg's to cloud welcome component skeleton working version

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,23 +1,85 @@
+<script setup>
+    import WelcomeItem from '~/components/WelcomeItem.vue';
+    
+    const welcomeItems = [
+        {
+            title: 'Cryptocurrencies',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525072/btc-circle_mbhyga.svg',
+            alt: 'cryptocurrencies image',
+            to: '/cryptocurrencies',
+        },
+        {
+            title: 'News',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-pink_nxk9ig.svg',
+            alt: 'news image',
+            to: '/news',
+        },
+        {
+            title: 'Defi',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-tree_b2gnne.svg',
+            alt: 'defi image',
+            to: '/defi',
+        },
+    ];
+</script>
+
 <template>
     <section class='welcome xl:mt-40 flex flax-col md:flex-row justify-center items-center gap-10 xl:gap-30'>
+        <WelcomeItem
+            v-for='item in welcomeItems'
+            :title='item.title'
+            :src='item.src'
+            :alt='item.alt'
+            :to='item.to'
+            :keyItem='item.title'
+        />
+        
+        <!--
         <NuxtLink to='/cryptocurrencies' class='link-item flex flex-col items-center justify-center'>
-            <NuxtImg src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525072/btc-circle_mbhyga.svg' alt='Image' />
+            <WelcomeSkeleton v-if='!isLoaded' />
+            
+            <NuxtImg
+                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525072/btc-circle_mbhyga.svg'
+                alt='Image'
+                @load='loadImg'
+                v-else
+            />
             <h3 class='my-5 uppercase'>Cryptocurrencies</h3>
         </NuxtLink>
+        -->
         
+<!--
         <NuxtLink to='/news' class='link-item flex flex-col items-center justify-center'>
-            <NuxtImg src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-pink_nxk9ig.svg' alt='Image' />
+            <WelcomeSkeleton v-if='!isLoaded' />
+            
+            <NuxtImg
+                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-pink_nxk9ig.svg'
+                alt='Image'
+                @load='loadImg'
+                v-else
+            />
             <h3 class='my-5 uppercase'>News</h3>
         </NuxtLink>
+        -->
         
+<!--
         <NuxtLink to='/defi' class='link-item flex flex-col items-center justify-center'>
-            <NuxtImg src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-tree_b2gnne.svg' alt='Image' />
+            <WelcomeSkeleton v-if='!isLoaded' />
+            
+            <NuxtImg
+                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753525063/btc-tree_b2gnne.svg'
+                alt='Image'
+                @load='loadImg'
+                v-else
+            />
             <h3 class='my-5 uppercase'>Defi</h3>
         </NuxtLink>
+        -->
     </section>
 </template>
 
 <style scoped>
+    /*
     .welcome {
         .link-item {
             &:hover img {
@@ -38,4 +100,5 @@
             width: 300px;
         }
     }
+    */
 </style>
--- components/WelcomeItem.vue
+++ components/WelcomeItem.vue
@@ -1,14 +1,16 @@
 <template>
-    <NuxtLink :to='to' class='link-item flex flex-col items-center justify-center'>
+    <NuxtLink :to='to' :key='keyItem' class='link-item flex flex-col items-center justify-center'>
         <WelcomeSkeleton v-if='!isLoaded' />
         
         <NuxtImg
             :src='src'
             :alt='alt'
             @load='loadImg'
-            v-else
+            :class="{ 'hidden': !isLoaded }"
         />
+        
         <h3 class='my-5 uppercase'>{{ title }}</h3>
+        <p>Image Loaded : {{isLoaded}} </p>
     </NuxtLink>
 </template>
 
@@ -16,23 +18,45 @@
     import WelcomeSkeleton from '~/components/skeletons/WelcomeSkeleton.vue';
     
     const props = defineProps({
+        title: String,
         src: String,
         alt: String,
         to: String,
-        title: String,
+        keyItem: String,
     });
     
     const {
+        title,
         src,
         alt,
         to,
-        title,
+        keyItem
     } = toRefs(props);
     
     const isLoaded = ref(false);
-    const loadImg = () => isLoaded.value = true;
+    const loadImg = () => {
+        isLoaded.value = true;
+        console.log('herre');
+    };
 </script>
 
 <style scoped>
-
+    .link-item {
+        &:hover img {
+            opacity: 0.85;
+            scale: 1.02;
+        }
+        
+        &:hover h3 {
+            color: var(--secondary);
+        }
+    }
+    
+    img {
+        border-radius: 6px;
+        height: 450px;
+        object-fit: cover;
+        transition: all 200ms ease-in-out;
+        width: 300px;
+    }
 </style>
