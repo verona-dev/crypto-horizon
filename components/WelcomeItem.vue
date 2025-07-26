6e97968ffbfd11a851281d23562213bd4fa53a84
verona-dev <veronadev@tuta.io>
2025-07-26 13:05:35 +0200

    CR-28 [PERFORMANCE] move svg's to cloud fill welcome-item component

--- components/WelcomeItem.vue
+++ components/WelcomeItem.vue
@@ -1,11 +1,36 @@
 <template>
-    <div class=''>
-    
-    </div>
+    <NuxtLink :to='to' class='link-item flex flex-col items-center justify-center'>
+        <WelcomeSkeleton v-if='!isLoaded' />
+        
+        <NuxtImg
+            :src='src'
+            :alt='alt'
+            @load='loadImg'
+            v-else
+        />
+        <h3 class='my-5 uppercase'>{{ title }}</h3>
+    </NuxtLink>
 </template>
 
 <script setup>
-
+    import WelcomeSkeleton from '~/components/skeletons/WelcomeSkeleton.vue';
+    
+    const props = defineProps({
+        src: String,
+        alt: String,
+        to: String,
+        title: String,
+    });
+    
+    const {
+        src,
+        alt,
+        to,
+        title,
+    } = toRefs(props);
+    
+    const isLoaded = ref(false);
+    const loadImg = () => isLoaded.value = true;
 </script>
 
 <style scoped>
