f65b892735cec50e41ac8eaa8f9a900744704c3e
verona-dev <veronadev@tuta.io>
2025-07-26 13:06:08 +0200

    CR-28 [PERFORMANCE] move svg's to cloud create welcome-skeleton component

---
+++ components/skeletons/WelcomeSkeleton.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class='h-[450px] w-[305px]'>
+        <Skeleton class='h-full w-full rounded-lg' />
+    </div>
+</template>
+
+<script setup>
+    import { Skeleton } from '~/components/ui/skeleton/index.js';
+</script>
