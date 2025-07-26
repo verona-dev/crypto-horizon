3cd35b89446c5b700c1d660321763a0908522fc8
verona-dev <veronadev@tuta.io>
2025-07-26 19:57:10 +0200

    CR-37 [PAGE] populate /news news adjustment

--- components/news/NewsItem.vue
+++ components/news/NewsItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <Card class='news-item w-[500px]'>
+    <Card class='news-item justify-between w-[450px] my-6'>
         <CardHeader>
             <CardDescription>
                 <img
@@ -8,29 +8,32 @@
                 />
             </CardDescription>
             
-            <h6 class='my-4'>{{ title }}</h6>
+            <p class='my-4'>{{ title }}</p>
         </CardHeader>
-        <CardContent>
-            <div class='flex flex-col space-y-1.5'>
-                <p>Name</p>
-                <p>description</p>
-            </div>
+        
+        <CardContent class='flex justify-between px-6 pb-6'>
+            <span>{{ article_author }}</span>
+            <span>{{ published_on }}</span>
         </CardContent>
+        
+        <!--
         <CardFooter class='flex justify-between px-6 pb-6'>
             <p>{{ author }}</p>
             <p>{{ publishedOn }}</p>
         </CardFooter>
+        -->
     </Card>
 </template>
 
 <script setup>
+    import dayjs from 'dayjs';
+    
     import {
         Card,
         CardContent,
         CardDescription,
         CardFooter,
         CardHeader,
-        CardTitle,
     } from '@/components/ui/card';
     
     const props = defineProps({
@@ -50,6 +53,12 @@
         publishedOn,
         imageUrl,
     } = toRefs(props);
+    
+    const published_on = computed(() => publishedOn.value && dayjs.unix(publishedOn.value).format('MMMM D, YYYY'));
+    const article_author = computed(() => {
+        if(author.value.length === 0) return 'Unknown author';
+        return author.value;
+    });
 </script>
 
 <style scoped>
