4438b6fe059a00866e120a23832706b85395555d
verona-dev <veronadev@tuta.io>
2025-08-02 17:52:57 +0200

    CR-45 [COIN] add news code cleanup

--- components/cryptocurrencies/coin/CoinNews.vue
+++ components/cryptocurrencies/coin/CoinNews.vue
@@ -1,9 +1,9 @@
 <template>
     <div v-if='coinNews' class='coin-news'>
-        this is coin news
+        <h1 class='my-28 self-center'>Latest {{ symbol }} Crypto News</h1>
         
         <div class='flex flex-wrap justify-center gap-12'>
-            test
+            
             <CoinNewsCard
                 v-for='article in articles'
                 :key='article.ID'
@@ -20,6 +20,16 @@
     import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
     const CryptocurrenciesStore = useCryptocurrenciesStore();
     
+    const props = defineProps({
+        coin: {
+            type: Object,
+            required: true,
+        }
+    });
+    
+    const { coin } = toRefs(props);
+    
     const { coinNews } = storeToRefs(CryptocurrenciesStore);
     const articles = computed(() => coinNews.value);
+    const symbol = coin.value?.symbol;
 </script>
