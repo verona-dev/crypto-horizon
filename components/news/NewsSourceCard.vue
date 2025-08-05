14b88aeecb2f126f6d857fc183d658c9e67aee93
verona-dev <veronadev@tuta.io>
2025-08-05 13:31:09 +0200

    CR-41 [NEWS] - additions create news-source-card component

---
+++ components/news/NewsSourceCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='news-source-card'>
+        {{ id }}
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        source: {}
+    });
+    
+    const { source } = toRefs(props);
+    
+    const id = source.value?.ID;
+    console.log(id);
+</script>
+
+<style scoped>
+
+</style>
