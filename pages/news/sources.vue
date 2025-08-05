76291e72fcd996ba11b7645fbd46217b16fdbd45
verona-dev <veronadev@tuta.io>
2025-08-05 13:18:37 +0200

    CR-41 [NEWS] - additions /news/sources add data fetching

--- pages/news/sources.vue
+++ pages/news/sources.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class='news-sources'>
-        this is News Sources page
+    <div class='news-sources max-w-[2000px] mx-auto flex flex-col'>
+        <div v-if='loading' class='flex items-center h-[75vh]'>
+            <MazSpinner
+                color='secondary'
+                size='3em'
+            />
+            <h4 class='ml-4 mb-0'>Loading...</h4>
+        </div>
+        
+        <div v-else>
+            <h1 class='my-28 self-center'>News Sources</h1>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    // CryptocurrenciesStore
+    import {storeToRefs} from 'pinia';
+    import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
+    const CryptocurrenciesStore = useCryptocurrenciesStore();
+    
+    const { newsSources, loading } = storeToRefs(CryptocurrenciesStore);
+    const { getCoindeskNewsSources } = CryptocurrenciesStore;
+    
+    onMounted(async() => {
+        await getCoindeskNewsSources();
+    });
 </script>
 
 <style scoped>
-
+    .news-sources {}
 </style>
