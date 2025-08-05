80c3b98484162656953dbdf9ee2ccab5f2856cee
verona-dev <veronadev@tuta.io>
2025-08-05 13:30:27 +0200

    CR-41 [NEWS] - additions /news add loading spinner while dta fetching

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,13 +1,23 @@
 <template>
     <div class='news max-w-[2000px] mx-auto flex flex-col'>
-        <h1 class='my-28 self-center'>Latest Crypto News</h1>
-        
-        <div v-if='articles' class='flex flex-wrap justify-center gap-12'>
-            <NewsCard
-                v-for='article in articles'
-                :key='article.ID'
-                :article='article'
+        <div v-if='loading' class='flex items-center h-[75vh]'>
+            <MazSpinner
+                color='secondary'
+                size='3em'
             />
+            <h4 class='ml-4 mb-0'>Loading...</h4>
+        </div>
+        
+        <div v-else>
+            <h1 class='my-28 self-center'>Latest Crypto News</h1>
+            
+            <div v-if='articles' class='flex flex-wrap justify-center gap-12'>
+                <NewsCard
+                    v-for='article in articles'
+                    :key='article.ID'
+                    :article='article'
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +29,7 @@
     import {useCryptocurrenciesStore} from '~/stores/CryptocurrenciesStore';
     const CryptocurrenciesStore = useCryptocurrenciesStore();
     
-    const { news } = storeToRefs(CryptocurrenciesStore);
+    const { news, loading } = storeToRefs(CryptocurrenciesStore);
     const articles = computed(() => news.value);
     const { getCoindeskNews } = CryptocurrenciesStore;
     
