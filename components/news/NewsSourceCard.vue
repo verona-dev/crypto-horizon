b64caba8b1ba65adba1e19773d04bdf1a0737da2
verona-dev <veronadev@tuta.io>
2025-08-05 15:19:04 +0200

    CR-41 [NEWS] - additions news-source-card layout adjustment

--- components/news/NewsSourceCard.vue
+++ components/news/NewsSourceCard.vue
@@ -1,6 +1,6 @@
 <template>
-    <Card v-if='source' class='news-source-card rounded-md border-card-border bg-transparent shadow-2xl justify-between w-[450px] py-0 gap-8'>
-        <CardHeader class='p-0'>
+    <Card v-if='source' class='news-source-card rounded-md border-none bg-transparent shadow-2xl justify-between w-7xl py-0 gap-8'>
+        <CardHeader class='px-6 py-10 flex items-center gap-8'>
             <!--  Main image  -->
             <NuxtImg
                 :src='img_url'
@@ -23,10 +23,19 @@
                 />
             </NuxtImg>
             
-            <p>Name: {{ name }}</p>
-            <span>Launch date: {{ launch_date }}</span>
-            <span>On Coindesk from: {{ created_on }}</span>
-            <p>Score: {{ benchmark_score }}</p>
+            <div>
+                <p>Name: {{ name }}</p>
+                <span>Launch date: {{ launch_date }}</span>
+                <span>On Coindesk from: {{ created_on }}</span>
+                <span>Last update: {{ last_updated }}</span>
+                <p>Score: {{ benchmark_score }}</p>
+                <div v-if='lang' class='flex items-center gap-2'>
+                    <p>Language:</p>
+                    <NuxtIcon  :name="`circle-flags:lang-${lang.toLowerCase()}`" size='20px' />
+                </div>
+                <p>Status: {{ status }}</p>
+                <p>Source type: {{ source_type }}</p>
+            </div>
         </CardHeader>
     </Card>
 </template>
@@ -38,8 +47,6 @@
     
     import { Card, CardDescription, CardFooter, CardHeader } from '@/components/ui/card';
     import { Skeleton } from '~/components/ui/skeleton/index.js';
-    import { Badge } from '@/components/ui/badge';
-    import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
     import { Button } from '@/components/ui/button';
     
     const props = defineProps({
@@ -52,7 +59,9 @@
     const created_on = dayjs.unix(source.value?.CREATED_ON).format('MMMM D, YYYY');
     const img_url = source.value?.IMAGE_URL;
     const lang = source.value?.LANG;
-    const last_updated = source.value?.LAST_UPDATED_TS;
+    const last_updated = computed(() => {
+        return dayjs.unix(source.value?.LAST_UPDATED_TS).format('MMMM D, YYYY, h:mma');
+    });
     const launch_date = computed(() => {
         if(source.value?.LAUNCH_DATE) {
             return dayjs.unix(source.value?.LAUNCH_DATE).format('MMMM D, YYYY');
@@ -67,12 +76,16 @@
 </script>
 
 <style scoped>
-.news-source-card {
-    img.main-image {
-        //object-fit: contain;
-        //height: 100%;
-        height: 200px;
-        //width: 100%;
+    .news-source-card {
+        &:hover {
+            background-color: var(--card-border);
+        }
+        
+        img.main-image {
+            //object-fit: contain;
+            //height: 100%;
+            height: 200px;
+            //width: 100%;
+        }
     }
-}
 </style>
