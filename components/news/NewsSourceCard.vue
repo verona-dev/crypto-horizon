58471599481f93c69c5f1864062ae43ffd91c7f7
verona-dev <veronadev@tuta.io>
2025-08-05 17:17:51 +0200

    CR-41 [NEWS] - additions news-source-card layout adjustment

--- components/news/NewsSourceCard.vue
+++ components/news/NewsSourceCard.vue
@@ -1,79 +1,81 @@
 <template>
-    <Card v-if='source' class='news-source-card rounded-md border-none bg-transparent shadow-2xl w-3xl py-0'>
-            <CardHeader class='px-6 py-10 flex items-center gap-12'>
-                <!--  Main image  -->
-                <NuxtLink :to='url' target='_blank'>
-                    <NuxtImg
-                        :src='img_url'
+    <Card v-if='source' class='news-source-card flex flex-col items-center gap-12 rounded-md border-none hover:shadow-2xl w-3xl py-12'>
+        <h2>{{ name }}</h2>
+        
+        <CardContent class='flex justify-around w-full'>
+            <!--  Main image  -->
+            <NuxtLink :to='url' target='_blank'>
+                <NuxtImg
+                    :src='img_url'
+                    alt='article image'
+                    class='main-image rounded-md'
+                    :custom='true'
+                    v-slot='{ src, isLoaded, imgAttrs }'
+                    preload
+                >
+                    <img
+                        v-if='isLoaded'
+                        v-bind='imgAttrs'
+                        :src='src'
                         alt='article image'
-                        class='main-image rounded-md'
-                        :custom='true'
-                        v-slot='{ src, isLoaded, imgAttrs }'
-                        preload
                     >
-                        <img
-                            v-if='isLoaded'
-                            v-bind='imgAttrs'
-                            :src='src'
-                            alt='article image'
-                        >
-                        
-                        <Skeleton
-                            v-else
-                            class='h-[250px] w-[250px]'
-                        />
-                    </NuxtImg>
-                </NuxtLink>
-                
-                <div class='flex flex-col'>
-                    <h3>{{ name }}</h3>
                     
-                    <Table>
-                        <TableCaption>Source details.</TableCaption>
-                        
-                        <TableBody>
-                            <!--  Launch Date  -->
-                            <TableRow>
-                                <TableCell class='font-medium'>Launch date</TableCell>
-                                <TableCell>{{ launch_date }}</TableCell>
-                            </TableRow>
-                             <!--  On Coindesk from  -->
-                            <TableRow v-if='created_on'>
-                                <TableCell class='font-medium'>On Coindesk from</TableCell>
-                                <TableCell>{{ created_on }}</TableCell>
-                            </TableRow>
-                            <!--  Last update  -->
-                            <TableRow v-if='last_updated'>
-                                <TableCell class='font-medium'>Last update</TableCell>
-                                <TableCell>{{ last_updated }}</TableCell>
-                            </TableRow>
-                            <!--  Benchmark Score  -->
-                            <TableRow v-if='benchmark_score'>
-                                <TableCell class='font-medium'>Score</TableCell>
-                                <TableCell>{{ benchmark_score }}</TableCell>
-                            </TableRow>
-                            <!--  Language  -->
-                            <TableRow v-if='lang'>
-                                <TableCell class='font-medium'>Language</TableCell>
-                                <TableCell>
-                                    <NuxtIcon  :name="`circle-flags:lang-${lang.toLowerCase()}`" size='20px' />
-                                </TableCell>
-                            </TableRow>
-                            <!--  Status  -->
-                            <TableRow v-if='lang'>
-                                <TableCell class='font-medium'>Status</TableCell>
-                                <TableCell>{{ status }}</TableCell>
-                            </TableRow>
-                            <!--  Source type  -->
-                            <TableRow v-if='lang'>
-                                <TableCell class='font-medium'>Source type</TableCell>
-                                <TableCell>{{ source_type }}</TableCell>
-                            </TableRow>
-                            
-                        </TableBody>
-                    </Table>
-                </div>
-            </CardHeader>
+                    <Skeleton
+                        v-else
+                        class='h-[275px] w-[275px]'
+                    />
+                </NuxtImg>
+            </NuxtLink>
+            
+            <div class='flex flex-col'>
+                <Table>
+                    <TableBody>
+                        <!--  Benchmark Score  -->
+                        <TableRow v-if='benchmark_score'>
+                            <TableCell class='font-medium'>Score</TableCell>
+                            <TableCell>{{ benchmark_score }}</TableCell>
+                        </TableRow>
+                        <!--  Launch Date  -->
+                        <TableRow>
+                            <TableCell class='font-medium'>Launch date</TableCell>
+                            <TableCell>{{ launch_date }}</TableCell>
+                        </TableRow>
+                        <!--  On Coindesk from  -->
+                        <TableRow v-if='created_on'>
+                            <TableCell class='font-medium'>Coindesk listing</TableCell>
+                            <TableCell>{{ created_on }}</TableCell>
+                        </TableRow>
+                        <!--  Language  -->
+                        <TableRow v-if='lang'>
+                            <TableCell class='font-medium'>Language</TableCell>
+                            <TableCell>
+                                <NuxtIcon  :name="`circle-flags:lang-${lang.toLowerCase()}`" size='20px' />
+                            </TableCell>
+                        </TableRow>
+                        <!--  Status  -->
+                        <TableRow v-if='lang'>
+                            <TableCell class='font-medium'>Status</TableCell>
+                            <TableCell>{{ status }}</TableCell>
+                        </TableRow>
+                        <!--  Source type  -->
+                        <TableRow v-if='lang'>
+                            <TableCell class='font-medium'>Source type</TableCell>
+                            <TableCell>{{ source_type }}</TableCell>
+                        </TableRow>
+                    
+                    </TableBody>
+                </Table>
+            </div>
+        </CardContent>
+        
+        <CardFooter class='flex justify-center'>
+            <!--  Read more  -->
+            <Button as-child variant='link' class='uppercase hover:text-foreground'>
+                <NuxtLink :to='url' target='_blank'>
+                    Visit Source
+                </NuxtLink>
+            </Button>
+        </CardFooter>
     </Card>
 </template>
 
@@ -82,9 +84,10 @@
     import relativeTime from 'dayjs/plugin/relativeTime';
     dayjs.extend(relativeTime, { rounding: Math.floor });
     
-    import { Card, CardDescription, CardFooter, CardHeader } from '@/components/ui/card';
+    import { Card, CardContent, CardFooter } from '@/components/ui/card';
     import { Skeleton } from '~/components/ui/skeleton/index.js';
     import { Button } from '@/components/ui/button';
+    
     import {
         Table,
         TableBody,
@@ -105,9 +108,6 @@
     const created_on = dayjs.unix(source.value?.CREATED_ON).format('MMMM D, YYYY');
     const img_url = source.value?.IMAGE_URL;
     const lang = source.value?.LANG;
-    const last_updated = computed(() => {
-        return dayjs.unix(source.value?.LAST_UPDATED_TS).format('MMMM D, YYYY, h:mma');
-    });
     const launch_date = computed(() => {
         if(source.value?.LAUNCH_DATE) {
             return dayjs.unix(source.value?.LAUNCH_DATE).format('MMMM D, YYYY');
@@ -117,21 +117,29 @@
     const name = source.value?.NAME;
     const source_type = source.value?.SOURCE_TYPE;
     const status = source.value?.STATUS;
-    const updated_on = source.value?.UPDATED_ON;
-    const url = source.value?.URL;
+
+    const source_url = source.value?.URL;
+    const url = computed(() => {
+        let url = new URL(source_url);
+        let protocol = url.protocol;
+        let host = url.host;
+        
+        return `${protocol}//${host}`;
+    });
 </script>
 
 <style scoped>
     .news-source-card {
+        
         &:hover {
-            background-color: var(--card-border);
+            background-color: var(--card-small);
         }
         
         img.main-image {
             //object-fit: contain;
             //height: 100%;
-            height: 250px;
-            width: 250px;
+            height: 275px;
+            width: 275px;
             //width: 100%;
         }
     }
