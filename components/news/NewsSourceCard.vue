57dffa392bc11b91569a09f146c0347a05019f4d
verona-dev <veronadev@tuta.io>
2025-08-05 13:39:46 +0200

    CR-41 [NEWS] - additions news-source-card extract individual source data

--- components/news/NewsSourceCard.vue
+++ components/news/NewsSourceCard.vue
@@ -12,7 +12,17 @@
     const { source } = toRefs(props);
     
     const id = source.value?.ID;
-    console.log(id);
+    const benchmark_score = source.value?.BENCHMARK_SCORE;
+    const created_on = source.value?.CREATED_ON;
+    const img_url = source.value?.IMAGE_URL;
+    const lang = source.value?.LANG;
+    const last_updated = source.value?.LAST_UPDATED_TS;
+    const launch_date = source.value?.LAUNCH_DATE;
+    const name = source.value?.NAME;
+    const source_type = source.value?.SOURCE_TYPE;
+    const status = source.value?.STATUS;
+    const updated_on = source.value?.UPDATED_ON;
+    const url = source.value?.URL;
 </script>
 
 <style scoped>
