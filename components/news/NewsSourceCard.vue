180e0b24d5f0a532a0e46a38392f31528950d1c7
verona-dev <veronadev@tuta.io>
2025-08-06 17:49:07 +0200

    CR-41 [NEWS] - additions news source card add flip effect

--- components/news/NewsSourceCard.vue
+++ components/news/NewsSourceCard.vue
@@ -1,80 +1,84 @@
 <template>
-    <Card v-if='source' class='news-source-card flex flex-col items-center gap-12 rounded-md border-none shadow-lg hover:shadow-2xl w-3xl py-12'>
-        <h2 class='hover-underline'>{{ name }}</h2>
+    <Card v-if='source' class='news-source-card flex items-center shadow-lg hover:shadow-2xl w-md h-[650px] gap-0 cursor-text'>
+        <h3 class='mt-24 mb-4 hover-underline text-wrap'>{{ name }}</h3>
         
-        <CardContent class='flex justify-around w-full'>
-            <!--  Main image  -->
-            <NuxtLink :to='url' target='_blank' external>
-                <NuxtImg
-                    :src='img_url'
-                    alt='article image'
-                    class='main-image rounded-md'
-                    :custom='true'
-                    v-slot='{ src, isLoaded, imgAttrs }'
-                    preload
-                >
-                    <img
-                        v-if='isLoaded'
-                        v-bind='imgAttrs'
-                        :src='src'
+        <div class='card-container h-full w-full'>
+            <div class='card-inner'>
+                <div class='card-front'>
+                    <!--  Main image  -->
+                    <NuxtImg
+                        :src='img_url'
                         alt='article image'
+                        class='main-image rounded-md'
+                        :custom='true'
+                        v-slot='{ src, isLoaded, imgAttrs }'
+                        preload
                     >
+                        <img
+                            v-if='isLoaded'
+                            v-bind='imgAttrs'
+                            :src='src'
+                            alt='article image'
+                        >
+                        
+                        <Skeleton
+                            v-else
+                            class='h-[200px] w-[200px]'
+                        />
+                    </NuxtImg>
+                </div>
+                
+                <div class='card-back bg-card flex flex-col gap-12'>
+                    <CardContent class='w-4/5 px-0'>
+                        <Table>
+                            <TableBody>
+                                <!--  Benchmark Score  -->
+                                <TableRow v-if='benchmark_score'>
+                                    <TableCell class='font-medium'>Score</TableCell>
+                                    <TableCell>{{ benchmark_score }}</TableCell>
+                                </TableRow>
+                                <!--  Launch Date  -->
+                                <TableRow>
+                                    <TableCell class='font-medium'>Launch date</TableCell>
+                                    <TableCell>{{ launch_date }}</TableCell>
+                                </TableRow>
+                                <!--  On Coindesk from  -->
+                                <TableRow v-if='created_on'>
+                                    <TableCell class='font-medium'>Coindesk listing</TableCell>
+                                    <TableCell>{{ created_on }}</TableCell>
+                                </TableRow>
+                                <!--  Language  -->
+                                <TableRow v-if='lang'>
+                                    <TableCell class='font-medium'>Language</TableCell>
+                                    <TableCell>
+                                        <NuxtIcon  :name="`circle-flags:lang-${lang.toLowerCase()}`" size='20px' />
+                                    </TableCell>
+                                </TableRow>
+                                <!--  Status  -->
+                                <TableRow v-if='lang'>
+                                    <TableCell class='font-medium'>Status</TableCell>
+                                    <TableCell>{{ status }}</TableCell>
+                                </TableRow>
+                                <!--  Source type  -->
+                                <TableRow v-if='lang'>
+                                    <TableCell class='font-medium'>Source type</TableCell>
+                                    <TableCell>{{ source_type }}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </CardContent>
                     
-                    <Skeleton
-                        v-else
-                        class='h-[275px] w-[275px]'
-                    />
-                </NuxtImg>
-            </NuxtLink>
-            
-            <div class='flex flex-col'>
-                <Table>
-                    <TableBody>
-                        <!--  Benchmark Score  -->
-                        <TableRow v-if='benchmark_score'>
-                            <TableCell class='font-medium'>Score</TableCell>
-                            <TableCell>{{ benchmark_score }}</TableCell>
-                        </TableRow>
-                        <!--  Launch Date  -->
-                        <TableRow>
-                            <TableCell class='font-medium'>Launch date</TableCell>
-                            <TableCell>{{ launch_date }}</TableCell>
-                        </TableRow>
-                        <!--  On Coindesk from  -->
-                        <TableRow v-if='created_on'>
-                            <TableCell class='font-medium'>Coindesk listing</TableCell>
-                            <TableCell>{{ created_on }}</TableCell>
-                        </TableRow>
-                        <!--  Language  -->
-                        <TableRow v-if='lang'>
-                            <TableCell class='font-medium'>Language</TableCell>
-                            <TableCell>
-                                <NuxtIcon  :name="`circle-flags:lang-${lang.toLowerCase()}`" size='20px' />
-                            </TableCell>
-                        </TableRow>
-                        <!--  Status  -->
-                        <TableRow v-if='lang'>
-                            <TableCell class='font-medium'>Status</TableCell>
-                            <TableCell>{{ status }}</TableCell>
-                        </TableRow>
-                        <!--  Source type  -->
-                        <TableRow v-if='lang'>
-                            <TableCell class='font-medium'>Source type</TableCell>
-                            <TableCell>{{ source_type }}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
+                    <CardFooter class='flex justify-center mt-6'>
+                        <!--  Read more  -->
+                        <Button as-child variant='link' class='uppercase hover:text-foreground'>
+                            <NuxtLink :to='url' target='_blank' external>
+                                Visit Source
+                            </NuxtLink>
+                        </Button>
+                    </CardFooter>
+                </div>
             </div>
-        </CardContent>
-        
-        <CardFooter class='flex justify-center'>
-            <!--  Read more  -->
-            <Button as-child variant='link' class='uppercase hover:text-foreground'>
-                <NuxtLink :to='url' target='_blank' external>
-                    Visit Source
-                </NuxtLink>
-            </Button>
-        </CardFooter>
+        </div>
     </Card>
 </template>
 
@@ -113,7 +117,7 @@
     const name = source.value?.NAME;
     const source_type = source.value?.SOURCE_TYPE;
     const status = source.value?.STATUS;
-
+    
     const source_url = source.value?.URL;
     const url = computed(() => {
         let url = new URL(source_url);
@@ -126,13 +130,43 @@
 
 <style scoped>
     .news-source-card {
-        &:hover {
-            background-color: var(--card-small);
+        .card-inner {
+            width: 100%;
+            height: 100%;
+            transition: transform 0.6s;
+            transform-style: preserve-3d;
+        }
+        
+        .card-front,
+        .card-back {
+            position: absolute;
+            width: 100%;
+            backface-visibility: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        
+        .card-front {
+            height: 350px;
+        }
+        
+        .card-back {
+            height: 450px;
+            transform: rotateY(180deg);
         }
         
         img.main-image {
-            height: 275px;
-            width: 275px;
+            height: 250px;
+            width: 250px;
+        }
+        
+        &:hover {
+            border: 1px solid var(--card-border);
+        }
+        
+        &:hover .card-inner {
+            transform: rotateY(180deg);
         }
         
         .hover-underline {
@@ -150,7 +184,7 @@
             left: 0;
             transform: scaleX(0);
             transform-origin: right;
-            transition: transform 0.4s ease-out;
+            transition: transform 0.6s ease-out;
         }
         
         .hover-underline::before {
