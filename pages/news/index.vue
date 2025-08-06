72adfd0b9a64b59aa52d1680d866c4e40c86e832
verona-dev <veronadev@tuta.io>
2025-08-06 17:15:13 +0200

    CR-41 [NEWS] - additions news card fix layout

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='news max-w-[2000px] mx-auto flex flex-col'>
+    <div class='news max-w-[2000px] mx-auto px-20 flex flex-col'>
         <div v-if='loading' class='flex items-center h-[75vh]'>
             <MazSpinner
                 color='secondary'
@@ -8,8 +8,11 @@
             <h4 class='ml-4 mb-0'>Loading...</h4>
         </div>
         
-        <div v-else>
-            <h1 class='my-28 self-center'>Latest Crypto News</h1>
+        <div
+            v-else
+            class='flex flex-col items-center'
+        >
+            <h1 class='my-28'>Latest Crypto News</h1>
             
             <div v-if='articles' class='flex flex-wrap justify-center gap-12'>
                 <NewsCard
