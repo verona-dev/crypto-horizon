98a7819ee06c29804397852be495fbd9c064f107
verona-dev <veronadev@tuta.io>
2025-08-07 15:10:44 +0200

    CR-41 [NEWS] - additions create breadcrumb component

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,37 @@
+<template>
+    <Breadcrumb class='breadcrumbs'>
+        <BreadcrumbList>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='/'>
+                    Home
+                </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+                <BreadcrumbLink href='/components'>
+                    Components
+                </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+                <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+            </BreadcrumbItem>
+        </BreadcrumbList>
+    </Breadcrumb>
+</template>
+
+<script setup>
+    import {
+        Breadcrumb,
+        BreadcrumbEllipsis,
+        BreadcrumbItem,
+        BreadcrumbLink,
+        BreadcrumbList,
+        BreadcrumbPage,
+        BreadcrumbSeparator,
+    } from '@/components/ui/breadcrumb';
+</script>
+
+<style scoped>
+
+</style>
