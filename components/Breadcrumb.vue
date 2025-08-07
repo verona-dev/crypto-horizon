afe39ef33f57df26a0540d1068f67321f994bf9b
verona-dev <veronadev@tuta.io>
2025-08-07 17:43:33 +0200

    CR-58 [NAV] add breadcrumbs breadcrumb working version

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -1,20 +1,15 @@
 <template>
-    <Breadcrumb class='breadcrumbs'>
+    <Breadcrumb class='breadcrumbs mt-20'>
         <BreadcrumbList>
-            <BreadcrumbItem>
-                <BreadcrumbLink href='/'>
-                    Home
-                </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-                <BreadcrumbLink href='/components'>
-                    Components
-                </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-                <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+            <BreadcrumbItem v-for='(item, index) in items' :key='index'>
+                <template v-if='index !== items.length - 1'>
+                    <NuxtLink :to='item.url'>{{ item.name }}</NuxtLink>
+                    
+                    <BreadcrumbSeparator />
+                </template>
+                <template v-else>
+                    <BreadcrumbPage>{{ item.name }}</BreadcrumbPage>
+                </template>
             </BreadcrumbItem>
         </BreadcrumbList>
     </Breadcrumb>
@@ -23,15 +18,25 @@
 <script setup>
     import {
         Breadcrumb,
-        BreadcrumbEllipsis,
         BreadcrumbItem,
-        BreadcrumbLink,
         BreadcrumbList,
         BreadcrumbPage,
         BreadcrumbSeparator,
     } from '@/components/ui/breadcrumb';
+    
+    const route = useRoute();
+    console.log(route);
+    
+    const items = computed(() => {
+        const paths = route.path.split('/').filter(Boolean);
+        
+        return [
+            { name: 'Home', url: '/' },
+            ...paths.map((segment, i) => {
+                const url = '/' + paths.slice(0, i + 1).join('/');
+                const name = segment.charAt(0).toUpperCase() + segment.slice(1);
+                return { name, url };
+            }),
+        ];
+    });
 </script>
-
-<style scoped>
-
-</style>
