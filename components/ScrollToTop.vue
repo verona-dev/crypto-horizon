715c8534a1a9abd8ab3fedf167b6552d1e36f3cc
verona-dev <veronadev@tuta.io>
2025-08-09 20:56:39 +0200

    CR-47 [FEATURE] add scroll to top scroll-to-top adjust logic

--- components/ScrollToTop.vue
+++ components/ScrollToTop.vue
@@ -1,18 +1,38 @@
 <template>
-    <div @click='scrollOnClick' class='scroll-to-top'>
+    <div v-if='show' @click='scrollOnClick' class='scroll-to-top'>
         Scroll to top
     </div>
 </template>
 
 <script setup>
+    const scrollY = ref(0);
+    const show = computed(() => scrollY.value >= 400);
+    
+    const handleScroll = () => scrollY.value = window.scrollY;
+    
     const scrollOnClick = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     };
+    
+    onMounted(() => {
+        window.addEventListener('scroll', handleScroll);
+    });
+    
+    onUnmounted(() => {
+        window.removeEventListener('scroll', handleScroll);
+    });
 </script>
 
 <style scoped>
-
+    .scroll-to-top {
+        position: fixed;
+        bottom: 100px;
+        right: 25px;
+        z-index: 30;
+        //opacity: 0;
+        transition: all 1s ease;
+    }
 </style>
