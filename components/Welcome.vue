0988ba08fdeb2cad683264daed2a07463b1b9555
verona-dev <veronadev@tuta.io>
2025-08-09 13:07:37 +0200

    CR-24 [DESIGN] Add favicon welcome add text animation

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='flex flex-col items-center gap-8 md:gap-32 px-10'>
+    <div class='flex flex-col items-center gap-8 md:gap-32 px-10 pb-10'>
         <h1 class='my-20 md:my-0'>Crypto Horizon</h1>
         
         <div class='flex flex-col md:flex-row justify-center items-center gap-6 xl:gap-30'>
@@ -43,8 +43,24 @@
 
 <style scoped>
     h1 {
-        background: -webkit-linear-gradient(#C6FFDD, #FBD786, #f7797d);
+        letter-spacing: 1rem;
+        word-spacing: 2.5rem;
+        background-image: url('https://images.unsplash.com/photo-1518546305927-5a555bb7020d?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+        //background-image: url('https://images.unsplash.com/photo-1516245834210-c4c142787335?q=80&w=2969&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+        background-size: 300%;
+        background-position: 0 50%;
+        background-clip: text;
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+        animation: animate-background 100s infinite alternate linear;
+    }
+    
+    @keyframes animate-background {
+        0% {
+            background-position: 0 50%;
+        }
+        100% {
+            background-position: 100% 50%;
+        }
     }
 </style>
