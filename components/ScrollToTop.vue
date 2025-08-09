1bc3feb553056b0bbf4c02b82abdef6cf9e17355
verona-dev <veronadev@tuta.io>
2025-08-09 20:48:34 +0200

    CR-47 [FEATURE] add scroll to top create scroll-to-top component

---
+++ components/ScrollToTop.vue
@@ -0,0 +1,18 @@
+<template>
+    <div @click='scrollOnClick' class='scroll-to-top'>
+        Scroll to top
+    </div>
+</template>
+
+<script setup>
+    const scrollOnClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+</script>
+
+<style scoped>
+
+</style>
