989c4c3c12a38483246eb8fde2dee844412a7902
verona-dev <veronadev@tuta.io>
2025-08-12 14:04:37 +0200

    CR-54 [PERFORMANCE] project cleanup composable apiCoingecko code cleanup

--- composables/apiCoingecko.js
+++ composables/apiCoingecko.js
@@ -12,13 +12,3 @@ export async function useFetchCoingecko(route, options = {})  {
         console.error('Error fetching CoinGecko data', error);
     }
 }
-
-/*
-export const useCoingeckoHistoricalChartData = (coin, days) => {
-    const params = new URLSearchParams();
-    params.append('vs_currency', 'usd');
-    params.append('interval', 'daily');
-    days && params.append('days', days);
-    const chartData = `coins/${coin}/market_chart?${params}`;
-}
-*/
