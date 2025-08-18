e553f06f33a114e25d500dd8a8c79fd4c7870b8f
verona-dev <veronadev@tuta.io>
2025-08-18 15:20:24 +0200

    CR-42 [COIN] - additions coin-supply adjust select-item default style

--- components/ui/select/SelectItem.vue
+++ components/ui/select/SelectItem.vue
@@ -25,14 +25,17 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="
       cn(
-        `focus:bg-accent focus:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex w-full cursor-default items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2`,
+        `focus:bg-muted focus:text-secondary [&_svg:not([class*='text-'])]:text-muted-foreground relative
+        flex w-full cursor-pointer items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none
+        data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none
+        [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2`,
         props.class,
       )
     "
   >
     <span class="absolute right-2 flex size-3.5 items-center justify-center">
       <SelectItemIndicator>
-        <Check class="size-4" />
+        <Check class="size-4 hover:text-secondary" />
       </SelectItemIndicator>
     </span>
 
