dd46a9443f48ac6501cc85d5e5a151694056e3fa
verona-dev <veronadev@tuta.io>
2025-08-18 16:40:58 +0200

    CR-42 [COIN] - additions coin supply adjust select style

--- components/ui/select/SelectTrigger.vue
+++ components/ui/select/SelectTrigger.vue
@@ -27,7 +27,7 @@ const forwardedProps = useForwardProps(delegatedProps)
       dark:bg-input/30 dark:hover:bg-input/50 flex w-fit items-center justify-between gap-2 rounded-md
       border bg-transparent px-3 py-2 text-sm whitespace-nowrap shadow-xs transition-[color,box-shadow]
       outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
-      data-[size=default]:h-16 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1
+      data-[size=default]:h-14 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1
       *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2
       [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
       props.class,
