fa1d478e383390f684e88e7d4de39d9cdc63809b
verona-dev <veronadev@tuta.io>
2025-08-18 13:59:24 +0200

    CR-42 [COIN] - additions /news update title

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -9,7 +9,7 @@
         </div>
         
         <div v-else class='flex flex-col items-center gap-40'>
-            <h1 class='mt-10'>Latest Crypto News</h1>
+            <h1 class='mt-10'>Latest News</h1>
             
             <div v-if='articles' class='flex flex-wrap justify-center gap-12'>
                 <NewsCard
