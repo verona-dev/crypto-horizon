f558a703af4223bf5e986658ab4d6803f81a03ee
verona-dev <veronadev@tuta.io>
2025-08-22 17:03:48 +0200

    CR-44 [COIN] chart add timeframes fix breadcrumb horizontal layout shift

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -1,18 +1,20 @@
 <template>
-    <Breadcrumb class='breadcrumbs h-10 my-10 border border-t-secondary border-b-secondary border-r-0 border-l-0 px-4 py-2'>
-        <BreadcrumbList>
-            <BreadcrumbItem v-for='(item, index) in items' :key='index'>
-                <template v-if='index !== items.length - 1'>
-                    <NuxtLink :to='item.url'>{{ item.name }}</NuxtLink>
-                    
-                    <BreadcrumbSeparator />
-                </template>
-                <template v-else>
-                    <BreadcrumbPage>{{ item.name }}</BreadcrumbPage>
-                </template>
-            </BreadcrumbItem>
-        </BreadcrumbList>
-    </Breadcrumb>
+    <div class='w-full'>
+        <Breadcrumb class='breadcrumbs w-fit h-10 mx-auto my-10 border border-t-secondary border-b-secondary border-r-0 border-l-0 px-4 py-2'>
+            <BreadcrumbList>
+                <BreadcrumbItem v-for='(item, index) in items' :key='index'>
+                    <template v-if='index !== items.length - 1'>
+                        <NuxtLink :to='item.url'>{{ item.name }}</NuxtLink>
+                        
+                        <BreadcrumbSeparator />
+                    </template>
+                    <template v-else>
+                        <BreadcrumbPage>{{ item.name }}</BreadcrumbPage>
+                    </template>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    </div>
 </template>
 
 <script setup>
