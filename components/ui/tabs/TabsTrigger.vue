638d750994c239d85d689ad401da74b76fc2e4ae
verona-dev <veronadev@tuta.io>
2025-08-23 15:37:44 +0200

    CR-44 [COIN] chart add timeframes reset shadcn tabs-trigger styles

--- components/ui/tabs/TabsTrigger.vue
+++ components/ui/tabs/TabsTrigger.vue
@@ -16,13 +16,7 @@ const forwardedProps = useForwardProps(delegatedProps)
     data-slot="tabs-trigger"
     v-bind="forwardedProps"
     :class="cn(
-      `w-72 py-5 data-[state=active]:bg-background dark:data-[state=active]:bg-accent-foreground hover:bg-accent-foreground
-      text-foreground dark:text-muted-foreground dark:data-[state=active]:text-secondary
-      border dark:data-[state=active]:border-secondary focus-visible:border-ring
-      focus-visible:ring-ring/50 inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-sm text-sm font-medium
-      whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px]
-      focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-xl
-      [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
+      `data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-card text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
       props.class,
     )"
   >
