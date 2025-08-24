0e4df0f49c234abc6726e61a26e47ccad6568165
verona-dev <veronadev@tuta.io>
2025-08-24 13:17:43 +0200

    CR-49 [FEATURE] create new navigation add new navigation to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
     <Navigation />
     
+    <NavigationNew />
+    
     <main class='flex flex-1 flex-col items-center max-w-[2000px] mx-auto'>
         <Breadcrumb />
         <slot />
@@ -10,6 +12,7 @@
 
 <script setup>
     import Navigation from '~/components/Navigation.vue';
+    import NavigationNew from '~/components/NavigationNew.vue';
     import Breadcrumb from '~/components/Breadcrumb.vue';
     import ScrollToTop from '~/components/ScrollToTop.vue';
 </script>
