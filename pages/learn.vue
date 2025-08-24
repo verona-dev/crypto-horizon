c7985ff64aea739d64efbf9ce5d17bbe3689c6bc
verona-dev <veronadev@tuta.io>
2025-08-24 18:36:56 +0200

    CR-49 [FEATURE] create new navigation create /learn

---
+++ pages/learn.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='learn custom-flex flex-col gap-6'>
+        <h1 class='text-pink-500'>Academy</h1>
+        <h6>Coming Soon...</h6>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .learn {}
+</style>
