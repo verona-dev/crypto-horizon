a00817974283e84334eae18a6f350dbffbae5d26
verona-dev <veronadev@tuta.io>
2025-08-26 16:19:14 +0200

    CR-49 [FEATURE] create new navigation sidebar adjust trigger

--- components/ui/sidebar/SidebarTrigger.vue
+++ components/ui/sidebar/SidebarTrigger.vue
@@ -8,16 +8,25 @@
         class?: HTMLAttributes["class"]
     }>()
     
-    const { toggleSidebar } = useSidebar()
+    const { open, toggleSidebar } = useSidebar()
 </script>
 
 <template>
+    <!--
+    :name='open ? "iconoir:sidebar-collapse" : "iconoir:sidebar-expand"'
+    :name='open ? "tabler:layout-sidebar-right-filled" : "tabler:layout-sidebar-right-collapse-filled"'
+    :name='open ? "stash:burger-arrow-left-duotone" : "stash:burger-arrow-right-duotone"'
+    -->
     <NuxtIcon
-        name='gala:sidebar-right'
+        :name='open ? "stash:burger-arrow-left-duotone" : "stash:burger-arrow-right-duotone"'
         data-sidebar="trigger"
         data-slot="sidebar-trigger"
-        :class="cn('h-8 w-8', props.class)"
-        class='hover:cursor-pointer hover:bg-secondary'
+        :class="cn(
+            'h-8 w-8 text-secondary hover:bg-secondary',
+                    { 'text-muted-custom': open },
+                    { 'hover:bg-muted-custom': !open },
+                    props.class
+            )"
         @click="toggleSidebar"
     >
         <PanelLeft />
