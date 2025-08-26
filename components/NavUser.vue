f04e058672e79850acd8142f681c300b9c646a53
verona-dev <veronadev@tuta.io>
2025-08-26 14:11:09 +0200

    CR-49 [FEATURE] create new navigation sidebar format cleanup

--- components/NavUser.vue
+++ components/NavUser.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang='ts'>
     import {
         BadgeCheck,
         Bell,
@@ -6,7 +6,7 @@
         CreditCard,
         LogOut,
         Sparkles,
-    } from "lucide-vue-next"
+    } from 'lucide-vue-next'
     import {
         Avatar,
         AvatarFallback,
@@ -46,44 +46,44 @@
                 <DropdownMenuTrigger as-child>
                     <!--  Close-->
                     <SidebarMenuButton
-                        size="lg"
-                        class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                        size='lg'
+                        class='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
                     >
-                        <Avatar class="h-8 w-8 rounded-lg">
-                            <AvatarImage :src="user.avatar" :alt="user.name" />
-                            <AvatarFallback class="rounded-lg text-lg">
+                        <Avatar class='h-8 w-8 rounded-lg'>
+                            <AvatarImage :src='user.avatar' :alt='user.name' />
+                            <AvatarFallback class='rounded-lg text-lg'>
                                 G
                             </AvatarFallback>
                         </Avatar>
                         
-                        <div class="grid flex-1 text-left text-sm leading-tight">
-                            <span class="truncate font-medium">{{ user.name }}</span>
-                            <span class="truncate text-xs">{{ user.email }}</span>
+                        <div class='grid flex-1 text-left text-sm leading-tight'>
+                            <span class='truncate font-medium'>{{ user.name }}</span>
+                            <span class='truncate text-xs'>{{ user.email }}</span>
                         </div>
                         
-                        <ChevronsUpDown class="ml-auto size-4" />
+                        <ChevronsUpDown class='ml-auto size-4' />
                     </SidebarMenuButton>
                 </DropdownMenuTrigger>
                 
                 <!--  Open-->
                 <DropdownMenuContent
-                    class="w-[--reka-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                    class='w-[--reka-dropdown-menu-trigger-width] min-w-56 rounded-lg'
                     :side="isMobile ? 'bottom' : 'right'"
-                    align="end"
-                    :side-offset="4"
+                    align='end'
+                    :side-offset='4'
                 >
-                    <DropdownMenuLabel class="p-0 font-normal">
-                        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                            <Avatar class="h-8 w-8 rounded-lg">
-                                <AvatarImage :src="user.avatar" :alt="user.name" />
-                                <AvatarFallback class="rounded-lg text-lg">
+                    <DropdownMenuLabel class='p-0 font-normal'>
+                        <div class='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
+                            <Avatar class='h-8 w-8 rounded-lg'>
+                                <AvatarImage :src='user.avatar' :alt='user.name' />
+                                <AvatarFallback class='rounded-lg text-lg'>
                                     G
                                 </AvatarFallback>
                             </Avatar>
                             
-                            <div class="grid flex-1 text-left text-sm leading-tight">
-                                <span class="truncate font-semibold">{{ user.name }}</span>
-                                <span class="truncate text-xs">{{ user.email }}</span>
+                            <div class='grid flex-1 text-left text-sm leading-tight'>
+                                <span class='truncate font-semibold'>{{ user.name }}</span>
+                                <span class='truncate text-xs'>{{ user.email }}</span>
                             </div>
                         </div>
                     </DropdownMenuLabel>
