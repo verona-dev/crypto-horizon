451d270eea60859cb4f03e443b240025909f9262
verona-dev <veronadev@tuta.io>
2025-08-27 20:18:23 +0200

    CR-49 [FEATURE] create new navigation sidebar logo adjust styles

--- components/NavLogo.vue
+++ components/NavLogo.vue
@@ -16,23 +16,23 @@
 </script>
 
 <template>
-    <SidebarMenu>
+    <SidebarMenu :class='{ "ml-3" : !open }'>
         <SidebarMenuItem>
             <DropdownMenu>
-                <DropdownMenuTrigger>
+                <DropdownMenuTrigger :class='{ "ml-3" : open }'>
                     <NuxtLink to='/'>
                         <SidebarMenuButton
-                            tooltip="Startpage"
-                            size="lg"
-                            class="flex gap-4 data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                            tooltip='Startpage'
+                            size='lg'
+                            class='flex gap-4 data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
                         >
-                            <div class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                            <div class='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary'>
                                 <NuxtIcon
                                     name='my-icon:astronaut'
                                     size='35'
                                 />
                             </div>
-                            <div class="grid flex-1 text-left text-sm leading-tight">
+                            <div class="flex-1 text-left text-sm leading-tight">
                                 <p class="custom truncate font-medium">Crypto Horizon</p>
                             </div>
                         </SidebarMenuButton>
