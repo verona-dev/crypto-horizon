ee644120fbb1fedfa6e235b66d2cea8a886154d3
verona-dev <veronadev@tuta.io>
2025-08-27 20:00:35 +0200

    CR-49 [FEATURE] create new navigation sidebar trigger adjust styles

--- components/ui/sidebar/SidebarTrigger.vue
+++ components/ui/sidebar/SidebarTrigger.vue
@@ -3,7 +3,7 @@
     import { PanelLeft } from "lucide-vue-next"
     import { cn } from "@/lib/utils"
     import { useSidebar } from "./utils"
-    import {SidebarMenuButton} from '~/components/ui/sidebar';
+    import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '~/components/ui/sidebar';
     
     const props = defineProps<{
         class?: HTMLAttributes["class"]
@@ -13,46 +13,52 @@
 </script>
 
 <template>
-    <!--  Open  -->
-    <template v-if='open'>
-        <SidebarMenuButton
-            tooltip='Close Menu'
-            size="lg"
-            class='h-10'
-            @click='toggleSidebar'
-        >
-            <div class='flex w-full'>
-                <NuxtIcon
-                    name='stash:burger-arrow-left-duotone'
-                    data-sidebar='trigger'
-                    data-slot='sidebar-trigger'
-                    :class='cn("h-7 w-7",props.class)'
+    <SidebarMenu
+        class='flex justify-center'
+        :class='{ "items-center" : !open }'
+    >
+        <SidebarMenuItem>
+            <!--  Open  -->
+            <template v-if='open'>
+                <SidebarMenuButton
+                    tooltip='Close Menu'
+                    class='h-10'
+                    @click='toggleSidebar'
                 >
-                    <PanelLeft />
-                    <span class='sr-only'>Toggle Menu</span>
-                </NuxtIcon>
-            </div>
-        </SidebarMenuButton>
-    </template>
-    
-    <!--  Close  -->
-    <template v-else>
-        <SidebarMenuButton
-            tooltip='Open Menu'
-            size="lg"
-            class=''
-            @click='toggleSidebar'
-        >
-            <NuxtIcon
-                name='stash:burger-arrow-right-duotone'
-                data-sidebar='trigger'
-                data-slot='sidebar-trigger'
-                :class='cn("h-7 w-7",props.class)'
-                
-            >
-                <PanelLeft />
-                <span class='sr-only'>Toggle Menu</span>
-            </NuxtIcon>
-        </SidebarMenuButton>
-    </template>
+                    <div class='flex w-full'>
+                        <NuxtIcon
+                            name='stash:burger-arrow-left-duotone'
+                            data-sidebar='trigger'
+                            data-slot='sidebar-trigger'
+                            :class='cn("h-7 w-7",props.class)'
+                        >
+                            <PanelLeft />
+                            <span class='sr-only'>Toggle Menu</span>
+                        </NuxtIcon>
+                    </div>
+                </SidebarMenuButton>
+            </template>
+            
+            <!--  Close  -->
+            <template v-else>
+                <SidebarMenuButton
+                    tooltip='Open Menu'
+                    size="lg"
+                    class=''
+                    @click='toggleSidebar'
+                >
+                    <NuxtIcon
+                        name='stash:burger-arrow-right-duotone'
+                        data-sidebar='trigger'
+                        data-slot='sidebar-trigger'
+                        :class='cn("h-7 w-7",props.class)'
+                    
+                    >
+                        <PanelLeft />
+                        <span class='sr-only'>Toggle Menu</span>
+                    </NuxtIcon>
+                </SidebarMenuButton>
+            </template>
+        </SidebarMenuItem>
+    </SidebarMenu>
 </template>
