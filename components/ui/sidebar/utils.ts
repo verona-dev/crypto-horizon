262a907e58cce70f89911f2411438d2a9ed3d6a1
verona-dev <veronadev@tuta.io>
2025-08-27 18:27:36 +0200

    CR-49 [FEATURE] create new navigation sidebar reduce width

--- components/ui/sidebar/utils.ts
+++ components/ui/sidebar/utils.ts
@@ -3,7 +3,7 @@ import { createContext } from "reka-ui"
 
 export const SIDEBAR_COOKIE_NAME = "sidebar_state"
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-export const SIDEBAR_WIDTH = "26rem"
+export const SIDEBAR_WIDTH = "24rem"
 export const SIDEBAR_WIDTH_MOBILE = "20rem"
 export const SIDEBAR_WIDTH_ICON = "3rem"
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b"
