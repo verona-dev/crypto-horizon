eee435c99ccbe0da1e3215974c7766bd356a7293
verona-dev <veronadev@tuta.io>
2025-08-27 15:31:42 +0200

    CR-49 [FEATURE] create new navigation create /defi/platforms

---
+++ pages/defi/platforms.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='defi-platforms custom-flex flex-col gap-6'>
+        <h1 class='text-pink-500'>Defi Platforms</h1>
+        <h6>Coming Soon...</h6>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .defi-platforms {}
+</style>
