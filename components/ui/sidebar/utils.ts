c7c8eaf56406ccd6334d55914fcc0ad26bcddf1b
verona-dev <veronadev@tuta.io>
2025-08-28 19:02:12 +0200

    CR-49 [FEATURE] create new navigation sidebar change keyboard shortcut

--- components/ui/sidebar/utils.ts
+++ components/ui/sidebar/utils.ts
@@ -6,7 +6,7 @@ export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 export const SIDEBAR_WIDTH = "24rem"
 export const SIDEBAR_WIDTH_MOBILE = "20rem"
 export const SIDEBAR_WIDTH_ICON = "4rem"
-export const SIDEBAR_KEYBOARD_SHORTCUT = "b"
+export const SIDEBAR_KEYBOARD_SHORTCUT = "s"
 
 export const [useSidebar, provideSidebarContext] = createContext<{
   state: ComputedRef<"expanded" | "collapsed">
