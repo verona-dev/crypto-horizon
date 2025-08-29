79c0b7e335d44adc0ba162b1ae5f7409e55dadf2
verona-dev <veronadev@tuta.io>
2025-08-29 17:54:21 +0200

    CR-49 [FEATURE] create new navigation sidebar main-nav cleanup closed bar

--- components/NavMain.vue
+++ components/NavMain.vue
@@ -71,6 +71,7 @@
                                 <ChevronRight class='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
                             </SidebarMenuButton>
                         </CollapsibleTrigger>
+                        
                         <CollapsibleContent>
                             <SidebarMenuSub class='pl-5'>
                                 <SidebarMenuSubItem v-for='subItem in item.items' :key='subItem.title'>
@@ -102,31 +103,13 @@
                 <!--  Close -->
                 <template v-else>
                     <NuxtLink :to='item.url'>
-                        <SidebarMenuItem v-if='item.items.length > 1'>
-                            <CollapsibleTrigger as-child>
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger>
                                 <SidebarMenuButton :is-active='item.isActive' :tooltip='item.title'>
                                     <component :is='item.icon' v-if='item.icon' />
                                     <span>{{ item.title }}</span>
-                                    <ChevronRight class='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
                                 </SidebarMenuButton>
                             </CollapsibleTrigger>
-                            <CollapsibleContent>
-                                <SidebarMenuSub class='pl-5'>
-                                    <SidebarMenuSubItem v-for='subItem in item.items' :key='subItem.title' >
-                                        <SidebarMenuSubButton as-child>
-                                            <NuxtLink :to='subItem.url'>
-                                                <span>{{ subItem.title }}</span>
-                                            </NuxtLink>
-                                        </SidebarMenuSubButton>
-                                    </SidebarMenuSubItem>
-                                </SidebarMenuSub>
-                            </CollapsibleContent>
-                        </SidebarMenuItem>
-                        <SidebarMenuItem v-else>
-                            <SidebarMenuButton :is-active='item.isActive' :tooltip='item.title'>
-                                <component :is='item.icon' v-if='item.icon' />
-                                <span>{{ item.title }}</span>
-                            </SidebarMenuButton>
                         </SidebarMenuItem>
                     </NuxtLink>
                 </template>
