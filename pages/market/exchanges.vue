e8352ead2d3e06ae687741a7db91cdb8f5469916
verona-dev <veronadev@tuta.io>
2025-09-02 20:17:08 +0200

    CR-49 [FEATURE] create new navigation create market/exchanges page

---
+++ pages/market/exchanges.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='exchanges custom-flex flex-col gap-6'>
+        <h1 class='text-pink-500'>Crypto Exchanges</h1>
+        <h6>Coming Soon...</h6>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .exchanges {}
+</style>
