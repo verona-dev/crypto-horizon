0cdda64f9725a7d4aaab8a925a3be121d67a1837
verona-dev <veronadev@tuta.io>
2025-09-02 17:51:24 +0200

    CR-49 [FEATURE] create new navigation adjust pages title style

--- pages/news/[slug]/index.vue
+++ pages/news/[slug]/index.vue
@@ -11,7 +11,7 @@
         <div v-else class='w-full'>
             <Card
                 v-if='article && article.ID'
-                class='bg-background border-none gap-12 xl:gap-20 max-w-7xl xl:px-20 pt-0 pb-10 mt-10 mb-40 mx-auto'
+                class='bg-background border-none gap-12 xl:gap-20 max-w-7xl xl:px-20 pt-0 pb-10 xl:mt-10 mb-40 mx-auto'
             >
                 <!--  Header  -->
                 <CardHeader class='flex flex-col gap-12'>
