f1640c53f3a241a0200e158513a3776a5108e3e9
verona-dev <veronadev@tuta.io>
2025-09-03 19:43:36 +0200

    CR-49 [FEATURE] create new navigation adjust sidebar to accommodate mobile navbar

--- components/ui/sidebar/SidebarTrigger.vue
+++ components/ui/sidebar/SidebarTrigger.vue
@@ -15,22 +15,23 @@
 <template>
     <SidebarMenuButton
         v-if='isMobile'
-        class='
-            mobile-nav flex items-center justify-between rounded-none
-            h-20 p-6
-            border border-b-sidebar-ring
-        '
-        @click='toggleSidebar'
+        class=' mobile-nav flex items-center h-20 p-6 border border-b-sidebar-ring rounded-none'
+        :class='isMobile ? "gap-8" : "justify-between"'
     >
         <NuxtIcon
             name='radix-icons:hamburger-menu'
             data-sidebar='trigger'
             data-slot='sidebar-trigger'
-            :class='cn("h-10 w-10",props.class)'
+            :class='cn("h-7 w-7",props.class)'
+            @click='toggleSidebar'
         >
             <PanelLeft />
             <span class='sr-only'>Toggle Menu</span>
         </NuxtIcon>
+        
+        <NuxtLink to='/'>
+            <p class='great-font'>Crypto Horizon</p>
+        </NuxtLink>
     </SidebarMenuButton>
 </template>
 
