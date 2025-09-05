f50583b0f1f2c5a89021706a0b450de755779205
verona-dev <veronadev@tuta.io>
2025-09-05 19:13:08 +0200

    CR-49 [FEATURE] create new navigation adjust global layout

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class='news'>
-        <div v-if='loading' class='flex items-center h-[75vh]'>
+    <section class='news gap-24'>
+        <h1 class='page-title'>Latest News</h1>
+        
+        <div v-if='loading' class='flex items-center'>
             <MazSpinner
                 color='secondary'
                 size='3em'
@@ -8,18 +10,14 @@
             <h4 class='ml-4 mb-0'>Loading...</h4>
         </div>
         
-        <div v-else class='flex flex-col items-center my-8 gap-24'>
-            <h1 class='page-title'>Latest News</h1>
-            
-            <div v-if='articles' class='flex flex-wrap justify-center gap-24'>
-                <NewsCard
-                    v-for='article in articles'
-                    :key='article.ID'
-                    :article='article'
-                />
-            </div>
+        <div v-else class='flex flex-wrap justify-center gap-24'>
+            <NewsCard
+                v-for='article in articles'
+                :key='article.ID'
+                :article='article'
+            />
         </div>
-    </div>
+    </section>
 </template>
 
 <script setup>
