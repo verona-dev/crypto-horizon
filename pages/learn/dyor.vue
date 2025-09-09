775fc1518140ece285c936b169361cd6060a1fc4
verona-dev <veronadev@tuta.io>
2025-09-09 13:54:00 +0200

    CR-59 [LAYOUT] refactor /learn/dyor pass the title to layout

--- pages/learn/dyor.vue
+++ pages/learn/dyor.vue
@@ -1,12 +1,13 @@
 <template>
     <div class='dyor custom-flex flex-col gap-6'>
-        <h1 class='page-title text-pink-500'>Dyor</h1>
         <h6>Coming Soon...</h6>
     </div>
 </template>
 
 <script setup>
-
+    definePageMeta({
+        title: 'Dyor'
+    });
 </script>
 
 <style scoped>
