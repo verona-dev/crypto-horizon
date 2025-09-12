c37ccdde2501b62707970db43e3b975dc1d3cb1b
verona-dev <veronadev@tuta.io>
2025-09-12 14:11:56 +0200

    CR-59 [LAYOUT] refactor adjust welcome layout

--- layouts/welcome.vue
+++ layouts/welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class='flex my-0 min-h-screen justify-center'>
+    <main class='flex my-0 min-h-screen items-center justify-center'>
         <slot />
     </main>
 </template>
