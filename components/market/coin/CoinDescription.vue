966469eb287d14c6d9a0b1b6930585552e7fb518
verona-dev <veronadev@tuta.io>
2025-09-21 19:20:06 +0200

    CR-66 [POLISH] minor changes coin-description join all content into 1 alert component

--- components/market/coin/CoinDescription.vue
+++ components/market/coin/CoinDescription.vue
@@ -1,52 +1,54 @@
 <template>
     <div v-if='coingecko' class='flex flex-col gap-12'>
-        <Alert
-            v-if='description'
-            class='bg-background flex flex-col gap-8 p-12'
-        >
-            <AlertTitle class='flex items-center gap-2'>
-                <NuxtIcon
-                    name='mdi:about-variant'
-                    class='block text-foreground'
-                    size='30'
-                />
+        <Alert class='bg-background flex flex-col gap-32 p-16'>
+            <div
+                v-if='description'
+                class='description flex flex-col gap-8'
+            >
+                <AlertTitle class='flex items-center gap-2'>
+                    <NuxtIcon
+                        name='mdi:about-variant'
+                        class='block text-foreground'
+                        size='30'
+                    />
+                    
+                    <h6 class='flex items-center gap-2'>
+                        What is {{ coingecko.name }}
+                        <span class='mt-0.5'>&#40;{{ coin.symbol }}&#41;</span>
+                    </h6>
+                </AlertTitle>
                 
-                <h6 class='flex items-center gap-2'>
-                    What is {{ coingecko.name }}
-                    <span class='mt-0.5'>&#40;{{ coin.symbol }}&#41;</span>
-                </h6>
-            </AlertTitle>
+                <AlertDescription>
+                    <p class='text-sm text-foreground'>{{ description }}</p>
+                </AlertDescription>
+            </div>
             
-            <AlertDescription>
-                <p class='text-sm text-foreground'>{{ description }}</p>
-            </AlertDescription>
-        </Alert>
-        
-        <!--  Categories  -->
-        <Alert
-            v-if='coingecko.categories'
-            class='bg-background flex flex-col gap-8 p-12'
-        >
-            <AlertTitle class='flex items-center gap-2'>
-                <NuxtIcon
-                    name='iconoir:hashtag'
-                    class='block text-foreground'
-                    size='24'
-                />
-                <h6>Categories</h6>
-            </AlertTitle>
-            
-            <div>
-                <MazBadge
-                    v-for='category in coingecko.categories'
-                    :key='category'
-                    color='white'
-                    class='m-2'
-                    outline
-                    pastel
-                >
-                    {{ category }}
-                </MazBadge>
+            <!--  Categories  -->
+            <div
+                v-if='coingecko.categories'
+                class='categories flex flex-col gap-8'
+            >
+                <AlertTitle class='flex items-center gap-2'>
+                    <NuxtIcon
+                        name='iconoir:hashtag'
+                        class='block text-foreground'
+                        size='24'
+                    />
+                    <h6>Categories</h6>
+                </AlertTitle>
+                
+                <div>
+                    <MazBadge
+                        v-for='category in coingecko.categories'
+                        :key='category'
+                        color='white'
+                        class='m-2'
+                        outline
+                        pastel
+                    >
+                        {{ category }}
+                    </MazBadge>
+                </div>
             </div>
         </Alert>
     </div>
