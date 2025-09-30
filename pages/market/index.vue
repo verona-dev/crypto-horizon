47aa1f6c1ca9cf193e33f98705608f1de0bd4a92
verona-dev <veronadev@tuta.io>
2025-09-30 20:55:38 +0200

    CR-72 [MARKET] Global market /market layout cleanup

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -1,34 +1,7 @@
 <template>
-    <div class='market'>
-        <!--  Hero Image  -->
-        <!--
-        <div class='hero-container w-screen'>
-            <NuxtImg
-                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1757866406/GettyImages-471296532-c-db7bc08_edtsqo.webp'
-                class='w-full h-full rotate-180'
-                :custom='true'
-                v-slot='{ src, isLoaded, imgAttrs }'
-                preload
-            >****
-                <img
-                    v-if='isLoaded'
-                    v-bind='imgAttrs'
-                    :src='src'
-                    alt='alt'
-                >
-                
-                <Skeleton
-                    v-else
-                    class='w-full h-full'
-                />
-            </NuxtImg>
-        </div>
-        -->
-        
-        <div class='flex flex-col items-center justify-center'>
-            <GlobalMarket />
-            <CoinsTable />
-        </div>
+    <div class='market flex flex-col items-center justify-center'>
+        <GlobalMarket />
+        <CoinsTable />
     </div>
 </template>
 
