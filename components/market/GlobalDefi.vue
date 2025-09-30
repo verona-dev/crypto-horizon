4a20bf86d94037f808360ff38643b04fb3d93c89
verona-dev <veronadev@tuta.io>
2025-09-30 23:22:00 +0200

    CR-72 [MARKET] Global market global-defi style

--- components/market/GlobalDefi.vue
+++ components/market/GlobalDefi.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class=''>
+    <Card class='!w-fit bg-accent-foreground flex flex-col gap-4 shadow-2xl p-6' v-if='globalDefi'>
         this is global Defi
-    </div>
+    </Card>
 </template>
 
 <script setup>
-
+    import { storeToRefs } from 'pinia';
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    const MarketStore = useMarketStore();
+    
+    const { globalDefi } = storeToRefs(MarketStore);
 </script>
-
-<style scoped>
-
-</style>
