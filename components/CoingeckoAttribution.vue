fc030ca0a35012b2deade3d97a86a4f35bae4d0b
verona-dev <veronadev@tuta.io>
2025-10-01 20:30:43 +0200

    CR-48 [FEATURE] add Coingecko attribution finalise coingecko-attribution style

--- components/CoingeckoAttribution.vue
+++ components/CoingeckoAttribution.vue
@@ -1,39 +1,37 @@
 <template>
-    <Alert class='w-fit coingecko-attribution flex flex-col gap-32 p-16'>
-        
-        <AlertTitle class='flex items-center gap-2'>
-            <h6 class='flex items-center gap-2'>
-                Powered by
-                <NuxtLink
-                    to='https://www.coingecko.com/en/api/'
-                    target='_blank'
-                    class='underline'
-                    external
+    <Alert class='w-full flex items-center justify-center p-8 rounded-none'>
+        <NuxtLink
+            to='https://www.coingecko.com/en/api/'
+            target='_blank'
+            class='flex items-center gap-3 hover:underline'
+            external
+        >
+            <h6>Data powered by</h6>
+            
+            <NuxtImg
+                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1759342218/CGAPI-Lockup-1_ysyezi.svg'
+                alt='coingecko logo'
+                v-slot='{ src, isLoaded, imgAttrs, alt }'
+                width='250'
+                preload
+            >
+                <img
+                    v-if='isLoaded'
+                    v-bind='imgAttrs'
+                    :src='src'
+                    :alt='alt'
                 >
-                </NuxtLink>
                 
-                <NuxtImg
-                    src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1759342218/CGAPI-Lockup-1_ysyezi.svg'
+                <Skeleton
+                    v-else
+                    class='w-[250px] rounded-lg'
                 />
-                
-                <NuxtIcon
-                    name='iconoir:open-new-window'
-                    size='50'
-                />
-            </h6>
-        </AlertTitle>
-        
-        <AlertDescription>
-            <p>This project uses the free Demo API plan from CoinGecko. We thank CoinGecko for providing high-quality cryptocurrency market data. Consider upgrading to a paid plan for enhanced features and higher rate limits.</p>
-        </AlertDescription>
+            </NuxtImg>
+        </NuxtLink>
     </Alert>
 </template>
 
 <script setup>
-    import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+    import { Alert } from '@/components/ui/alert';
     import { Skeleton } from '~/components/ui/skeleton/index.js';
 </script>
-
-<style scoped>
-
-</style>
