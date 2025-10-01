3362a488bb461e208088fc733182cbcb1d6592ed
verona-dev <veronadev@tuta.io>
2025-10-01 15:01:16 +0200

    CR-72 [MARKET] Global market global-defi add top-coin and trading volume

--- components/market/GlobalDefi.vue
+++ components/market/GlobalDefi.vue
@@ -22,6 +22,21 @@
             <span>{{ eth_market_cap }}</span>
         </CardContent>
         
+        <CardContent>
+            Top Coin:
+            <span>{{ top_coin_name }}</span>
+        </CardContent>
+        
+        <CardContent>
+            Top Coin Dominance:
+            <span>{{ top_coin_defi_dominance }}</span>
+        </CardContent>
+        
+        <CardContent>
+            Trading Volume (24h):
+            <span>{{ trading_volume_24h }}</span>
+        </CardContent>
+        
     </Card>
 </template>
 
@@ -45,6 +60,13 @@
         style: 'percent', decimals: 2
     }));
     const eth_market_cap = computed(() => formatNumber(globalDefi.value?.eth_market_cap, {
+        compact: true, decimals: 2
+    }));
+    const top_coin_name = computed(() => globalDefi.value?.top_coin_name);
+    const top_coin_defi_dominance = computed(() => formatNumber(globalDefi.value?.top_coin_defi_dominance, {
+        style: 'percent', decimals: 2
+    }));
+    const trading_volume_24h = computed(() => formatNumber(globalDefi.value?.trading_volume_24h, {
         compact: true, decimals: 2
     }));
 </script>
