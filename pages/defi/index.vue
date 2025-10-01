7467a3e3ffd3976bf249b56ca5b4e3f202f1b045
verona-dev <veronadev@tuta.io>
2025-10-01 15:08:27 +0200

    CR-72 [MARKET] Global market move global defi data to /defi

--- pages/defi/index.vue
+++ pages/defi/index.vue
@@ -1,10 +1,19 @@
 <template>
     <div class='defi'>
-        <h6>Coming Soon...</h6>
+        <GlobalDefi />
     </div>
 </template>
 
 <script setup>
+    import GlobalDefi from '~/components/market/GlobalDefi.vue';
+    
+    import { storeToRefs } from 'pinia';
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    const MarketStore = useMarketStore();
+    const { getCoingeckoGlobalMarket } = MarketStore;
+    
+    onMounted(() => getCoingeckoGlobalMarket());
+    
     definePageMeta({
         title: 'Defi',
     });
