1c738c98f25658c3d2de439699eac37fa5f809b9
verona-dev <veronadev@tuta.io>
2025-10-02 15:01:18 +0200

    CR-51 [MARKET] add fear and greed index /market refactor and remove passing props, use store instead

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -2,32 +2,20 @@
     <div class='market flex flex-col items-center justify-center gap-16'>
         <GlobalMarket />
         
-        <GlobalMarketDominance
-            v-if='mcap_dominance'
-            :mcap-dominance='mcap_dominance'
-            :updated-at='updated_at'
-        />
+        <GlobalMarketDominance />
         
         <CoinsTable />
     </div>
 </template>
 
 <script setup>
-    import dayjs from 'dayjs';
-    import relativeTime from 'dayjs/plugin/relativeTime';
-    dayjs.extend(relativeTime);
     import GlobalMarket from '~/components/market/GlobalMarket.vue';
     import CoinsTable from '~/components/market/CoinsTable.vue';
     import GlobalMarketDominance from '~/components/market/GlobalMarketDominance.vue';
     
-    import { storeToRefs } from 'pinia';
     import { useMarketStore } from '~/stores/MarketStore.js';
     const MarketStore = useMarketStore();
-    
-    const { globalMarket } = storeToRefs(MarketStore);
     const { getCoingeckoGlobalMarket } = MarketStore;
-    const mcap_dominance = computed(() => globalMarket.value?.market_cap_percentage);
-    const updated_at = computed(() => dayjs.unix(globalMarket.value?.updated_at).format('MMM D YYYY, HH:mm[h]'));
     
     onMounted(() => getCoingeckoGlobalMarket());
 </script>
