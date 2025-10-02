21e6442b34055bcbabde48536a9f13d8e6db2894
verona-dev <veronadev@tuta.io>
2025-10-02 16:04:28 +0200

    CR-51 [MARKET] add fear and greed index create global-market-fear-greed component

---
+++ components/market/GlobalMarketFearGreed.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-if='fear_and_greed' class='fear-and-greed'>
+        {{ fear_and_greed }}
+    </div>
+</template>
+
+<script setup>
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    const MarketStore = useMarketStore();
+    
+    const { getCmcFearAndGreed } = MarketStore;
+    
+    const fear_and_greed = toRef(MarketStore.globalMarket, 'fearAndGreed');
+    
+    onMounted(() => getCmcFearAndGreed());
+</script>
