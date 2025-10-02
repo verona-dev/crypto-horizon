9358bd76fba70777e9b9bf5e7b7f681411924434
verona-dev <veronadev@tuta.io>
2025-10-02 15:47:57 +0200

    CR-51 [MARKET] add fear and greed index create nuxt server api for Cmc (CoinMarketCap)

---
+++ server/api/cmc/[...route].js
@@ -0,0 +1,42 @@
+import { createError, getQuery } from 'h3';
+
+export default defineCachedEventHandler(async (event) => {
+    const { cmc_api_key } = useRuntimeConfig().public;
+    
+    const route = event.context.params?.route || '';
+    const queryEvent = getQuery(event);
+    const query = new URLSearchParams(queryEvent).toString();
+    const apiUrl = `https://pro-api.coinmarketcap.com/${route}?${query}`;
+    
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-CMC_PRO_API_KEY': cmc_api_key,
+            },
+            redirect: 'follow',
+        });
+        
+        if(!response.ok) {
+            throw createError({
+                statusCode: response.status,
+                statusMessage: response.statusText,
+            })
+        }
+        
+        const json = await response.json().data;
+        return json;
+        
+    } catch(error) {
+        if (error.statusCode) {
+            throw error;
+        } else {
+            throw createError({
+                statusCode: 500,
+                statusMessage: 'Failed to fetch Cmc data',
+            });
+        }
+    }
+}, { maxAge: 60 * 60 });
