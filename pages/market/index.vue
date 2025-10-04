ee181c645df8d456002ec8bd71b566a75664cf47
verona-dev <veronadev@tuta.io>
2025-10-04 14:20:43 +0200

    CR-51 [MARKET] add fear and greed index market-fear-greed move fetch to /market

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -2,6 +2,8 @@
     <div class='market flex flex-col items-center justify-center gap-16'>
         <GlobalMarket />
         
+        <GlobalMarketFearGreed v-if='fearAndGreed'/>
+        
         <GlobalMarketDominance />
         
         <CoinsTable />
@@ -12,10 +14,16 @@
     import GlobalMarket from '~/components/market/GlobalMarket.vue';
     import CoinsTable from '~/components/market/CoinsTable.vue';
     import GlobalMarketDominance from '~/components/market/GlobalMarketDominance.vue';
+    import GlobalMarketFearGreed from '~/components/market/GlobalMarketFearGreed.vue';
     
+    import { storeToRefs } from 'pinia';
     import { useMarketStore } from '~/stores/MarketStore.js';
     const MarketStore = useMarketStore();
-    const { getCoingeckoGlobalMarket } = MarketStore;
+    const { getCoingeckoGlobalMarket, getCmcFearAndGreed } = MarketStore;
+    const { fearAndGreed } = storeToRefs(MarketStore);
     
-    onMounted(() => getCoingeckoGlobalMarket());
+    onMounted(() => {
+         getCoingeckoGlobalMarket();
+         getCmcFearAndGreed();
+    });
 </script>
