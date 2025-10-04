eaa7cabd528de441a3364ab2a2ce5b121b6fc718
verona-dev <veronadev@tuta.io>
2025-10-04 22:43:32 +0200

    CR-51 [MARKET] add fear and greed index market rename global-market

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='market flex flex-col items-center justify-center gap-16'>
-        <GlobalMarket />
+        <GlobalMarketBar />
         
         <GlobalMarketFearGreed v-if='fearAndGreed'/>
         
@@ -11,7 +11,7 @@
 </template>
 
 <script setup>
-    import GlobalMarket from '~/components/market/GlobalMarket.vue';
+    import GlobalMarketBar from '~/components/market/GlobalMarketBar.vue';
     import CoinsTable from '~/components/market/CoinsTable.vue';
     import GlobalMarketDominance from '~/components/market/GlobalMarketDominance.vue';
     import GlobalMarketFearGreed from '~/components/market/GlobalMarketFearGreed.vue';
