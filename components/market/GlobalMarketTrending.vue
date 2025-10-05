d03b388de5f12dccdbf35ead7ee746646d6bffc5
verona-dev <veronadev@tuta.io>
2025-10-05 13:52:33 +0200

    CR-73 [MARKET] Trending coins create global-market-trending component

---
+++ components/market/GlobalMarketTrending.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if='globalTrending' class=''>
+        trending data
+    </div>
+</template>
+
+<script setup>
+    import { storeToRefs } from 'pinia';
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    const MarketStore = useMarketStore();
+    
+    const { getCoingeckoGlobalTrending } = MarketStore;
+    
+    const { globalTrending } = storeToRefs(MarketStore);
+    
+    onMounted(() => getCoingeckoGlobalTrending());
+</script>
