17a1ea94a55a670c2194c62c216163e213120c5b
verona-dev <veronadev@tuta.io>
2025-10-06 01:45:47 +0200

    CR-73 [MARKET] Trending coins global-market-trending adjust styles

--- components/market/GlobalMarketTrendingCoinCard.vue
+++ components/market/GlobalMarketTrendingCoinCard.vue
@@ -1,83 +1,100 @@
 <template>
     <Card
         v-if='coin'
-        class='!w-60 bg-background shadow-2xl p-6'
+        class='!w-64 bg-background/50 shadow-2xl !py-6 !px-0 !border-warning/10'
     >
-        <HoverCard :openDelay='200'>
-           <HoverCardTrigger class='flex flex-col items-center gap-4'>
-               <h5>{{ rank }}</h5>
-               
-               <NuxtImg
-                   :src='image'
-                   alt='trending coin logo'
-                   class='w-12 h-12 rounded-md rounded-b-none'
-                   :custom='true'
-                   v-slot='{ src, isLoaded, imgAttrs, alt }'
-                   preload
-               >
-                   <img
-                       v-if='isLoaded'
-                       v-bind='imgAttrs'
-                       :src='src'
-                       :alt='alt'
-                   >
-                   
-                   <Skeleton
-                       v-else
-                       class='w-12 h-12'
-                   />
-               </NuxtImg>
-               
-               <div class='flex items-center gap-2'>
-                   <p class='text-xs'>#{{ mcap_rank }}</p>
-                   <p class='text-xs'>{{ name }}</p>
-                   <p class='text-xs'>{{ symbol }}</p>
-               </div>
-               
-               <h5>{{ price_label }}</h5>
-               
-               <div class='flex items-center gap-1'>
-                   <NuxtIcon
-                       :name='getTrendIcon(price_change_percentage_1d)'
-                       size='20'
-                       :class='getTextColorClass(price_change_percentage_1d)'
-                   />
-                   
-                   <p
-                       :class='[getTextColorClass(price_change_percentage_1d)]'
-                       class='flex items-center'
-                   >
-                       {{ price_change_percentage_1d_label }}
-                   </p>
-               </div>
-               
-               <!--  Sparkline  -->
-               <NuxtImg
-                   :src='sparkline'
-                   alt='trending coin logo'
-                   class='w-full'
-                   :custom='true'
-                   v-slot='{ src, isLoaded, imgAttrs, alt }'
-                   preload
-               >
-                   <img
-                       v-if='isLoaded'
-                       v-bind='imgAttrs'
-                       :src='src'
-                       :alt='alt'
-                   >
-                   
-                   <Skeleton
-                       v-else
-                       class='h-[100px] w-full'
-                   />
-               </NuxtImg>
-           </HoverCardTrigger>
+        <CardHeader class='flex items-center justify-center gap-4 border-b border-warning/10 px-0 !pb-4'>
+            <Badge class='select-none text-sm text-warning border-warning/10' variant='outline'>{{ rank }}</Badge>
             
-            <HoverCardContent v-if='content'>
-                <p class='text-xs'>{{ content }}</p>
-            </HoverCardContent>
-        </HoverCard>
+            <h6 class='text-warning'>{{ name }}</h6>
+        </CardHeader>
+        
+        <CardContent class='flex flex-col items-center justify-center gap-8 px-0'>
+            <div class='flex flex-col items-center gap-3'>
+                <NuxtImg
+                    :src='image'
+                    alt='trending coin logo'
+                    class='w-20 h-20 rounded-md rounded-b-none select-none'
+                    :custom='true'
+                    v-slot='{ src, isLoaded, imgAttrs, alt }'
+                    preload
+                >
+                    <img
+                        v-if='isLoaded'
+                        v-bind='imgAttrs'
+                        :src='src'
+                        :alt='alt'
+                    >
+                    
+                    <Skeleton
+                        v-else
+                        class='w-20 h-20'
+                    />
+                </NuxtImg>
+                
+                <div class='flex items-center gap-2 text-muted-foreground'>
+                    <p>#{{ mcap_rank }}</p>
+                    
+                    <p>{{ symbol }}</p>
+                    
+                    <HoverCard :openDelay='200' v-if='description'>
+                        <HoverCardTrigger class='info-icon'>
+                            <NuxtIcon
+                                name='radix-icons:info-circled'
+                                size='15'
+                                class='flex text-muted-foreground'
+                            />
+                        </HoverCardTrigger>
+                        
+                        <HoverCardContent class='!p-6 flex flex-col !gap-3'>
+                            <p class='text-xs'>{{ title }}</p>
+                            <p class='text-xs'>{{ description }}</p>
+                        </HoverCardContent>
+                    </HoverCard>
+                </div>
+            </div>
+            
+            <div class='flex flex-col items-center gap-1'>
+                <h4>{{ price_label }}</h4>
+                
+                <div class='flex items-center gap-1 mr-2'>
+                    <NuxtIcon
+                        :name='getTrendIcon(price_change_percentage_1d)'
+                        size='15'
+                        :class='getTextColorClass(price_change_percentage_1d)'
+                    />
+                    
+                    <span
+                        :class='[getTextColorClass(price_change_percentage_1d)]'
+                        class='flex items-center'
+                    >
+                    {{ price_change_percentage_1d_label }}
+                </span>
+                </div>
+            </div>
+            
+            <!--  Sparkline  -->
+            <NuxtImg
+                :src='sparkline'
+                alt='trending coin logo'
+                class='w-2/3'
+                :custom='true'
+                v-slot='{ src, isLoaded, imgAttrs, alt }'
+                preload
+            >
+                <img
+                    v-if='isLoaded'
+                    v-bind='imgAttrs'
+                    :src='src'
+                    :alt='alt'
+                >
+                
+                <Skeleton
+                    v-else
+                    class='h-[100px] w-full'
+                />
+            </NuxtImg>
+        </CardContent>
     </Card>
 </template>
 
@@ -86,6 +103,7 @@
     import { formatNumber } from '~/utils/formatUtils.js';
     import { getTrendIcon, getTextColorClass } from '~/utils/styleUtils.js';
     import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card/index.ts';
+    import { Badge } from '@/components/ui/badge';
     
     const props = defineProps({
         coin: {
@@ -102,12 +120,12 @@
     const symbol = coin.value?.symbol;
     const price = coin.value?.data?.price;
     const price_label = formatNumber(price, {
-        maximumFractionDigits: 4,
     });
     const price_change_percentage_1d = coin.value?.data?.price_change_percentage_24h?.usd;
     const price_change_percentage_1d_label = formatNumber(price_change_percentage_1d, {
         style: 'percent', compact: true, decimals: 2,
     });
     const sparkline = coin.value?.data?.sparkline;
-    const content = coin.value?.data?.content?.description;
+    const title = coin.value?.data?.content?.title;
+    const description = coin.value?.data?.content?.description;
 </script>
