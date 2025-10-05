aa2857c953a7ffa94977fd1aaf0e7c2c2c5114c5
verona-dev <veronadev@tuta.io>
2025-10-05 14:45:18 +0200

    CR-73 [MARKET] Trending coins create global-market-trending-coin-card

--- components/market/GlobalMarketTrending.vue
+++ components/market/GlobalMarketTrending.vue
@@ -1,49 +1,22 @@
 <template>
-    <Alert class='!bg-background w-4/5 !shadow-none animate-fadeIn'>
+    <Alert class='!bg-background flex w-4/5 !shadow-none animate-fadeIn'>
         <Skeleton
             v-if='!trending_coins'
             class='w-full h-full'
         />
         
-        <div v-else class='flex'>
-            <Card
+        <div v-else class='flex items-start gap-12'>
+            <GlobalMarketTrendingCoinCard
                 v-for='coin in trending_coins'
                 :key='coin.item.id'
-                class='!w-fit bg-accent-foreground flex items-center gap-4 shadow-2xl p-6'
-            >
-                <NuxtImg
-                    :src='coin.item?.large'
-                    alt='trending coin logo'
-                    class='rounded-md rounded-b-none'
-                    :custom='true'
-                    v-slot='{ src, isLoaded, imgAttrs, alt }'
-                    preload
-                >
-                    <img
-                        v-if='isLoaded'
-                        v-bind='imgAttrs'
-                        :src='src'
-                        :alt='alt'
-                    >
-                    
-                    <Skeleton
-                        v-else
-                        class='h-[100px] w-full'
-                    />
-                </NuxtImg>
-                
-                <div class='flex items-center gap-2'>
-                    <p class='text-xs'>#{{ coin.item?.market_cap_rank }}</p>
-                    <p class='text-xs'>{{ coin.item?.name }}</p>
-                </div>
-                
-                <p class='text-xs'>{{ coin.item?.data?.price }}</p>
-            </Card>
+                :coin='coin.item'
+            />
         </div>
     </Alert>
 </template>
 
 <script setup>
+    import GlobalMarketTrendingCoinCard from '~/components/market/GlobalMarketTrendingCoinCard.vue';
     import { Skeleton } from '~/components/ui/skeleton/index.js';
     
     import { storeToRefs } from 'pinia';
