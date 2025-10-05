749a00d3e10c0ff0ef05bf4c141b04b58f146ebc
verona-dev <veronadev@tuta.io>
2025-10-05 22:04:46 +0200

    CR-73 [MARKET] Trending coins global-market-trending carousel working version

--- components/market/GlobalMarketTrending.vue
+++ components/market/GlobalMarketTrending.vue
@@ -1,23 +1,41 @@
 <template>
-    <Alert class='!bg-background flex w-4/5 !border-none !shadow-none animate-fadeIn'>
+    <Alert class='!bg-background flex w-4/ h-96 !border-none !shadow-none animate-fadeIn'>
         <Skeleton
             v-if='!trending_coins'
             class='w-full h-full'
         />
         
-        <div v-else class='flex items-start gap-12'>
-            <GlobalMarketTrendingCoinCard
-                v-for='coin in trending_coins'
-                :key='coin.item.id'
-                :coin='coin.item'
-            />
+        <div v-else class='flex items-start justify-center mx-auto gap-12 max-w-[800px]'>
+            <Carousel
+                class="relative w-full"
+                :plugins="[plugin]"
+                @mouseenter="plugin.stop"
+                @mouseleave="[plugin.reset(), plugin.play()];"
+            >
+                <CarouselContent>
+                    <CarouselItem
+                        v-for='coin in trending_coins'
+                        :key='coin.item.id'
+                        class='pl-1 md:basis-1/2 lg:basis-1/3'
+                    >
+                        <GlobalMarketTrendingCoinCard
+                            :coin='coin.item'
+                        />
+                    </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </div>
+
     </Alert>
 </template>
 
 <script setup>
     import GlobalMarketTrendingCoinCard from '~/components/market/GlobalMarketTrendingCoinCard.vue';
     import { Skeleton } from '~/components/ui/skeleton/index.js';
+    import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+    import Autoplay from 'embla-carousel-autoplay';
     
     import { storeToRefs } from 'pinia';
     import { useMarketStore } from '~/stores/MarketStore.js';
@@ -28,6 +46,12 @@
     const { globalTrending } = storeToRefs(MarketStore);
     const trending_coins = computed(() => globalTrending.value?.coins);
     console.log(trending_coins.value);
+    
+    const plugin = Autoplay({
+        delay: 2000,
+        stopOnMouseEnter: true,
+        stopOnInteraction: false,
+    })
     
     // onMounted(() => getCoingeckoGlobalTrending());
 </script>
