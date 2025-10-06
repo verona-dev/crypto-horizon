e9225c190f8e55c796c16217620fac0bc5757fcb
verona-dev <veronadev@tuta.io>
2025-10-06 18:37:02 +0200

    CR-73 [MARKET] Trending coins move market-trending to its own page /trending

---
+++ pages/market/trending.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class='trending flex flex-col items-center justify-center gap-20'>
+        <MarketTrending />
+    </div>
+</template>
+
+<script setup>
+    import MarketTrending from '~/components/market/trending/MarketTrending.vue';
+</script>
