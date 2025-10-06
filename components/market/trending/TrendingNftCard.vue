1e07d79bc2f96978d36a7242293e7add2e891d17
verona-dev <veronadev@tuta.io>
2025-10-06 19:43:53 +0200

    CR-73 [MARKET] Trending coins trending-nft-card extract data

--- components/market/trending/TrendingNftCard.vue
+++ components/market/trending/TrendingNftCard.vue
@@ -1,10 +1,12 @@
 <template>
-    <div v-if='nft' class=''>
+    <div v-if='nft'>
         {{ nft.name }}
     </div>
 </template>
 
 <script setup>
+    import { formatNumber } from '~/utils/formatUtils.js';
+    
     const props = defineProps({
        nft: {
            type: Object,
@@ -12,4 +14,23 @@
     });
     
     const { nft } = toRefs(props);
+    
+    const slug = nft.value?.id;
+    const name = nft.value?.name;
+    const symbol = nft.value?.symbol;
+    const image = nft.value?.thumb;
+    const price = nft.value?.floor_price_in_native_currency;
+    const price_label = formatNumber(price);
+    const currency = nft.value?.native_currency;
+    
+    const flooe_price = nft.value?.data?.floor_price;
+    const price_change_percentage_1d = nft.value?.data?.floor_price_in_usd_24h_percentage_change;
+    const price_change_percentage_1d_label = formatNumber(price_change_percentage_1d, {
+        style: 'percent', compact: true, decimals: 2,
+    });
+    const volume_24h = nft.value?.data?.h24_volume;
+    const average_sale_price_24h = nft.value?.data?.h24_average_sale_price;
+    const sparkline = nft.value?.data?.sparkline;
+    const title = nft.value?.data?.content?.title;
+    const description = nft.value?.data?.content?.description;
 </script>
