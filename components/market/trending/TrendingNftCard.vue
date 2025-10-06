c08fa3a93f8e7a6cf6709e2a1ca979195cbcfdd6
verona-dev <veronadev@tuta.io>
2025-10-06 19:03:20 +0200

    CR-73 [MARKET] Trending coins create trending-nft card component

---
+++ components/market/trending/TrendingNftCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if='nft' class=''>
+        {{ nft.name }}
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+       nft: {
+           type: Object,
+       }
+    });
+    
+    const { nft } = toRefs(props);
+</script>
