173b708df32c346efa4c9c5722ddfc6edd46c3b6
verona-dev <veronadev@tuta.io>
2025-10-08 23:45:56 +0200

    CR-84 [STYLE] reset Shadcn update Sidebar component and usage

--- components/ui/sidebar/SidebarGroupLabel.vue
+++ components/ui/sidebar/SidebarGroupLabel.vue
@@ -11,12 +11,11 @@ const props = defineProps<PrimitiveProps & {
 
 <template>
   <Primitive
-    data-slot="sidebar-group-label"
     data-sidebar="group-label"
     :as="as"
     :as-child="asChild"
     :class="cn(
-      'text-sidebar-foreground/70 ring-sidebar-ring flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium outline-hidden transition-[margin,opacity] duration-700 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0',
+      'duration-200 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opacity] ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0',
       'group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0',
       props.class)"
   >
