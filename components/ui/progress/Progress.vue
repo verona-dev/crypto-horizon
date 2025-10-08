531824494442a79c8b6e47dfd3e1ffde3dcd723c
verona-dev <veronadev@tuta.io>
2025-10-08 22:47:48 +0200

    CR-84 [STYLE] reset Shadcn update Progress component and usage

--- components/ui/progress/Progress.vue
+++ components/ui/progress/Progress.vue
@@ -1,45 +1,37 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { reactiveOmit } from '@vueuse/core'
+import type { ProgressRootProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { reactiveOmit } from "@vueuse/core"
 import {
   ProgressIndicator,
   ProgressRoot,
-  type ProgressRootProps,
-} from 'reka-ui'
-import { cn } from '@/lib/utils'
+
+} from "reka-ui"
+import { cn } from "@/lib/utils"
 
 const props = withDefaults(
-  defineProps<ProgressRootProps & {
-      class?: HTMLAttributes['class'],
-      indicatorColor?: string,
-  }>(),
+  defineProps<ProgressRootProps & { class?: HTMLAttributes["class"] }>(),
   {
     modelValue: 0,
-    indicatorColor: '',
   },
 )
 
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, "class")
 </script>
 
 <template>
   <ProgressRoot
-    data-slot="progress"
     v-bind="delegatedProps"
     :class="
       cn(
-        'progress-root bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
+        'relative h-2 w-full overflow-hidden rounded-full bg-primary/20',
         props.class,
       )
     "
   >
     <ProgressIndicator
-      data-slot="progress-indicator"
-      class="progress-indicator bg-primary h-full w-full flex-1 transition-all"
-      :style="{
-        background: props.indicatorColor,
-        transform: `translateX(-${100 - (props.modelValue ?? 0)}%)`
-    }"
+      class="h-full w-full flex-1 bg-primary transition-all"
+      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
     />
   </ProgressRoot>
 </template>
