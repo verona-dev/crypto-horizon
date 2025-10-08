4c999cd3dc94755c70451bb61b46d2fa9de63a71
verona-dev <veronadev@tuta.io>
2025-10-08 23:45:29 +0200

    CR-84 [STYLE] reset Shadcn update Sidebar component and usage

--- components/ui/sidebar/SidebarMenuSubButton.vue
+++ components/ui/sidebar/SidebarMenuSubButton.vue
@@ -16,14 +16,13 @@ const props = withDefaults(defineProps<PrimitiveProps & {
 
 <template>
   <Primitive
-    data-slot="sidebar-menu-sub-button"
     data-sidebar="menu-sub-button"
     :as="as"
     :as-child="asChild"
     :data-size="size"
     :data-active="isActive"
     :class="cn(
-      'text-sidebar-foreground ring-sidebar-primary hover:bg-sidebar-primary hover:text-sidebar-accent-foreground active:bg-sidebar-primary active:text-sidebar-accent-foreground [&>svg]:text-sidebar-accent-foreground flex h-10 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 outline-hidden focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
+      'flex h-10 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground',
       'data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground',
       size === 'sm' && 'text-xs',
       size === 'md' && 'text-sm',
