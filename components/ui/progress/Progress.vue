950da7e3f043f3ae69d00352063e30604ee55fb9
verona-dev <veronadev@tuta.io>
2025-10-09 18:26:19 +0200

    CR-84 [STYLE] reset Shadcn fix broken shadcn Progress component

--- components/ui/progress/Progress.vue
+++ components/ui/progress/Progress.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import type { ProgressRootProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import { reactiveOmit } from "@vueuse/core"
 import {
-  ProgressIndicator,
-  ProgressRoot,
-
-} from "reka-ui"
+    ProgressIndicator,
+    ProgressRoot,
+    type ProgressRootProps,
+} from 'reka-ui'
 import { cn } from "@/lib/utils"
 
 const props = withDefaults(
-  defineProps<ProgressRootProps & { class?: HTMLAttributes["class"] }>(),
-  {
+    defineProps<ProgressRootProps & {
+        class?: HTMLAttributes['class'],
+        indicatorColor?: string,
+    }>(),  {
     modelValue: 0,
+        indicatorColor: '',
   },
 )
 
@@ -29,9 +31,12 @@ const delegatedProps = reactiveOmit(props, "class")
       )
     "
   >
-    <ProgressIndicator
-      class="h-full w-full flex-1 bg-primary transition-all"
-      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
-    />
+      <ProgressIndicator
+          class="progress-indicator bg-primary h-full w-full flex-1 transition-all"
+          :style="{
+        background: props.indicatorColor,
+        transform: `translateX(-${100 - (props.modelValue ?? 0)}%)`
+    }"
+      />
   </ProgressRoot>
 </template>
