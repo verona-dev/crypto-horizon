5a68bd44e2ddbe31c4a3193bf56c8a247e8c9ee7
verona-dev <veronadev@tuta.io>
2025-10-09 03:09:49 +0200

    CR-84 [STYLE] reset Shadcn coin-overview fix tables

--- components/ui/table/TableCell.vue
+++ components/ui/table/TableCell.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   <td
     :class="
       cn(
-        'p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5',
+        'p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
         props.class,
       )
     "
