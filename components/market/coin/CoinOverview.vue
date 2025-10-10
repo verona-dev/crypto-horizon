9a6c0ab039dc1c8d66ce0bc8525f2e7a8e62665e
verona-dev <veronadev@tuta.io>
2025-10-10 23:13:08 +0200

    CR-84 [STYLE] reset Shadcn coin-overview remove price-performance data

--- components/market/coin/CoinOverview.vue
+++ components/market/coin/CoinOverview.vue
@@ -329,40 +329,6 @@
             </TableBody>
         </Table>
     </div>
-    
-    <!--  Price Performance 24h -->
-    <div class='flex flex-col gap-8'>
-        <!--  Price Title  -->
-        <div class='flex items-center gap-3'>
-            <NuxtIcon
-                name='iconoir:arc-3d-center-point'
-                size='27'
-            />
-            <h3>Price Performance</h3>
-        </div>
-        
-        <!--  Price Data -->
-        <div class='flex flex-col gap-2'>
-            <div class='flex justify-between items-end text-sm'>
-                <div class='flex flex-col gap-1'>
-                    <span class='text-muted-custom'>Low</span>
-                    <p>{{ low_24h_label }}</p>
-                </div>
-                
-                <span class='text-muted-custom'>24h Range</span>
-                
-                <div class='flex flex-col gap-1'>
-                    <span class='text-muted-custom'>High</span>
-                    <p>{{ high_24h_label }}</p>
-                </div>
-            </div>
-            
-            <Progress
-                v-model='progress'
-                :indicatorColor='progress_color'
-            />
-        </div>
-    </div>
 </template>
 
 <script setup>
@@ -372,7 +338,6 @@
     dayjs.extend(relativeTime, { rounding: Math.floor });
     import { Table, TableBody, TableCell, TableRow } from '~/components/ui/table/index.ts';
     import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card/index.ts';
-    import { Progress } from '~/components/ui/progress';
     import { formatNumber } from '~/utils/formatUtils.js';
     
     const props = defineProps({
@@ -385,37 +350,6 @@
     const { coin } = toRefs(props);
     const livecoinwatch = toRef(coin.value.livecoinwatch);
     const coingecko = toRef(coin.value.coingecko);
-    
-    const current_price = coingecko.value?.market_data?.current_price?.usd;
-    const low_24h = coingecko.value?.market_data?.low_24h?.usd;
-    const low_24h_computed = computed(() => {
-        // Coingecko Api has delays in updating the low24h value therefore the current price can temporarily be under the low24h
-        if(current_price < low_24h) return current_price;
-        return low_24h;
-    });
-    const low_24h_label = formatNumber(low_24h_computed.value, {
-        maximumFractionDigits: 4,
-    });
-    const high_24h = coingecko.value?.market_data?.high_24h?.usd;
-    const high_24h_computed = computed(() => {
-        // Coingecko Api has delays in updating the high24h value therefore the current price can temporarily be above the high24h
-        if(current_price > high_24h) return current_price;
-        return high_24h;
-    });
-    const high_24h_label = formatNumber(high_24h_computed.value, {
-        maximumFractionDigits: 4,
-    });
-    
-    const progress = computed(() => {
-        const range = high_24h_computed.value - low_24h_computed.value;
-        if (range < 0.005) return 99; // for stablecoins, since range can be as low as .001
-        return ((current_price - low_24h_computed.value) / range) * 100;
-    });
-    const progress_color = computed(() => {
-        if(progress.value < 25) return '#E32D2D';
-        else if(progress.value < 50) return 'linear-gradient(90deg, #E32D2D 75%, #EBAA28 100%)';
-        return 'linear-gradient(90deg, #E32D2D 0%, #EBAA28 50%, #1AC914 100%)';
-    });
     
     const genesis_date = coingecko.value?.genesis_date;
     const generis_date_from_now = dayjs(genesis_date).fromNow();
