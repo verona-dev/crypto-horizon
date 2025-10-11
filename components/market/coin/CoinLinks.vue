ab0d68d45ede313ee82dbb22e83e23723996dc53
verona-dev <veronadev@tuta.io>
2025-10-11 02:47:51 +0200

    CR-84 [STYLE] reset Shadcn coin-links adjust new layout

--- components/market/coin/CoinLinks.vue
+++ components/market/coin/CoinLinks.vue
@@ -1,102 +1,113 @@
 <template>
-    <section class='coin-links flex flex-wrap items-start gap-16'>
-        <!-- Websites -->
-        <div class='websites'>
-            <h5 class='mb-4'>Websites</h5>
-            
-            <NuxtLink
-                v-if='homepage'
-                :to='homepage'
-                external
-                target='_blank'
-                class='flex items-center mb-3 link-item'
-            >
-                <NuxtIcon name='radix-icons:home' size='25' class='w-[50px]' />
-                <p>Homepage</p>
-            </NuxtLink>
-            
-            <NuxtLink
-                v-if='whitepaper'
-                :to='whitepaper'
-                external
-                target='_blank'
-                class='flex items-center mb-3 link-item'
-            >
-                <NuxtIcon name='iconoir:open-book' size='25' class='w-[50px]' />
-                <p>Whitepaper</p>
-            </NuxtLink>
-            
-            <div v-for='(link, name) in official_forum' :key='name'>
-                <NuxtLink
-                    v-if='link'
-                    :to='link'
-                    external
-                    target='_blank'
-                    class='flex items-center mb-3 link-item'
-                >
-                    <NuxtIcon name='radix-icons:chat-bubble' size='25' class='w-[50px]' />
-                    <p>Official Forum</p>
-                </NuxtLink>
-            </div>
+    <Card class='coin-links bg-background flex flex-col flex-wrap gap-10 p-10 w-full'>
+        <!--  Links Title  -->
+        <div class='flex items-center gap-3'>
+            <NuxtIcon
+                name='iconoir:link'
+                size='20'
+            />
+            <h4>Links</h4>
         </div>
         
-        <!-- Chat -->
-        <div v-if='chats.length' class='chat'>
-            <h5 class='mb-4'>Chat</h5>
-            
-            <div v-for='chat in chats' :key='chat'>
+        <div class='flex flex-col gap-12'>
+            <!-- Websites -->
+            <div v-if='websites' class='websites flex flex-col gap-4'>
+                <h5>Websites</h5>
+                
                 <NuxtLink
-                    v-if='chat'
-                    :to='chat'
+                    v-if='homepage'
+                    :to='homepage'
                     external
                     target='_blank'
-                    class='flex items-center mb-3 link-item'
+                    class='flex items-center link-item gap-3'
                 >
-                    <NuxtIcon name='iconoir:chat-lines' size='25' class='w-[50px]' />
-                    <p class='text-sm'>{{ chat }}</p>
+                    <NuxtIcon name='radix-icons:home' size='20' />
+                    <p class='text-sm'>Homepage</p>
                 </NuxtLink>
-            </div>
-        </div>
-        
-        <!-- Community -->
-        <div v-if='socials && Object.keys(socials).length' class='community'>
-            <h5 class='mb-4'>Community</h5>
-            
-            <div
-                v-for='(link, name) in socials'
-                :key='name'
-            >
+                
                 <NuxtLink
-                    v-if='link && name'
-                    :to='link'
+                    v-if='whitepaper'
+                    :to='whitepaper'
                     external
                     target='_blank'
-                    class='flex items-center py-2 link-item'
+                    class='flex items-center link-item gap-2'
                 >
-                    <NuxtIcon :name='`fa-brands:${name}`' size='25' class='w-[50px]' />
-                    <p>{{ name }}</p>
+                    <NuxtIcon name='iconoir:open-book' size='25' />
+                    <p class='text-sm'>Whitepaper</p>
                 </NuxtLink>
+                
+                <div v-for='(link, name) in official_forum' :key='name'>
+                    <NuxtLink
+                        v-if='link'
+                        :to='link'
+                        external
+                        target='_blank'
+                        class='flex items-center link-item gap-2'
+                    >
+                        <NuxtIcon name='radix-icons:chat-bubble' size='25' />
+                        <p class='text-sm'>Official Forum</p>
+                    </NuxtLink>
+                </div>
             </div>
-        </div>
-        
-        <!-- Github -->
-        <div v-if='github.length' class='github'>
-            <h5 class='mb-4'>Github</h5>
             
-            <div v-for='link in github' :key='link'>
-                <NuxtLink
-                    v-if='link'
-                    :to='link'
-                    external
-                    target='_blank'
-                    class='flex items-center py-2 link-item'
+            <!-- Chat -->
+            <div v-if='chats.length' class='chat flex flex-col gap-4'>
+                <h5>Chat</h5>
+                
+                <div v-for='chat in chats' :key='chat'>
+                    <NuxtLink
+                        v-if='chat'
+                        :to='chat'
+                        external
+                        target='_blank'
+                        class='flex items-center link-item gap-3'
+                    >
+                        <NuxtIcon name='iconoir:chat-lines' size='20' />
+                        <p class='text-sm'>{{ chat }}</p>
+                    </NuxtLink>
+                </div>
+            </div>
+            
+            <!-- Community -->
+            <div v-if='socials && Object.keys(socials).length' class='community flex flex-col gap-4'>
+                <h5>Community</h5>
+                
+                <div
+                    v-for='(link, name) in socials'
+                    :key='name'
                 >
-                    <NuxtIcon name='fa-brands:github' size='25' class='w-[50px]' />
-                    <p class='text-sm'>{{ link }}</p>
-                </NuxtLink>
+                    <NuxtLink
+                        v-if='link && name'
+                        :to='link'
+                        external
+                        target='_blank'
+                        class='flex items-center py-2 link-item gap-3'
+                    >
+                        <NuxtIcon :name='`fa-brands:${name}`' size='20' />
+                        <p class='text-sm'>{{ name }}</p>
+                    </NuxtLink>
+                </div>
+            </div>
+            
+            <!-- Github -->
+            <div v-if='github.length' class='github flex flex-col gap-4'>
+                <h5>Github</h5>
+                
+                <div v-for='link in github' :key='link'>
+                    <NuxtLink
+                        v-if='link'
+                        :to='link'
+                        external
+                        target='_blank'
+                        class='flex items-center link-item gap-3'
+                    >
+                        <NuxtIcon name='fa-brands:github' size='20' />
+                        <p class='text-sm'>{{ link }}</p>
+                    </NuxtLink>
+                </div>
             </div>
         </div>
-    </section>
+    </Card>
 </template>
 
 <script setup>
@@ -112,6 +123,7 @@
     });
     
     const { livecoinwatchLinks, coingeckoLinks } = toRefs(props);
+    const websites = computed(() => homepage.value || whitepaper.value || official_forum.value);
     const homepage = computed(() => coingeckoLinks.value?.homepage[0]);
     const whitepaper = computed(() => coingeckoLinks.value?.whitepaper);
     const official_forum = computed(() => coingeckoLinks.value?.official_forum_url);
@@ -123,7 +135,7 @@
 <style scoped>
     .coin-links {
         .link-item {
-            color: rgb(156 163 175 / var(--maz-tw-text-opacity, 1));
+            color: var(--ring);
             
             &:hover {
                 color: var(--foreground);
