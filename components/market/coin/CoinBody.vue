a6152cbc5956a3bfb749809270eb97e0b65781ac
verona-dev <veronadev@tuta.io>
2025-10-11 22:33:08 +0200

    CR-84 [STYLE] reset Shadcn coin-body adjust new layout

--- components/market/coin/CoinBody.vue
+++ components/market/coin/CoinBody.vue
@@ -1,20 +1,31 @@
 <template>
-    <CardContent class='coin-body grid grid-cols-1 xl:grid-cols-[1fr_3fr] gap-6'>
-        <!-- Left -->
-        <div class='flex flex-col space-y-6'>
-            <CoinOverview :coin='coin' />
-            <CoinInsights :coin='coin' />
-        </div>
-        
-        <!-- Right -->
-        <div class='flex flex-col space-y-6'>
-            <CoinChart :coin='coin' />
-            <CoinLinks
-                :livecoinwatchLinks='livecoinwatch.links'
-                :coingeckoLinks='coingecko.links'
-            />
-            <CoinDelta :marketData='coingecko.market_data' />
-            <CoinDescription :coin='coin' />
+    <CardContent class='coin-body'>
+        <div class='grid grid-cols-1 xl:grid-cols-6 gap-6'>
+            <!-- Left -->
+            <div class='lg:col-span-2 xl:col-span-2 space-y-6'>
+                <!-- Overview -->
+                <CoinOverview :coin='coin' />
+                
+                <!-- Insights -->
+                <CoinInsights :coin='coin' />
+            </div>
+            <!-- Right -->
+            <div class='xl:col-span-4 space-y-6'>
+                <!-- Chart -->
+                <CoinChart :coin='coin' />
+                
+                <!-- Delta -->
+                <CoinDelta :marketData='coingecko.market_data' />
+                
+                <!-- Description -->
+                <CoinDescription :coin='coin' />
+                
+                <!-- Links -->
+                <CoinLinks
+                    :livecoinwatchLinks='livecoinwatch.links'
+                    :coingeckoLinks='coingecko.links'
+                />
+            </div>
         </div>
     </CardContent>
 </template>
