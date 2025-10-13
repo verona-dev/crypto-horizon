88d50be32a7e04140cd4bdf5d3c9fc303c4cc43d
verona-dev <veronadev@tuta.io>
2025-10-13 23:44:12 +0200

    CR-86 [OPTIMISATIONS] improvements create LoadingSpinner component

---
+++ app/components/LoadingSpinner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if='loading' class='flex items-center gap-4 mb-40'>
+        <Spinner class='size-10 text-secondary' />
+        
+        <h4>Loading...</h4>
+    </div>
+</template>
+
+<script setup>
+    // MarketStore
+    import { storeToRefs } from 'pinia';
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    import { Spinner } from '@/components/ui/spinner/index.js';
+    const MarketStore = useMarketStore();
+    
+    // State
+    const { loading } = storeToRefs(MarketStore);
+</script>
