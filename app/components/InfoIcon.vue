1e45e9f0ca2a79790596237c185962e8a8c6d204
verona-dev <veronadev@tuta.io>
2025-10-14 14:15:21 +0200

    CR-86 [OPTIMISATIONS] improvements create InfoIcon component

---
+++ app/components/InfoIcon.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtIcon
+        name='radix-icons:info-circled'
+        :size='size'
+        class='info-icon mt-0.5 flex bg-muted-foreground/50'
+    />
+</template>
+
+<script setup>
+    const props = defineProps({
+        size: {
+            type: [ String, Number ],
+            default: 15,
+        },
+    });
+</script>
