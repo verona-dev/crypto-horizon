f3b906e4019d64e06d576c35382c6a88166ea06a
verona-dev <veronadev@tuta.io>
2025-10-17 01:44:14 +0200

    CR-79 [WELCOME] Add animations added Meteors to /market

--- app/pages/market/index.vue
+++ app/pages/market/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div class='market page flex flex-col items-center justify-center gap-20'>
+        <Meteors :count="30" />
+        
         <h1 class='page-title'>Crypto Market</h1>
         
         <div class='flex flex-col xl:flex-row items-center justify-center gap-12 w-screen max-w-7xl'>
