624084b43ed63fa5814195c665703be83f92b6b4
verona-dev <veronadev@tuta.io>
2025-10-17 01:44:37 +0200

    CR-79 [WELCOME] Add animations /index added ParticlesBg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,15 @@
 <template>
     <div class='flex flex-col items-center gap-8 2xl:gap-32 px-20 pb-20 2xl:pb-10 relative'>
-        <Meteors :count="30" />
+<!--        <Meteors :count="30" />-->
+        
+        <ParticlesBg
+            class='absolute inset-0'
+            :quantity='100'
+            :ease='100'
+            color='#FFF'
+            :staticity='10'
+            refresh
+        />
         
         <h1 class='mt-20 mb-20 2xl:mb-0 select-none break-words flex flex-col gap-3'>
             <span>Crypto</span>
@@ -37,28 +46,28 @@
     const welcomeItems = [
         {
             title: 'Markets',
-            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1755195826/cyberpunk-bitcoin-illustration-2_u6fytd.webp',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/c_scale,w_800/v1755195826/cyberpunk-bitcoin-illustration-2_u6fytd.webp',
             alt: 'market image',
             to: '/market',
             anchor: 90,
         },
         {
             title: 'News',
-            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1753545835/ai-generated-8517436_1920_vk0pnk.png',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/c_scale,w_800/v1753545835/ai-generated-8517436_1920_vk0pnk.png',
             alt: 'news image',
             to: '/news',
             anchor: 45,
         },
         {
             title: 'Defi',
-            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1755195953/cyberpunk-bitcoin-illustration_jejcnb.webp',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/c_scale,w_800/v1755195953/cyberpunk-bitcoin-illustration_jejcnb.webp',
             alt: 'defi image',
             to: '/defi',
             anchor: 90,
         },
         {
             title: 'Learn',
-            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1755194748/andre-francois-mckenzie-uw3VwkDHiMI-unsplash_hs9t15.jpg',
+            src: 'https://res.cloudinary.com/dgcyv1ehi/image/upload/c_scale,w_800/v1755194748/andre-francois-mckenzie-uw3VwkDHiMI-unsplash_hs9t15.jpg',
             alt: 'learn image',
             to: '/learn',
             anchor: 45,
