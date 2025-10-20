40c03e064ff580cdaf62c808482b762878f47940
verona-dev <veronadev@tuta.io>
2025-10-20 02:07:57 +0200

    CR-81 [DESIGN] Create dark mode adjust color-mode component

--- app/components/ColorMode.vue
+++ app/components/ColorMode.vue
@@ -1,36 +1,39 @@
 <template>
-    <Button
-        @click='toggleMode'
-        variant='outline'
-        class='!bg-transparent border-none rounded-none h-16'
-    >
-            <NuxtIcon
-                :name='icon'
-                class='h-5 w-5'
-            />
-    </Button>
+    <SidebarMenu class=''>
+        <SidebarMenuItem class='h-16 flex items-center justify-center'>
+            <SidebarMenuButton
+                @click='toggleMode'
+                variant='outline'
+                :tooltip='active_mode.label'
+                class='bg-sidebar h-full flex items-center justify-center'
+                :class='{ "rounded-none" :  open }'
+            >
+                <NuxtIcon
+                    :name='active_mode.icon'
+                    class='h-5 w-5'
+                />
+            </SidebarMenuButton>
+        </SidebarMenuItem>
+    </SidebarMenu>
+
 </template>
 
 <script setup>
     import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '~/components/ui/sidebar';
     import { useSidebar } from './ui/sidebar/utils';
-    const { open, toggleSidebar, isMobile } = useSidebar()
-    
-    import { Button } from '@/components/ui/button';
+    const { open } = useSidebar();
     
     const colorMode = useColorMode();
-    const light = computed(() => colorMode.value === 'light');
+    const active_mode = computed(() => color_modes.value.find(mode => mode.value === colorMode.value) || color_modes.value[0]);
+    
+    const color_modes = computed(() => [
+        { value: 'light', label: 'Toggle Dark mode', icon: 'radix-icons:moon' },
+        { value: 'dark', label: 'Toggle Light mode', icon: 'radix-icons:sun' },
+    ]);
     
     const toggleMode = () => {
-        const current_index = color_modes.findIndex(mode => mode.value === colorMode.preference);
-        const next_index = (current_index + 1) % color_modes.length;
-        colorMode.preference = color_modes[next_index].value;
+        const current_index = color_modes.value?.findIndex(mode => mode.value === colorMode.preference);
+        const next_index = (current_index + 1) % color_modes.value?.length;
+        colorMode.preference = color_modes.value[next_index]?.value;
     };
-    
-    const color_modes = [
-        { value: 'light', label: 'Light', icon: 'radix-icons:sun' },
-        { value: 'dark', label: 'Dark', icon: 'radix-icons:moon' },
-    ];
-    
-    const icon = computed(() => light.value ? 'radix-icons:moon' : 'radix-icons:sun');
 </script>
