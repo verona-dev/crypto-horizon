1599a4326c172ff4c3affe53b0e19e987c307a3b
verona-dev <veronadev@tuta.io>
2025-10-20 23:33:30 +0200

    CR-81 [DESIGN] Create dark mode add global ul style rules

--- app/components/ColorMode.vue
+++ app/components/ColorMode.vue
@@ -1,11 +1,12 @@
 <template>
     <SidebarMenu>
         <SidebarMenuItem class='h-16 flex items-center justify-center'>
-            <SidebarMenuButton
+            <Button
+                ref='toggleRef'
                 @click='toggleMode'
                 variant='outline'
                 :tooltip='active_mode.label'
-                class='bg-sidebar h-full flex items-center justify-center'
+                class='bg-sidebar w-full h-full flex items-center justify-center'
                 :class='{ "rounded-none" :  open }'
             >
                 <NuxtIcon
@@ -18,7 +19,7 @@
                             "dark-animation": active_mode.value === "dark"
                         }'
                 />
-            </SidebarMenuButton>
+            </Button>
         </SidebarMenuItem>
     </SidebarMenu>
 
@@ -27,9 +28,12 @@
 <script setup>
     import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '~/components/ui/sidebar';
     import { useSidebar } from './ui/sidebar/utils';
-    const { open } = useSidebar();
+    import { Button } from '~/components/ui/button';
     
+    const { open } = useSidebar();
     const colorMode = useColorMode();
+    const toggleRef = ref(null);
+    
     const active_mode = computed(() => color_modes.value.find(mode => mode.value === colorMode.value) || color_modes.value[0]);
     
     const color_modes = computed(() => [
@@ -37,10 +41,46 @@
         { value: 'dark', label: 'Toggle Light mode', icon: 'radix-icons:sun' },
     ]);
     
-    const toggleMode = () => {
-        const current_index = color_modes.value?.findIndex(mode => mode.value === colorMode.preference);
-        const next_index = (current_index + 1) % color_modes.value?.length;
-        colorMode.preference = color_modes.value[next_index]?.value;
+    const toggleMode = async () => {
+        if (!document.startViewTransition || !toggleRef.value) {
+            const current_index = color_modes.value.findIndex(
+                (mode) => mode.value === colorMode.preference
+            );
+            const next_index = (current_index + 1) % color_modes.value.length;
+            colorMode.preference = color_modes.value[next_index].value;
+            return;
+        }
+        
+        const { left, top } = toggleRef.value.$el.getBoundingClientRect();
+        const x = left + toggleRef.value.$el.offsetWidth / 2;
+        const y = top + toggleRef.value.$el.offsetHeight / 2;
+        
+        const transition = document.startViewTransition(async () => {
+            // delay added because of Firefox glitch
+            await new Promise(resolve => setTimeout(resolve, 100));
+            
+            const current_index = color_modes.value.findIndex(
+                (mode) => mode.value === colorMode.preference
+            );
+            const next_index = (current_index + 1) % color_modes.value.length;
+            colorMode.preference = color_modes.value[next_index].value;
+        });
+        
+        await transition.ready;
+        
+        document.documentElement.animate(
+            {
+                clipPath: [
+                    `circle(0px at ${x}px ${y}px)`,
+                    `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`,
+                ],
+            },
+            {
+                duration: 500,
+                easing: 'ease-in-out',
+                pseudoElement: '::view-transition-new(root)',
+            }
+        );
     };
 </script>
 
@@ -50,6 +90,7 @@
     }
     
     .dark-animation {
+        
         animation: dark-animation 0.25s forwards;
     }
     
