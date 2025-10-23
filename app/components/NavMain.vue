2e68f40d6306f6c0c7e2ffbb14a828092e4bd8ff
verona-dev <veronadev@tuta.io>
2025-10-23 12:48:20 +0200

    CR-95 [SIDEBAR] new design sidebar nav-main add tooltip

--- app/components/NavMain.vue
+++ app/components/NavMain.vue
@@ -8,7 +8,7 @@
         items: {
             title: string
             url: string
-            icon?: LucideIcon
+            icon: string
             isActive?: boolean
             items?: {
                 title: string
@@ -23,23 +23,22 @@
 <template>
     <SidebarGroup>
         <SidebarGroupLabel>Menu</SidebarGroupLabel>
+        
         <SidebarMenu
             :class='[
                 isMobile ? "gap-8 items-stretch" : "gap-3",
                 { "flex items-center": !open && !isMobile },
             ]'
         >
-            <!--            <NavLogo />-->
-            
-            <Collapsible
-                v-for='item in items'
-                :key='item.title'
-                as-child
-                :default-open='item.isActive'
-                class='group/collapsible'
-            >
-                <!--  Open Desktop + Mobile  -->
-                <template v-if='open || isMobile'>
+            <!--  Open Desktop + Mobile  -->
+            <template v-if='open || isMobile'>
+                <Collapsible
+                    v-for='item in items'
+                    :key='item.title'
+                    as-child
+                    :default-open='item.isActive'
+                    class='group/collapsible'
+                >
                     <!--  Home -->
                     <template v-if='item.url === "/"'>
                         <NuxtLink :to='item.url'>
@@ -96,43 +95,44 @@
                             </CollapsibleContent>
                         </SidebarMenuItem>
                     </template>
-                </template>
-                
-                <!--  Close Desktop -->
-                <template v-else-if='!open && !isMobile'>
-                    <NuxtLink :to='item.url'>
-                        <!--  Home -->
-                        <template v-if='item.url === "/"'>
-                            <SidebarMenuItem>
-                                <CollapsibleTrigger>
-                                    <SidebarMenuButton
-                                        tooltip='Launch Pad'
-                                        class='hover:bg-transparent active:bg-transparent focus:bg-transparent'
-                                        size='lg'
-                                    >
-                                        <NuxtIcon
-                                            name='streamline-ultimate-color:space-rocket-earth'
-                                            class='h-8 w-8'
-                                        />
-                                    </SidebarMenuButton>
-                                </CollapsibleTrigger>
-                            </SidebarMenuItem>
-                        </template>
-                        
-                        <!--  Other routes -->
-                        <template v-else>
-                            <SidebarMenuItem>
-                                <CollapsibleTrigger :class='{ "w-full" : isMobile }'>
-                                    <SidebarMenuButton :is-active='item.isActive' :tooltip='item.title'>
-                                        <component :is='item.icon' v-if='item.icon' />
-                                        <span>{{ item.title }}</span>
-                                    </SidebarMenuButton>
-                                </CollapsibleTrigger>
-                            </SidebarMenuItem>
-                        </template>
-                    </NuxtLink>
-                </template>
-            </Collapsible>
+                </Collapsible>
+            </template>
+            
+            <!--  Close Desktop -->
+            <template v-if='!open && !isMobile'>
+                <NuxtLink
+                    v-for='item in items'
+                    :key='item.title'
+                    :to='item.url'
+                    class='my-2'
+                >
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            :tooltip='item.title'
+                            class='peer/menu-button w-full items-center gap-2 overflow-hidden rounded-md !p-8 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground h-12 text-sm group-data-[collapsible=icon]:!p-0 flex justify-center'
+                            size='lg'
+                        >
+                            <NuxtIcon
+                                v-if='item.icon'
+                                :name='item.icon'
+                                class='h-10 w-10 dark:bg-green-shamrock'
+                            />
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </NuxtLink>
+            </template>
         </SidebarMenu>
     </SidebarGroup>
 </template>
+
+<style scoped>
+    a {
+    
+    }
+    .iconify {
+        height: 1.6rem !important;
+        width: 1.6rem !important;
+        font-size: 1.6rem !important;
+        //padding: 12px !important;
+    }
+</style>
