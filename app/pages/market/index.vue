742fab1d0643646339384ee509f5b7e8dac1e0cb
verona-dev <veronadev@tuta.io>
2025-10-25 21:38:14 +0200

    CR-94 [MARKET] Trending card add TopTrendingCoins component to /market

--- app/pages/market/index.vue
+++ app/pages/market/index.vue
@@ -4,6 +4,8 @@
         
         <h1 class='page-title'>Crypto Market</h1>
         
+        <TopTrendingCoins />
+        
         <div class='flex flex-col xl:flex-row items-center xl:items-start justify-center gap-12 w-screen max-w-7xl'>
             <MarketDominance />
             <MarketFearGreed />
@@ -16,17 +18,19 @@
 <script setup>
     import MarketFearGreed from '~/components/market/MarketFearGreed.vue';
     import MarketDominance from '~/components/market/MarketDominance.vue';
+    import TopTrendingCoins from '@/components/market/TopTrendingCoins.vue';
     import CoinsTable from '~/components/market/CoinsTable.vue';
     import { Meteors } from '@/components/ui/meteors/index.js';
     
     import { useMarketStore } from '~/stores/MarketStore.js';
     const MarketStore = useMarketStore();
-    const { getFearAndGreed } = MarketStore;
+    const { getFearAndGreed, getTrendingSearchList } = MarketStore;
     
     const colorMode = useColorMode();
     const dark_mode = computed(() => colorMode.value === 'dark');
     
     onMounted(() => {
         getFearAndGreed();
+        getTrendingSearchList();
     });
 </script>
