64480960d5d4297746c436e720e6ad024c04309e
verona-dev <veronadev@tuta.io>
2025-10-26 20:47:49 +0100

    CR-94 [MARKET] Trending card rename sidebar-trigger

--- app/components/ui/sidebar/SidebarTrigger.vue
+++ app/components/ui/sidebar/SidebarTrigger.vue
@@ -15,7 +15,7 @@
 <template>
     <SidebarMenuButton
         v-if='isMobile'
-        class=' mobile-nav bg-background flex items-center h-20 p-6 border border-b-sidebar-ring rounded-none'
+        class=' mobile-nav bg-background flex items-center h-20 p-6 rounded-none z-100'
         :class='isMobile ? "gap-8" : "justify-between"'
     >
         <NuxtIcon
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,7 +11,7 @@
                     -->
                     
                     <!--  Mobile Trigger  -->
-                    <SidebarTrigger />
+                    <MobileSidebarTrigger />
                     
                     <!--
                     <h1 v-if='title' class='page-title'>{{ title }}</h1>
@@ -33,7 +33,7 @@
     import ScrollToTop from '~/components/ScrollToTop.vue';
     import AppSidebar from "~/components/AppSidebar.vue";
     import Breadcrumb from "~/components/Breadcrumb.vue";
-    import { SidebarInset, SidebarProvider, SidebarTrigger, } from '~/components/ui/sidebar';
+    import { SidebarInset, SidebarProvider, MobileSidebarTrigger, } from '~/components/ui/sidebar';
     import { useRoute } from 'vue-router';
     const route = useRoute();
     // const title = computed(() => route.meta.title);
