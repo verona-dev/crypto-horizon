2d025c850eb5858ff1383c13527d64efd1072f56
verona-dev <veronadev@tuta.io>
2025-10-26 02:59:51 +0200

    CR-94 [MARKET] Trending card /market adjust layout

--- app/pages/market/index.vue
+++ app/pages/market/index.vue
@@ -1,26 +1,27 @@
 <template>
     <div class='market page flex flex-col items-center justify-center gap-20'>
-        <Meteors v-if='dark_mode' :count="30" />
-        
         <h1 class='page-title'>Crypto Market</h1>
         
-        <TopTrendingCoins />
         
-        <div class='flex flex-col xl:flex-row items-center xl:items-start justify-center gap-12 w-screen max-w-7xl'>
-            <MarketDominance />
+        <div class='flex flex-col flex-wrap xl:flex-row items-center xl:items-start justify-center gap-12 w-screen max-w-7xl'>
             <MarketFearGreed />
+            <TopTrendingCoins />
         </div>
         
+        <MarketDominance />
+        
         <CoinsTable />
+        
+        <Meteors v-if='dark_mode' :count='30' />
     </div>
 </template>
 
 <script setup>
     import MarketFearGreed from '~/components/market/MarketFearGreed.vue';
     import MarketDominance from '~/components/market/MarketDominance.vue';
-    import TopTrendingCoins from '@/components/market/TopTrendingCoins.vue';
+    import TopTrendingCoins from '~/components/market/TopTrendingCoins.vue';
     import CoinsTable from '~/components/market/CoinsTable.vue';
-    import { Meteors } from '@/components/ui/meteors/index.js';
+    import { Meteors } from '~/components/ui/meteors';
     
     import { useMarketStore } from '~/stores/MarketStore.js';
     const MarketStore = useMarketStore();
