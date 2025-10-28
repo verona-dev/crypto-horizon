1674740a12bd04d8b1a17eb0e66589dbdf9301c8
verona-dev <veronadev@tuta.io>
2025-10-28 02:45:04 +0100

    CR-88 [NEWS] Design change bento-grid default margins

--- app/components/ui/bento-grid/BentoGridItem.vue
+++ app/components/ui/bento-grid/BentoGridItem.vue
@@ -2,7 +2,7 @@
   <div
     :class="
       cn(
-        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
+        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-6',
         props.class,
       )
     "
@@ -13,7 +13,7 @@
       <div class="my-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
         <slot name="title" />
       </div>
-      <div class="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+      <div class="mb-4 mt-8 font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
         <slot name="description" />
       </div>
     </div>
