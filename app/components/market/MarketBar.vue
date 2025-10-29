4c6926292223bec4831d4eafbe5feb635028a4dd
verona-dev <veronadev@tuta.io>
2025-10-29 22:19:30 +0100

    CR-88 [NEWS] Design market-bar change background opacity

--- app/components/market/MarketBar.vue
+++ app/components/market/MarketBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <Card class='bg-background/50 !w-screen !h-20 p-0 !m-0 border-none z-10'>
+    <Card class='bg-background/75 !w-screen !h-20 p-0 !m-0 border-none z-10'>
         <Skeleton
             v-if='!markets'
             class='w-full h-full'
