acad7d87288b891cdda2b69e37fd6ce4cea6c0f3
verona-dev <veronadev@tuta.io>
2025-11-01 12:41:18 +0100

    CR-88 [NEWS] Design coindesk-api remove caching

--- server/api/coindesk/[...route].js
+++ server/api/coindesk/[...route].js
@@ -1,6 +1,6 @@
 import { createError, getQuery } from 'h3';
 
-export default defineCachedEventHandler(async(event) => {
+export default defineEventHandler(async(event) => {
     const { coindesk_api } = useRuntimeConfig().public;
     
     const route = Array.isArray(event.context.params.route)
@@ -34,4 +34,4 @@ export default defineCachedEventHandler(async(event) => {
             statusMessage: 'Internal Server Error',
         });
     }
-}, { maxAge: 60 * 60 * 5 });
+});
