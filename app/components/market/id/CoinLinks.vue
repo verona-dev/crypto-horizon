b19dcd3154b319a4d038e8b25b1ea1a30f0deb02
verona-dev <veronadev@tuta.io>
2025-11-08 02:45:14 +0100

    CR-97 [OPTIMISATIONS] improvements refactor icons to phosphor

--- app/components/market/id/CoinLinks.vue
+++ app/components/market/id/CoinLinks.vue
@@ -35,7 +35,7 @@
                         target='_blank'
                         class='flex items-center link-item gap-2'
                     >
-                        <NuxtIcon name='radix-icons:chat-bubble' size='25' />
+                        <NuxtIcon name='ph:chat-centered-text-light' size='22' />
                         <p class='text-sm'>Official Forum</p>
                     </NuxtLink>
                 </div>
@@ -54,39 +54,42 @@
                     target='_blank'
                     class='flex items-center link-item gap-3'
                 >
-                    <NuxtIcon name='iconoir:chat-lines' size='20' />
+                    <NuxtIcon name='ph:chat-teardrop-light' size='22' />
                     <p class='text-sm'>{{ chat }}</p>
                 </NuxtLink>
             </div>
         </CardContent>
         
         <!-- Community -->
-        <CardContent v-if='socials && Object.keys(socials).length' class='community flex flex-col gap-6'>
+        <CardContent v-if='socials && Object.keys(socials).length' class='community flex flex-col gap-8'>
             <h3>Community</h3>
             
-            <div
-                v-for='(link, name) in socials'
-                :key='name'
-                class='flex flex-col gap-4'
-            >
-                <NuxtLink
-                    v-if='link && name'
-                    :to='link'
-                    external
-                    target='_blank'
-                    class='flex items-center py-2 link-item gap-3'
+            <div class='flex flex-col gap-4'>
+                <div
+                    v-for='(link, name) in socials'
+                    :key='name'
                 >
-                    <NuxtIcon :name='`fa-brands:${name}`' size='20' />
-                    <p class='text-sm'>{{ name }}</p>
-                </NuxtLink>
+                    <NuxtLink
+                        v-if='link && name'
+                        :to='link'
+                        external
+                        target='_blank'
+                        class='flex items-center link-item gap-3'
+                    >
+                        <NuxtIcon :name='`ph:${name}-logo-light`' size='22' />
+                        <p class='text-sm'>{{ name }}</p>
+                    </NuxtLink>
+                </div>
             </div>
+            
+           
         </CardContent>
         
         <!-- Github -->
         <CardContent v-if='github.length' class='github flex flex-col gap-8'>
             <h3>Github</h3>
             
-            <div v-for='link in github' :key='link'>
+            <div v-for='link in github' :key='link' class='flex items-center link-item gap-3'>
                 <NuxtLink
                     v-if='link'
                     :to='link'
@@ -94,7 +97,7 @@
                     target='_blank'
                     class='flex items-center link-item gap-3'
                 >
-                    <NuxtIcon name='fa-brands:github' size='20' />
+                    <NuxtIcon name='ph:github-logo-light' size='22' />
                     <p class='text-sm'>{{ link }}</p>
                 </NuxtLink>
             </div>
@@ -131,7 +134,6 @@
     .coin-links {
         .link-item {
             &:hover {
-                color: var(--foreground);
                 text-decoration: underline;
             }
         }
