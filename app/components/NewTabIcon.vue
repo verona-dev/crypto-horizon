11a97cdda7c929636344e95e55543a14225039a3
verona-dev <veronadev@tuta.io>
2025-11-09 02:04:19 +0100

    CR-97 [OPTIMISATIONS] improvements create new-tab-icon component

---
+++ app/components/NewTabIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <NuxtIcon
+        name='ph:arrow-up-right'
+        :size='size'
+        class='mb-0.5'
+    />
+</template>
+
+<script setup>
+    const props = defineProps({
+        size: String,
+        default: '12',
+    });
+</script>
