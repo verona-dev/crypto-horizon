e7a3963f335d810f50901632f6ae8e274aba7f45
verona-dev <veronadev@tuta.io>
2025-11-18 22:24:04 +0100

    CR-97 [OPTIMISATIONS] improvements refactor coin-supply table

--- app/components/market/id/CoinSupplyTable.vue
+++ app/components/market/id/CoinSupplyTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if='market_data'>
+    <div v-if='market_data_computed'>
         <Table class='w-full xl:w-96'>
             <TableHeader>
                 <TableRow>
@@ -9,25 +9,22 @@
             </TableHeader>
             
             <TableBody>
-                <!--  Max Supply  -->
-                <TableRow v-if='max_supply'>
-                    <TableCell class='font-medium'>Max Supply</TableCell>
-                    <TableCell>{{ max_supply_value }}</TableCell>
-                </TableRow>
-                <!--  Total Supply  -->
-                <TableRow v-if='total_supply'>
-                    <TableCell class='font-medium'>Total Supply</TableCell>
-                    <TableCell>{{ total_supply_value }}</TableCell>
-                </TableRow>
-                <!--  Circulating Supply  -->
-                <TableRow v-if='circulating_supply'>
-                    <TableCell class='font-medium'>Circulating Supply</TableCell>
-                    <TableCell>{{ circulating_supply_value }}</TableCell>
-                </TableRow>
-                <!--  Remaining Supply  -->
-                <TableRow v-if='remaining_supply'>
-                    <TableCell class='font-medium'>Remaining Supply</TableCell>
-                    <TableCell>{{ remaining_supply_value }}</TableCell>
+                <TableRow
+                    v-for='item in market_data_computed'
+                    :key='item.key'
+                >
+                    <TableCell class='flex items-center gap-2'>
+                        {{ item.label }}
+                        
+                        <HoverCard :openDelay='200' class='flex'>
+                            <HoverCardTrigger>
+                                <InfoIcon />
+                            </HoverCardTrigger>
+                            <HoverCardContent>{{ item.description }}</HoverCardContent>
+                        </HoverCard>
+                    </TableCell>
+                    
+                    <TableCell>{{ item.value }}</TableCell>
                 </TableRow>
             </TableBody>
         </Table>
@@ -37,6 +34,9 @@
 <script setup>
     import { Table, TableBody, TableCell, TableRow, TableHeader, TableHead } from '~/components/ui/table';
     import { formatNumber } from '~/utils/formatUtils.js';
+    import glossary from '~/assets/data/market/glossary.json';
+    import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card/index.ts';
+    import InfoIcon from '@/components/InfoIcon.vue';
     
     const props = defineProps({
         coin: {
@@ -48,21 +48,63 @@
     const symbol = computed(() => coin.value?.symbol);
     const { coin } = toRefs(props);
     const market_data = computed(() => coin.value?.coingecko?.market_data);
-    const max_supply = computed(() => market_data.value?.max_supply);
-    const max_supply_value = computed(() => formatNumber(max_supply.value, {
-        style: 'decimal'
-    }));
     
+    const market_data_computed = computed(() => {
+        const items = [];
+        
+        // Max Supply
+        if(max_supply.value) {
+            items.push({
+                key: 'max_supply',
+                label: glossary.max_supply.label,
+                description: glossary.max_supply.description,
+                value: formatNumber(max_supply.value, {
+                    style: 'decimal'
+                }),
+            });
+        }
+        
+        // Total Supply
+        if(total_supply.value) {
+            items.push({
+                key: 'total_supply',
+                label: glossary.total_supply.label,
+                description: glossary.total_supply.description,
+                value: formatNumber(total_supply.value, {
+                    style: 'decimal'
+                }),
+            });
+        }
+        
+        // Circulating Supply
+        if(circulating_supply.value) {
+            items.push({
+                key: 'circulating_supply',
+                label: glossary.circulating_supply.label,
+                description: glossary.circulating_supply.description,
+                value: formatNumber(circulating_supply.value, {
+                    style: 'decimal'
+                })
+            })
+        }
+        
+        // Remaining Supply
+        if(remaining_supply.value) {
+            items.push({
+                key: 'remaining_supply',
+                label: glossary.remaining_supply.label,
+                description: glossary.remaining_supply.description,
+                value: formatNumber(remaining_supply.value, {
+                    style: 'decimal'
+                }),
+            });
+        }
+        
+        return items;
+    });
+    
+    const max_supply = computed(() => market_data.value?.max_supply);
     const total_supply = computed(() =>  market_data.value?.total_supply);
-    const total_supply_value = computed(() => formatNumber(total_supply.value, {
-        style: 'decimal'
-    }));
     const circulating_supply = computed(() => market_data.value?.circulating_supply);
-    const circulating_supply_value = computed(() => formatNumber(market_data.value?.circulating_supply, {
-        style: 'decimal'
-    }));
     const remaining_supply = computed(() => max_supply.value - total_supply.value);
-    const remaining_supply_value = computed(() => formatNumber(remaining_supply.value, {
-        style: 'decimal'
-    }));
 </script>
