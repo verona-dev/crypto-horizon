e2af670181fd2d1aa3dad49e713e1070119c4463
verona-dev <veronadev@tuta.io>
2025-11-21 23:35:04 +0100

    CR-100 [COIN] Links design coin-links code cleanup

--- app/components/market/id/CoinLinks.vue
+++ app/components/market/id/CoinLinks.vue
@@ -104,13 +104,8 @@
     });
     
     const { links } = toRefs(props);
-    // const websites = computed(() => homepage.value || whitepaper.value || official_forum.value);
-    // const homepage = computed(() => coingeckoLinks.value?.homepage[0]);
-    // const whitepaper = computed(() => coingeckoLinks.value?.whitepaper);
     // const official_forum = computed(() => coingeckoLinks.value?.official_forum_url);
     // const chats = computed(() => coingeckoLinks.value?.chat_url);
-    // const github = computed(() => coingeckoLinks.value?.repos_url?.github);
-    // const socials = computed(() => livecoinwatchLinks.value?.socials);
     
-    console.log(links.value);
+    // console.log(links.value);
 </script>
