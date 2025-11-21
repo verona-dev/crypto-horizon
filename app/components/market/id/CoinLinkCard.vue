79a08428e88ec6e5172c04c51b7395c27b293749
verona-dev <veronadev@tuta.io>
2025-11-21 22:26:12 +0100

    CR-100 [COIN] Links design coin-link-card add tooltip on action button hover

--- app/components/market/id/CoinLinkCard.vue
+++ app/components/market/id/CoinLinkCard.vue
@@ -1,48 +1,69 @@
 <template>
     <Card
         v-if='link'
-        class='!bg-background w-52 2xl:min-w-84 p-16 2xl:px-4 2xl:py-3 flex flex-col 2xl:flex-row items-center gap-6 2xl:gap-3 rounded-lg
-                hover:border-green-shamrock/75 transition-all duration-500 ease-out border-border/100 shadow-none'
+        class='!bg-background w-52 2xl:min-w-84 p-12 2xl:px-3 2xl:py-3 flex flex-col 2xl:flex-row items-center gap-6 2xl:gap-3 rounded-lg
+                hover:border-green-shamrock/75 hover:shadow-2xl transition-all duration-500 ease-out border-border/50 shadow-md'
     >
-        <div class='flex flex-col items-center gap-2 2xl:gap-3 w-full'>
+        <div class='flex flex-col 2xl:flex-row items-center gap-2 2xl:gap-3 w-full'>
             <NuxtIcon
-                :name='icon'
-                size='25'
+                :name='platform_icon'
+                size='30'
                 class='mt-0.5'
             />
             
             
             <CardTitle class='text-lg font-satoshi flex-1 capitalize'>{{ name }}</CardTitle>
-            <span v-if='social_handle' class='text-xs font-satoshi'>{{ src }}</span>
-            
+            <!--   <span v-if='social_handle' class='text-xs font-satoshi'>{{ src }}</span>   -->
+        
         </div>
         
         <div class='flex items-center gap-3 justify-self-end'>
-            <Button variant='outline' @click='onCopy(name, src)'>
-                <NuxtIcon
-                    name='ph:copy'
-                    size='15'
-                />
-            </Button>
+            <TooltipProvider :delayDuration='250'>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <Button variant='ghost' @click='onCopy(name, src)'>
+                            <NuxtIcon
+                                name='ph:copy'
+                                size='17'
+                            />
+                        </Button>
+                    </TooltipTrigger>
+                    
+                    <TooltipContent :side-offset='5'>
+                        <p class='text-xs'>Copy link</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
             
-            <NuxtLink
-                :to='src'
-                external
-                target='_blank'
-                class='link-as-button'
-            >
-                <NewTabIcon />
-            </NuxtLink>
+            <TooltipProvider :delayDuration='250'>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <NuxtLink
+                            :to='src'
+                            external
+                            target='_blank'
+                            class='ghost-link-as-button'
+                        >
+                            <NewTabIcon size='17' />
+                        </NuxtLink>
+                    </TooltipTrigger>
+                    
+                    <TooltipContent :side-offset="5">
+                        <p class='text-xs'>Open link in a new tab</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
         </div>
     </Card>
 </template>
 
 <script setup>
+    import { capitalize } from '@/utils/formatUtils.js';
     import { Button } from '@/components/ui/button/index.ts';
     import { Card, CardTitle } from '@/components/ui/card/index.ts';
     import NewTabIcon from '@/components/NewTabIcon.vue';
     import { toast } from 'vue-sonner';
-    import { capitalize } from '@/utils/formatUtils.js';
+    import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip';
     
     const props = defineProps({
         link: {
@@ -58,7 +79,7 @@
     const name = ref(link.value?.key);
     const src = ref(link.value?.value);
     const social_handle = computed(() => category.value === 'socials');
-    const icon = computed(() => {
+    const platform_icon = computed(() => {
         if(name.value === 'website') return 'ph:house-thin';
         else if(name.value === 'whitepaper') return 'ph:book-open-thin';
         else if(category.value === 'socials') return `ph:${name.value}-logo-thin`;
