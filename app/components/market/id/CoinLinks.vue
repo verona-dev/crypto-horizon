e1615c136f2baa6f51ec6fa81486e4ab7115ff22
verona-dev <veronadev@tuta.io>
2025-11-22 00:40:11 +0100

    CR-100 [COIN] Links design coin-links add github section

--- app/components/market/id/CoinLinks.vue
+++ app/components/market/id/CoinLinks.vue
@@ -1,7 +1,7 @@
 <template>
     <Card class='coin-links bg-background flex flex-col 2xl:flex-row gap-16 p-10 lg:py-20 lg:px-12 w-full'>
-        <CardContent class='flex flex-col gap-9'>
-            <!-- Websites -->
+        <CardContent class='flex flex-col gap-12'>
+            <!-- Project -->
             <h3>Project</h3>
             
             <div class='flex flex-wrap 2xl:flex-col gap-9 2xl:gap-3'>
@@ -14,7 +14,7 @@
         
         <CardContent
             v-if='links.socials'
-            class='flex flex-col gap-9'
+            class='flex flex-col gap-12'
         >
             <!-- Socials -->
             <h3>Socials</h3>
@@ -28,64 +28,28 @@
                 />
             </div>
         </CardContent>
-
-        <!--
-        <div v-for='(link, name) in official_forum' :key='name'>
-            <NuxtLink
-                v-if='link'
-                :to='link'
-                external
-                target='_blank'
-                class='flex items-center link-item gap-2'
-            >
-                <NuxtIcon name='ph:chat-centered-text-light' size='22' />
-                <p class='text-sm'>Official Forum</p>
-            </NuxtLink>
-        </div>
-        -->
         
-        <!--
-        &lt;!&ndash; Chat &ndash;&gt;
-        <CardContent v-if='chats.length' class='chat flex flex-col gap-8'>
-            <h3>Chat</h3>
-            
-            <div v-for='chat in chats' :key='chat' class='flex flex-col gap-4'>
-                <NuxtLink
-                    v-if='chat'
-                    :to='chat'
-                    external
-                    target='_blank'
-                    class='flex items-center link-item gap-3'
-                >
-                    <NuxtIcon name='ph:chat-teardrop-light' size='22' />
-                    <p class='text-sm'>{{ chat }}</p>
-                </NuxtLink>
-            </div>
-        </CardContent>
-        
-        &lt;!&ndash; Github &ndash;&gt;
-        <CardContent v-if='github.length' class='github flex flex-col gap-8'>
+        <CardContent
+            v-if='links.github'
+            class='flex flex-col gap-12'
+        >
+            <!-- Github -->
             <h3>Github</h3>
             
-            <div v-for='link in github' :key='link' class='flex items-center link-item gap-3'>
-                <NuxtLink
-                    v-if='link'
-                    :to='link'
-                    external
-                    target='_blank'
-                    class='flex items-center link-item gap-3'
-                >
-                    <NuxtIcon name='ph:github-logo-light' size='22' />
-                    <p class='text-sm'>{{ link }}</p>
-                </NuxtLink>
+            <div class='flex flex-wrap 2xl:flex-col gap-9 2xl:gap-3'>
+                <CoinLinkCard
+                    v-for='link in links.github'
+                    :key='link'
+                    :link='link'
+                    category='github'
+                />
             </div>
         </CardContent>
-        -->
     </Card>
 </template>
 
 <script setup>
-    import { Card, CardTitle, CardDescription, CardHeader, CardContent } from '~/components/ui/card';
+    import { Card, CardContent } from '~/components/ui/card';
     import CoinLinkCard from '@/components/market/id/CoinLinkCard.vue';
     
     const props = defineProps({
