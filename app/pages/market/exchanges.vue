cb2c350b35af41c3c262e23ddf36a1ccc5d1ffae
verona-dev <veronadev@tuta.io>
2025-11-30 17:09:49 +0100

    CR-113 [SEO] add titles market exchanges add SEO

--- app/pages/market/exchanges.vue
+++ app/pages/market/exchanges.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script setup>
-    definePageMeta({
-        title: 'Crypto Exchanges',
+    const route = useRoute();
+    const config = useRuntimeConfig();
+    
+    const title = 'Crypto Exchanges List: Ranked by Volume & Trust Score';
+    const description = 'Compare crypto exchanges by trading volume, trust score, supported coins, fees, and more. Find the best CEX and DEX platforms for trading Bitcoin, Ethereum, and altcoins.';
+    const url = computed(() => `${config.public.site_url}${route.path}`);
+    const keywords = 'crypto fear and greed index, market dominance, bitcoin dominance, ethereum dominance, stablecoins dominance, trending cryptocurrencies, top NFTs, crypto trends, trending NFTs, crypto market sentiment, global crypto data, crypto analytics, BTC market cap share';
+    
+    useSeoMeta({
+        title,
+        description,
+        ogTitle: title,
+        ogDescription: description,
+        ogUrl: url,
+        keywords,
+        robots: 'noindex, nofollow',
     });
 </script>
-
-<style scoped>
-    .exchanges {}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,12 @@ export default defineNuxtConfig({
    devtools: { enabled: true },
 
    // SEO
+   // site-config
    site: {
       url: site_url, // canonical URL
       name: site_name, // used in Title and Meta tags
       description: site_description, // used in Meta tags
+      defaultLocale: 'en_US',
    },
 
    // seo-utils
