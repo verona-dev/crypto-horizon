c24899a70e5d377cc1e9580c2be020effa6d6537
verona-dev <veronadev@tuta.io>
2025-12-04 19:00:47 +0100

    CR-27 [PERFORMANCE] SEO move canonical url to global meta plugin

--- app/plugins/global-meta.ts
+++ app/plugins/global-meta.ts
@@ -3,18 +3,19 @@ export default defineNuxtPlugin(() => {
    const config = useRuntimeConfig();
    const route = useRoute();
 
-   const url = `${config.public.site_url}${route.path}`;
+   const current_url = `${config.public.site_url}${route.path}`;
    const site_image = 'https://res.cloudinary.com/dgcyv1ehi/image/upload/v1755195826/cyberpunk-bitcoin-illustration-2_u6fytd.webp';
 
    useHead({
       link: [
+         { rel: 'canonical', href: current_url },
          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
          { rel: 'apple-touch-icon', href: '/favicon.ico' },
       ],
    });
 
    useSeoMeta({
-      ogUrl: url,
+      ogUrl: current_url,
       ogImage: {
          url: site_image,
          type: 'image/webp',
