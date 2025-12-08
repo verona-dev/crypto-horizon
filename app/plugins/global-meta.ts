201ccce95271c6323599db4156bd220aca2fbef1
verona-dev <veronadev@tuta.io>
2025-12-08 18:17:05 +0100

    CR-27 [PERFORMANCE] SEO global-meta add page-title market add useSeoMeta

--- app/plugins/global-meta.ts
+++ app/plugins/global-meta.ts
@@ -13,7 +13,7 @@ export default defineNuxtPlugin(() => {
       // titleTemplate: (pageTitle?: string) => {
       //    return pageTitle ? `${pageTitle} | ${site_name}` : site_name;
       // },
-      titleTemplate: `%s | ${site_title}`,
+      titleTemplate: `${page_title} | ${site_title}`,
       title: page_title,
       meta: [
          { name: 'description', content: page_description },
