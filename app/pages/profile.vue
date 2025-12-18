d34868245a1d22451a318fcd50b57d38fcc5ed48
verona-dev <veronadev@tuta.io>
2025-12-18 14:46:31 +0100

    CR-83 [API ] Login /profile fetches user on mount

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script setup>
+    // AuthStore
+    import { storeToRefs } from 'pinia';
+    import { useAuthStore } from '~/stores/AuthStore.js';
+    const AuthStore = useAuthStore();
+    const { getUser } = AuthStore;
+    
+    onMounted(() => {
+        getUser();
+    });
+    
     definePageMeta({
         middleware: ['auth'],
     });
