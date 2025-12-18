db90bedf3291f232f0545d97e369e694823b30a7
verona-dev <veronadev@tuta.io>
2025-12-18 20:11:27 +0100

    CR-83 [API ] Login auth-modal implement: - status label - OTP pin input - email sign-in - verify OTP

--- app/components/AuthModal.vue
+++ app/components/AuthModal.vue
@@ -26,7 +26,7 @@
                 </form>
             </DialogHeader>
             
-            <DialogFooter>
+            <DialogFooter class='flex !flex-col gap-4 items-center'>
                 <!--   Submit buttons   -->
                 <div class='w-80 mx-auto flex flex-col gap-4'>
                     <Button @click='onEmailSubmit' class='py-5'>
@@ -38,6 +38,36 @@
                         <Button variant='outline' class='w-full py-5'>Cancel</Button>
                     </DialogClose>
                 </div>
+                
+                <!--   Status  -->
+                <div class='h-4'>
+                    <p v-if='status_label_visible' class='text-destructive text-sm'>
+                        {{ status_label_computed }}
+                    </p>
+                </div>
+                
+                <!--   OTP Pin Input   -->
+                <PinInput
+                    v-if='show_otp_input'
+                    id='pin-input'
+                    v-model='otp_input'
+                    placeholder='○'
+                    @complete='onOtpSubmit'
+                    otp
+                    required
+                >
+                    <PinInputGroup class='gap-1'>
+                        <template v-for='(id, index) in 8' :key='id'>
+                            <PinInputSlot
+                                class='rounded-md border'
+                                :index='index'
+                            />
+                            <template v-if='index !== 7'>
+                                <PinInputSeparator />
+                            </template>
+                        </template>
+                    </PinInputGroup>
+                </PinInput>
             </DialogFooter>
         </DialogContent>
     </Dialog>
@@ -47,22 +77,60 @@
     import { Button } from '~/components/ui/button';
     import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogClose } from '@/components/ui/dialog';
     import { Input } from '~/components/ui/input';
+    import { PinInput, PinInputGroup, PinInputSeparator, PinInputSlot } from '~/components/ui/pin-input';
     import { Spinner } from '~/components/ui/spinner';
     
     // AuthStore
     import { storeToRefs } from 'pinia';
     import { useAuthStore } from '~/stores/AuthStore.js';
     const AuthStore = useAuthStore();
-    const { signInWithOtp } = AuthStore;
+    const { signInWithOtp, verifyOtp } = AuthStore;
     const { loading, authModal } = storeToRefs(AuthStore);
-
-    const email = ref('veronadev@tuta.io');
+    
+    const email = ref('');
+    const status_label = ref('');
+    const status_label_computed = computed(() => status_label.value);
+    const status_label_visible = ref(false);
+    
     const onEmailSubmit = async() => {
-        await signInWithOtp(email.value);
+        if (!email.value) {
+            alert('Please enter a valid email');
+            return;
+        }
+        
+        const { error } = await signInWithOtp(email.value);
+        
+        if (error) {
+            status_label_visible.value = true;
+            status_label.value = error.message;
+            
+            setTimeout(() => {
+                status_label_visible.value = false;
+            }, 5000);
+        } else {
+            status_label_visible.value = true;
+            status_label.value = 'Check your email';
+            show_otp_input.value = true;
+        }
     };
     
     const button_label = computed(() => {
         if(loading.value) return 'Sending...';
         else return 'Submit';
     });
+    
+    const otp_input = ref([]);
+    const show_otp_input = ref(false);
+    const onOtpSubmit = async() => {
+        const joined_otp_input = otp_input.value.join('');
+        const { data, error } = await verifyOtp(email.value, joined_otp_input);
+        
+        if(data) {
+            console.log(data);
+        }
+        
+        if(error) {
+            console.log(error);
+        }
+    };
 </script>
