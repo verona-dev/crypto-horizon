6ff22718c17f85e70ac297bf4929b5f3fcb0c5a7
verona-dev <veronadev@tuta.io>
2025-12-26 16:52:07 +0100

    CR-83 [API ] Login add user email to profile

--- server/api/supabase/get-user.js
+++ server/api/supabase/get-user.js
@@ -1,12 +1,24 @@
-import { serverSupabaseClient } from '#supabase/server';
+import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server';
 
 export default defineEventHandler(async (event) => {
     const client = await serverSupabaseClient(event);
+    const user = await serverSupabaseUser(event);
     
     const { data, error } = await client
        .from('profiles')
-       .select(`username, website, avatar_url`)
+       .select(`username, avatar_url`)
        .single()
     
-    return { data, error };
+    if (error) {
+        console.error('Error fetching profile:', error);
+        return { data: null, error };
+    }
+    
+    const profile = {
+        email: user?.email,
+        username: data.username,
+        avatar_url: data.avatar_url,
+    };
+    
+    return { data: profile, error };
 });
