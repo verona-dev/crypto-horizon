ce817d07f73cb2b33fc4fb10b4bcc56a86937eb6
verona-dev <veronadev@tuta.io>
2025-12-28 20:22:45 +0100

    CR-118 [ACCOUNT] Nav-User profile-store cleanup

--- app/stores/ProfileStore.js
+++ app/stores/ProfileStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useProfileStore = defineStore('ProfileStore', {
     state: () => ({
-        profile: {},
+        profile: null,
     }),
     
     actions: {
