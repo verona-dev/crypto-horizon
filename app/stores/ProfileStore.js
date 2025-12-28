d608c9e8140422ae6fad9b87ae0e8863897f5d75
verona-dev <veronadev@tuta.io>
2025-12-28 16:59:57 +0100

    CR-118 [ACCOUNT] Nav-User create profile-store

---
+++ app/stores/ProfileStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia';
+
+export const useProfileStore = defineStore('ProfileStore', {
+    state: () => ({
+        profile: {},
+    }),
+    
+    actions: {
+
+    },
+});
