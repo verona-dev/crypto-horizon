d78e50ee0619b26fccb854686246681dee29ed8e
verona-dev <veronadev@tuta.io>
2025-12-30 20:01:30 +0100

    CR-118 [ACCOUNT] Nav-User /profile remove middleware

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -9,14 +9,5 @@
     // ProfileStore
     import { useProfileStore } from '~/stores/ProfileStore.js';
     const ProfileStore = useProfileStore();
-    const { getProfile } = ProfileStore;
     const { profile } = storeToRefs(ProfileStore);
-    
-    onMounted(async() => {
-       await getProfile();
-    });
-    
-    definePageMeta({
-        middleware: ['auth'],
-    });
 </script>
