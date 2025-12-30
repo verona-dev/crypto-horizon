fe2646a5f0c3ee85cf7db2694ec2eaaca390f24e
verona-dev <veronadev@tuta.io>
2025-12-30 20:02:30 +0100

    CR-118 [ACCOUNT] Nav-User make auth middleware global and fetch user profile on page reload

--- app/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-
-export default defineNuxtRouteMiddleware(async (to, from) => {
-    const user = useSupabaseUser();
-    
-    if(to.path === '/profile' && !user.value) {
-        return navigateTo('/');
-    }
-});
