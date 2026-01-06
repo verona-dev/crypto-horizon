6f607cb8d3ea332dec75a7acdb826fc4939ce0a2
verona-dev <veronadev@tuta.io>
2026-01-06 20:51:28 +0100

    CR-82 [ACCOUNT] Create Profile add connection badge

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -4,15 +4,16 @@
         
         <div class='h-140 flex flex-col xl:flex-row items-center gap-6'>
             <!--  Avatar + Username  -->
-            <Card class='bg-popover h-full xl:w-120 flex flex-col items-center p-6 gap-8 !shadow-none'>
-                <CardHeader class='items-center'>
+            <Card class='bg-popover h-full xl:w-120 flex flex-col items-center p-6 gap-6 !shadow-none relative'>
+                <CardHeader class='items-center gap-2'>
                     <CardTitle class='text-4xl'>{{ username }}</CardTitle>
+                    
                     <CardDescription class='text-lg capitalize'>&#8226; {{ astronaut_type }} &#8226;</CardDescription>
                 </CardHeader>
                 
                 <CardContent>
                     <div class='p-2 rounded-full shadow-lg'>
-                        <Avatar class='h-72 w-72 rounded-full ring-offset-background ring-12 ring-ebony-clay ring-offset-1'>
+                        <Avatar class='h-72 w-72 rounded-full ring-offset-background ring-10 ring-secondary ring-offset-1'>
                             <AvatarImage
                                 src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1757869820/cat-7710382_zqbc1t.jpg'
                                 alt='avatar'
@@ -21,6 +22,13 @@
                         </Avatar>
                     </div>
                 </CardContent>
+                
+                <CardFooter>
+                    <Badge class='bottom-4 relative bg-emerald-600/10 dark:bg-emerald-600/20 hover:bg-emerald-600/10 text-emerald-500 shadow-none rounded-full'>
+                        <div class='h-1.5 w-1.5 rounded-full bg-emerald-500 mr-1' />
+                        <span class='text-xxs'>Connected</span>
+                    </Badge>
+                </CardFooter>
             </Card>
             
             <!--  Personal information -->
@@ -45,8 +53,9 @@
 </template>
 
 <script setup>
-    import { Card, CardTitle, CardContent, CardDescription, CardHeader } from '~/components/ui/card';
     import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+    import { Badge } from '~/components/ui/badge';
+    import { Card, CardTitle, CardContent, CardDescription, CardHeader, CardFooter } from '~/components/ui/card';
     
     import dayjs from 'dayjs';
     import relativeTime from 'dayjs/plugin/relativeTime';
