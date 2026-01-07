58aed0ce7b5d307d2d64b78fa659d68c91fdae2e
verona-dev <veronadev@tuta.io>
2026-01-07 20:37:27 +0100

    CR-82 [ACCOUNT] Create Profile move profile avatar to its own component

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -3,33 +3,10 @@
         <h3>Profile</h3>
         
         <div class='w-full flex flex-col xl:flex-row items-center justify-center gap-6'>
-            <!--  Avatar + Username  -->
-            <Card class='bg-popover h-full xl:h-120 w-full md:w-4/5 xl:w-fit flex flex-col items-center p-6 gap-4 !shadow-none relative'>
-                <CardHeader class='items-center gap-2'>
-                    <CardTitle class='text-4xl'>{{ username }}</CardTitle>
-                    
-                    <CardDescription class='text-lg capitalize'>&#8226; {{ astronaut_type }} &#8226;</CardDescription>
-                </CardHeader>
-                
-                <CardContent>
-                    <div class='p-2 rounded-full shadow-lg'>
-                        <Avatar class='h-52 w-52 rounded-full ring-offset-background ring-10 ring-secondary ring-offset-1'>
-                            <AvatarImage
-                                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1757869820/cat-7710382_zqbc1t.jpg'
-                                alt='avatar'
-                            />
-                            <AvatarFallback class='rounded-full'>A</AvatarFallback>
-                        </Avatar>
-                    </div>
-                </CardContent>
-                
-                <CardFooter>
-                    <Badge class='bg-emerald-600/10 dark:bg-emerald-600/20 hover:bg-emerald-600/10 text-emerald-500 shadow-none rounded-full'>
-                        <div class='h-1.5 w-1.5 rounded-full bg-emerald-500 mr-1' />
-                        <span class='text-xxs'>Connected</span>
-                    </Badge>
-                </CardFooter>
-            </Card>
+            <ProfileAvatar
+                :username='username'
+                :astronaut_type='astronaut_type'
+            />
             
             <!--  Personal information -->
             <Card class='bg-popover h-full xl:h-120 w-full md:w-4/5 xl:w-140 flex flex-col p-6 gap-4 !shadow-none'>
@@ -40,7 +17,7 @@
                 <div class='flex'>
                     <CardContent class='flex-1 flex flex-col gap-6'>
                         <div
-                            v-for='item in personal_information.slice(0, 4)'
+                            v-for='item in profile_formatted.slice(0, 4)'
                             :key='item.name'
                             class='flex flex-col gap-0'
                         >
@@ -51,7 +28,7 @@
                     
                     <CardContent class='flex-1 flex flex-col gap-6'>
                         <div
-                            v-for='item in personal_information.slice(4, personal_information.length)'
+                            v-for='item in profile_formatted.slice(4, profile_formatted.length)'
                             :key='item.name'
                             class='flex flex-col gap-0'
                         >
@@ -66,9 +43,8 @@
 </template>
 
 <script setup>
-    import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
-    import { Badge } from '~/components/ui/badge';
     import { Card, CardTitle, CardContent, CardDescription, CardHeader, CardFooter } from '~/components/ui/card';
+    import ProfileAvatar from '~/components/profile/ProfileAvatar.vue';
     
     import dayjs from 'dayjs';
     import relativeTime from 'dayjs/plugin/relativeTime';
@@ -80,11 +56,12 @@
     import { useProfileStore } from '~/stores/ProfileStore.js';
     const ProfileStore = useProfileStore();
     const { profile } = storeToRefs(ProfileStore);
+    const { getAvatars } = ProfileStore;
     
     const username = ref(profile.value?.username);
     const astronaut_type = ref(profile.value?.astronaut_type);
     
-    const personal_information = ref([
+    const profile_formatted = computed(() => [
         { name: 'username', label: 'Username', value: username.value },
         { name: 'email', label: 'Email', value: profile.value?.email },
         { name: 'astronaut_type', label: 'Astronaut Type', value: astronaut_type.value },
@@ -111,5 +88,9 @@
         ogDescription: undefined,
         ogImage: undefined,
         twitterCard: undefined,
+    });
+    
+    onMounted(async() => {
+        await getAvatars();
     });
 </script>
