287f69e3fd5653febb82cc73a6ed286238f7b9e9
verona-dev <veronadev@tuta.io>
2026-01-07 20:41:53 +0100

    CR-82 [ACCOUNT] Create Profile profile-store create getAvatars method

--- app/stores/ProfileStore.js
+++ app/stores/ProfileStore.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 export const useProfileStore = defineStore('ProfileStore', {
     state: () => ({
         profile: null,
+        avatars: null,
     }),
     
     actions: {
@@ -15,6 +16,19 @@ export const useProfileStore = defineStore('ProfileStore', {
                 if(error) throw error;
                 
                 this.profile = data;
+            } catch(error) {
+                console.error(error);
+            }
+        },
+        
+        async getAvatars() {
+            try {
+                const { data, error } = await $fetch('/api/supabase/user/avatars');
+                
+                if(error) throw error;
+                
+                this.avatars = data;
+                console.log(JSON.parse(JSON.stringify(this.avatars)));
             } catch(error) {
                 console.error(error);
             }
