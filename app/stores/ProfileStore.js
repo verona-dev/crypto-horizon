597f74478a36a25d9eab6494881e3131a2bd40e3
verona-dev <veronadev@tuta.io>
2026-01-08 20:09:17 +0100

    CR-82 [ACCOUNT] Create Profile profile-store create updateAvatar method

--- app/stores/ProfileStore.js
+++ app/stores/ProfileStore.js
@@ -23,14 +23,29 @@ export const useProfileStore = defineStore('ProfileStore', {
         
         async getAvatars() {
             try {
-                const { data, error } = await $fetch('/api/supabase/user/avatars');
+                const { data, error } = await $fetch('/api/supabase/user/avatars/list', {
+                    headers: useRequestHeaders(['cookie']),
+                });
                 
                 if(error) throw error;
                 
                 this.avatars = data;
-                console.log(JSON.parse(JSON.stringify(this.avatars)));
             } catch(error) {
                 console.error(error);
+            }
+        },
+        
+        async updateAvatar(avatarUrl) {
+            try {
+                const { success, error } = await $fetch('/api/supabase/user/profile/avatar', {
+                    method: 'POST',
+                    headers: useRequestHeaders(['cookie']),
+                    body: { avatarUrl },
+                });
+
+                return { success, error };
+            } catch (error) {
+                console.error('Error updating avatar:', error);
             }
         },
     },
