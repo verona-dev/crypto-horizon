e315f04f1d3fdffd605f281d576c6691103bf9ed
verona-dev <veronadev@tuta.io>
2026-01-08 12:53:33 +0100

    CR-82 [ACCOUNT] Create Profile add test-avatar

--- app/components/profile/ProfileAvatar.vue
+++ app/components/profile/ProfileAvatar.vue
@@ -1,5 +1,4 @@
 <template>
-    <!--  Avatar + Username  -->
     <Card class='bg-popover h-full xl:h-120 w-full md:w-4/5 xl:w-fit flex flex-col items-center p-6 gap-4 !shadow-none relative'>
         <CardHeader class='items-center gap-2'>
             <CardTitle class='text-4xl'>{{ username }}</CardTitle>
@@ -13,7 +12,7 @@
                         <TooltipTrigger>
                             <Avatar
                                 @click='toggleDrawerVisibility'
-                                class='h-52 w-52 rounded-full ring-offset-background ring-10 ring-secondary ring-offset-1 hover:opacity-85 hover:cursor-pointer hover:ring-green-shamrock'
+                                class='h-52 w-52 rounded-full ring-offset-background ring-10 ring-secondary ring-offset-1 hover:cursor-pointer hover:ring-green-shamrock'
                             >
                                 <AvatarImage
                                     :src='avatar_src'
@@ -29,19 +28,28 @@
                     </Tooltip>
                 </TooltipProvider>
                 
+                <!--  Avatar Change  -->
                 <Drawer v-model:open='drawer_visibility'>
                     <DrawerContent>
                         <div class='mx-auto w-full max-w-sm'>
                             <DrawerHeader>
-                                <DrawerTitle>Avatar</DrawerTitle>
-                                <DrawerDescription>Change your profile avatar.</DrawerDescription>
+                                <DrawerTitle>Select your avatar</DrawerTitle>
+                                <DrawerDescription>Click to select your profile avatar.</DrawerDescription>
                             </DrawerHeader>
                             
-                            <div class='p-4 pb-0'>
-                                Content
+                            <div class='my-24 p-4 border w-fit'>
+                                <div class='relative'>
+                                    <Avatar class='size-16 rounded-md'>
+                                        <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <BadgeCheck class='absolute bottom-1 -right-1 size-4.5 rounded-full fill-blue-500 text-white'></BadgeCheck>
+                                </div>
                             </div>
                             
                             <DrawerFooter>
+                                <Button @click='onSelect'>Done</Button>
+                                
                                 <DrawerClose as-child>
                                     <Button variant='outline'>
                                         Cancel
@@ -70,6 +78,7 @@
     import { Card, CardTitle, CardContent, CardDescription, CardHeader, CardFooter } from '~/components/ui/card';
     import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle } from '~/components/ui/drawer';
     import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip';
+    import { BadgeCheck } from 'lucide-vue-next';
     
     const props = defineProps({
         username: String,
@@ -81,4 +90,8 @@
     const avatar_src = ref('https://oqnuuqvoiolgpdpkhyby.supabase.co/storage/v1/object/public/avatars/avatar-6.webp');
     const drawer_visibility = ref(false);
     const toggleDrawerVisibility = () => drawer_visibility.value = !drawer_visibility.value;
+    
+    const onSelect = () => {
+        console.log('selected');
+    };
 </script>
