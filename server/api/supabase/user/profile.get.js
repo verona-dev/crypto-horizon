b49227ec6f9bf03e92ed6c49a6475f8ac73b0732
verona-dev <veronadev@tuta.io>
2026-01-08 16:29:02 +0100

    CR-82 [ACCOUNT] Create Profile get-profile adjust error handling

--- server/api/supabase/user/profile.get.js
+++ server/api/supabase/user/profile.get.js
@@ -4,27 +4,43 @@ export default defineEventHandler(async (event) => {
     const client = await serverSupabaseClient(event);
     const user = await serverSupabaseUser(event);
     
-    const { data, error } = await client
-       .from('profiles')
-       .select('*')
-       .single()
-    
-    if (error) {
-        console.error('Error fetching profile:', error);
-        return { data: null, error };
+    if(!user) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+        });
     }
     
-    const profile = {
-        astronaut_type: data.astronaut_type,
-        avatar_url: data.avatar_url,
-        bio: data.bio,
-        dob: data.dob,
-        email: user?.email,
-        last_sign_in: data.last_sign_in,
-        location: data.location,
-        updated_at: data.updated_at,
-        username: data.username,
-    };
-    
-    return { data: profile, error };
+    try {
+        const { data, error } = await client
+           .from('profiles')
+           .select('*')
+           .single()
+        
+        if (error) {
+            console.error('Error fetching profile:', error);
+            return { data: null, error };
+        }
+        
+        const profile = {
+            astronaut_type: data.astronaut_type,
+            avatar_url: data.avatar_url,
+            bio: data.bio,
+            dob: data.dob,
+            email: user?.email,
+            last_sign_in: data.last_sign_in,
+            location: data.location,
+            updated_at: data.updated_at,
+            username: data.username,
+        };
+        
+        return { data: profile, error };
+    } catch(error) {
+        console.error('Unexpected error:', error);
+        
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Internal Server Error',
+        });
+    }
 });
