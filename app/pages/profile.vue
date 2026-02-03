09bb8e8a44a71ec21fc257ace10541e8d090471d
verona-dev <veronadev@tuta.io>
2026-02-03 23:23:39 +0100

    CR-82 [ACCOUNT] Create Profile Add snowfall-bg to profile page

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -1,6 +1,8 @@
 <template>
     <div class='profile page gap-12 flex-col'>
+        <!--
         <h3>Profile</h3>
+        -->
         
         <div class='flex flex-col items-center gap-6'>
             <div class='w-full flex flex-col xl:flex-row items-center justify-center gap-6'>
@@ -16,6 +18,18 @@
                 <ProfileMap :coordinates='coordinates' />
             </client-only>
         </div>
+        
+        <div class="absolute inset-0 h-screen w-screen">
+            <SnowfallBg
+                v-if='dark_mode'
+                color='eee8a9'
+                class='absolute inset-0'
+                :min-radius='0.2'
+                :max-radius='4'
+                :speed='0.5'
+            />
+        </div>
+    
     </div>
 </template>
 
@@ -23,6 +37,7 @@
     import ProfileAvatar from '~/components/profile/ProfileAvatar.vue';
     import ProfileInformation from '~/components/profile/ProfileInformation.vue';
     import ProfileMap from '~/components/profile/ProfileMap.vue';
+    import { SnowfallBg } from '~/components/ui/snowfall-bg';
     
     import dayjs from 'dayjs';
     import relativeTime from 'dayjs/plugin/relativeTime';
@@ -30,8 +45,12 @@
     dayjs.extend(relativeTime);
     dayjs.extend(utc);
     
+    const colorMode = useColorMode();
+    const dark_mode = computed(() => colorMode.value === 'dark');
+    
     // ProfileStore
     import { useProfileStore } from '~/stores/ProfileStore.js';
+    import { ParticlesBg } from '@/components/ui/particles-bg/index.ts';
     const ProfileStore = useProfileStore();
     const { profile } = storeToRefs(ProfileStore);
     
