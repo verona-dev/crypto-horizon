75a0f9e6b4d10b76bbec21206275704e404171cf
verona-dev <veronadev@tuta.io>
2026-02-04 00:49:33 +0100

    CR-122 [ACCOUNT] Watchlist Create server route for updating watchlist

---
+++ server/api/supabase/user/profile/watchlist.js
@@ -0,0 +1,39 @@
+import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+    const client = await serverSupabaseClient(event);
+    const user = await serverSupabaseUser(event);
+    const body = await readBody(event);
+    
+    if (!user) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+        });
+    }
+    console.log(body);
+    try {
+        const { data, error } = await client
+           .from('profiles')
+           .update(body)
+           .eq('id', user.sub)
+           .select()
+           .single();
+        
+        if (error) {
+            throw createError({
+                statusCode: 500,
+                statusMessage: 'Failed to update watchlist',
+            });
+        }
+        
+        return { data, error };
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Internal Server Error',
+        });
+    }
+});
