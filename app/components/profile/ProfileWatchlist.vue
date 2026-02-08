3a8a905f8202ee35a0abbd177134cdbca361e84c
verona-dev <veronadev@tuta.io>
2026-02-08 14:03:40 +0100

    CR-122 [ACCOUNT] Watchlist Add current price and price percentage change

--- app/components/profile/ProfileWatchlist.vue
+++ app/components/profile/ProfileWatchlist.vue
@@ -4,11 +4,11 @@
             <TableCaption>A list of your watchlist coins.</TableCaption>
             <TableHeader>
                 <TableRow>
-                    <TableHead class='border w-28'>Rank</TableHead>
-                    <TableHead class='border w-64'>Name</TableHead>
-                    <TableHead class='border'>Price (USD)</TableHead>
-                    <TableHead class='border'>24h Change</TableHead>
-                    <TableHead class='border'>Market Cap</TableHead>
+                    <TableHead>Rank</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Price</TableHead>
+                    <TableHead>24h &#37;</TableHead>
+                    <TableHead>Market Cap</TableHead>
                 </TableRow>
             </TableHeader>
             
@@ -17,21 +17,33 @@
                     v-for='coin in watchlistData'
                     :key='coin.id'
                 >
-                    <TableCell>
-                        #{{ coin.market_cap_rank }}
-                    </TableCell>
+                    <TableCell>&#35;{{ coin.market_cap_rank }}</TableCell>
                     
                     <TableCell class='flex items-center gap-2'>
                         <NuxtImg
                             :src='coin.image'
                             alt='coin logo'
-                            class='w-8 rounded-full'
+                            class='w-6 rounded-full'
                         />
                         
                         <div class='flex items-center gap-2 truncate'>
                             <p class='font-medium'>{{ coin.name }}</p>
                             <p class='uppercase text-muted-foreground'>{{ coin.symbol }}</p>
                         </div>
+                    </TableCell>
+                    
+                    <TableCell>{{ formatNumber(coin.current_price, { maximumFractionDigits: 4 }) }}</TableCell>
+                    
+                    <TableCell :class='getTrendClass(coin.price_change_percentage_24h)'>
+                        <div class='flex items-center gap-1'>
+                            <NuxtIcon
+                                :name='getTrendIcon(coin.price_change_percentage_24h)'
+                                size='13'
+                            />
+                            
+                            <span>{{ formatNumber(coin.price_change_percentage_24h, { style: 'percent' }) }}</span>
+                        </div>
+                    
                     </TableCell>
                 </TableRow>
             </TableBody>
@@ -40,8 +52,10 @@
 </template>
 
 <script setup>
-    import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+    import { formatNumber } from '~/utils/formatUtils.js';
+    import { getTrendClass, getTrendIcon } from '~/utils/styleUtils.js';
     import { Card, CardTitle, CardContent, CardDescription, CardHeader, CardFooter } from '~/components/ui/card';
+    import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
     
     // ProfileStore
     import { storeToRefs } from 'pinia';
