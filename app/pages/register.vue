f1668cb92f38b7c155427c0ca265eb72eeacea6f
verona-dev <veronadev@tuta.io>
2026-02-09 20:49:36 +0100

    CR-119 [ACCOUNT] add Login types Create /register page

---
+++ app/pages/register.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='bg-background flex flex-col items-center justify-center gap-6 p-6 md:p-10'>
+        <div class='w-full max-w-sm'>
+            <SignupForm />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import SignupForm from '@/components/SignupForm.vue';
+</script>
