2cd1d76050c47db1fd25508ef922c7c187be07ce
verona-dev <veronadev@tuta.io>
2026-02-12 20:34:38 +0100

    CR-119 [ACCOUNT] add Login types Refactor signup-form component

--- app/components/auth/SignupForm.vue
+++ app/components/auth/SignupForm.vue
@@ -1,164 +1,95 @@
 <template>
-    <div :class='cn("flex flex-col gap-6 max-w-sm", props.class)'>
-        <Form
-            v-slot='{ meta, validate, setFieldError }'
-            as=''
-            keep-values
-            :validation-schema='validationSchema'
-        >
-            <form @submit.prevent='() => validate()'>
-                <FieldGroup>
-                    <!--   Email   -->
-                    <FormField
-                        v-slot='{ componentField }'
-                        v-model='email'
-                        name='email'
-                    >
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            
-                            <FormControl>
-                                <Input
-                                    v-bind='componentField'
-                                    type='email'
-                                    class='!w-full'
-                                    placeholder='name@example.com'
-                                    @input='validate()'
-                                />
-                            </FormControl>
-                            
-                            <FieldDescription v-if='otp_signup' class='text-xs'>
-                                New astronaut? We’ll automatically create an account on first sign-up.
-                            </FieldDescription>
-                            
-                            <!--
-                            <FormMessage />
-                            -->
-                        </FormItem>
-                    </FormField>
-                    
-                    <!--   Password   -->
-                    <FormField
-                        v-if='!otp_signup'
-                        v-slot='{ componentField }'
-                        v-model='email'
-                        name='email'
-                    >
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            
-                            <FormControl>
-                                <Input
-                                    v-bind='componentField'
-                                    id='password'
-                                    type='password'
-                                    required
-                                />
-                                <FieldDescription>
-                                    Must be at least 8 characters long.
-                                </FieldDescription>
-                            </FormControl>
-                            
-                            <!--
-                            <FormMessage />
-                            -->
-                        </FormItem>
-                    </FormField>
-                    
-                    <!--   Create account   -->
-                    <Field>
-                        <Button
-                            @click='onCreateAccount'
-                            type='submit'
-                        >
-                            Create Account
-                        </Button>
-                    </Field>
-                    
-                    <Field>
-                        <Button
-                            @click='onToggleOtpSignup'
-                            type='button'
-                            variant='link'
-                        >
-                            
-                            {{ otp_signup ? 'Use password signup instead' : 'Use OTP signup instead' }}
-                        </Button>
-                    </Field>
-                    
-                    <FieldSeparator>Or</FieldSeparator>
-                    
-                    <Field class='grid gap-4 sm:grid-cols-2'>
-                        <Button variant='outline' type='button'>
-                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-                                <path
-                                    d='M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701'
-                                    fill='currentColor'
-                                />
-                            </svg>
-                            Continue with Apple
-                        </Button>
+    <Form
+        v-slot='{ validate, setFieldError }'
+        as=''
+        keep-values
+        :validation-schema='validationSchema'
+    >
+        <form @submit.prevent='() => validate()'>
+            <FieldGroup>
+                <!--   Email   -->
+                <FormField
+                    v-slot='{ componentField }'
+                    v-model='email'
+                    name='email'
+                >
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
                         
-                        <Button variant='outline' type='button'>
-                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-                                <path
-                                    d='M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z'
-                                    fill='currentColor'
-                                />
-                            </svg>
-                            Continue with Google
-                        </Button>
-                    </Field>
-                </FieldGroup>
-            </form>
-        </Form>
-        
-        <FieldDescription class='px-6 text-center'>
-            By clicking continue, you agree to our <a href='#'>Terms of Service</a>
-            and <a href='#'>Privacy Policy</a>.
-        </FieldDescription>
-    </div>
+                        <FormControl>
+                            <Input
+                                v-bind='componentField'
+                                type='email'
+                                class='!w-full'
+                                placeholder='name@example.com'
+                                @input='validate()'
+                            />
+                        </FormControl>
+                        
+                        <!--
+                        <FormMessage />
+                        -->
+                    </FormItem>
+                </FormField>
+                
+                <!--   Password   -->
+                <FormField
+                    v-slot='{ componentField }'
+                    v-model='password'
+                    name='password'
+                >
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        
+                        <FormControl>
+                            <Input
+                                v-bind='componentField'
+                                id='password'
+                                type='password'
+                                required
+                            />
+                            <FieldDescription>
+                                Must be at least 8 characters long.
+                            </FieldDescription>
+                        </FormControl>
+                        
+                        <!--
+                        <FormMessage />
+                        -->
+                    </FormItem>
+                </FormField>
+                
+                <!--   Create account   -->
+                <Field>
+                    <Button
+                        @click='onCreateAccount'
+                        type='submit'
+                    >
+                        Create Account
+                    </Button>
+                </Field>
+            </FieldGroup>
+        </form>
+    </Form>
 </template>
 
 <script setup lang='ts'>
     import * as z from 'zod';
+    import { Button } from '@/components/ui/button';
+    import { Field, FieldDescription, FieldGroup } from '@/components/ui/field';
+    import { Form, FormControl, FormField, FormLabel, FormItem, FormMessage } from '@/components/ui/form';
+    import { Input } from '@/components/ui/input';
+    const { setFieldError } = useForm();
     import { toTypedSchema } from '@vee-validate/zod';
     import { useForm } from 'vee-validate';
-    const { setFieldError } = useForm();
-    import type { HTMLAttributes } from 'vue';
-    import { cn } from '@/lib/utils';
-    import { Button } from '@/components/ui/button';
-    import { Field, FieldDescription, FieldGroup, FieldLabel, FieldSeparator } from '@/components/ui/field';
-    import { Input } from '@/components/ui/input';
-    import { Form, FormControl, FormField, FormLabel, FormItem, FormMessage } from '@/components/ui/form';
-    
-    const props = defineProps<{
-        class?: HTMLAttributes['class']
-    }>();
     
     // AuthStore
-    import { storeToRefs } from 'pinia';
     import { useAuthStore } from '~/stores/AuthStore.js';
     const AuthStore = useAuthStore();
     const { signUp } = AuthStore;
     
-    // ProfileStore
-    import { useProfileStore } from '~/stores/ProfileStore.js';
-    const ProfileStore = useProfileStore();
-    const { getProfile } = ProfileStore;
-    
     const email = ref('');
     const password = ref('');
-    const otp_signup = ref(true);
-    
-    const onToggleOtpSignup = () => {
-        otp_signup.value = !otp_signup.value;
-    };
-    
-    // const formSchema = [
-    //     z.object({ email: z.string().email() }),
-    //     z.object({ password: z.string().min(8, "Password must be at least 8 chars") })
-    // ];
     
     const validationSchema = toTypedSchema(
         z.object({
