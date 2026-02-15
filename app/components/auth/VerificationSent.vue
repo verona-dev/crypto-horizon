8ce994852f5f57c798c1a8c6c5d6c222741a39db
verona-dev <veronadev@tuta.io>
2026-02-15 17:18:58 +0100

    CR-119 [ACCOUNT] add Login types Create verification-sent component

---
+++ app/components/auth/VerificationSent.vue
@@ -0,0 +1,25 @@
+<template>
+    <FormField name='verify'>
+        <FormItem class='flex flex-col items-center gap-8'>
+            <div class='flex flex-col items-center gap-2'>
+                <p>We sent a verification link to your email address.</p>
+                <p>Please check your inbox and click the link to verify your account.</p>
+            </div>
+            
+            <NuxtLink to='/'>
+                <Button variant='outline' size='lg'>
+                    <NuxtIcon
+                        name='ph:house'
+                        size='17'
+                    />
+                    Go home
+                </Button>
+            </NuxtLink>
+        </FormItem>
+    </FormField>
+</template>
+
+<script setup>
+    import { Button } from '@/components/ui/button/index.ts';
+    import { FormField, FormItem } from '@/components/ui/form/index.ts';
+</script>
