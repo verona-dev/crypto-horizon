19b0329eae5311d111da9100fffbff762877f2bc
verona-dev <veronadev@tuta.io>
2026-02-15 14:14:31 +0100

    CR-119 [ACCOUNT] add Login types Change default Card component border radius

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   <div
     :class="
       cn(
-        'rounded-xl border border-border/50 hover:border-border bg-card text-card-foreground shadow-2xl animate-fadeIn z-10',
+        'rounded-md border border-border/50 hover:border-border bg-card text-card-foreground shadow-2xl animate-fadeIn z-10',
         props.class,
       )
     "
