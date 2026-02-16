6cd1b4b2c122463e5f396db4da96e2c95223a3c6
verona-dev <veronadev@tuta.io>
2026-02-16 17:47:00 +0100

    CR-119 [ACCOUNT] add Login types Register forms adjust layout

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -1,76 +1,79 @@
 <template>
-    <div class='absolute inset-0 h-screen w-screen'>
-        <SnowfallBg
-            color='7393bc'
-            class='absolute inset-0 !z-0'
-            :min-radius='0.2'
-            :max-radius='2'
-            :speed='0.1'
-            :quantity='150'
-        />
-    </div>
-    <Card class='bg-transparent p-8 mt-8 xl:min-w-150 flex flex-col items-center border-none shadow-none'>
-        <CardContent class='w-full flex flex-col items-center justify-center gap-6'>
-            <!--  Logo  -->
-            <NuxtImg
-                src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1766403245/astronaut-cartoon_tnp9t4.gif'
-                alt='crypto horizon login logo'
-                class='w-44 h-44 rounded-full select-none self-center p-1.5 border-2 border-secondary'
-                :custom='true'
-                v-slot='{ src, isLoaded, imgAttrs }'
-                preload
-            >
-                <img
-                    v-if='isLoaded'
-                    v-bind='imgAttrs'
-                    :src='src'
+    <div class='page'>
+        <section class='absolute inset-0 h-screen w-screen'>
+            <SnowfallBg
+                color='7393bc'
+                class='absolute inset-0 !z-0'
+                :min-radius='0.2'
+                :max-radius='2'
+                :speed='0.1'
+                :quantity='150'
+            />
+        </section>
+        
+        <Card class='bg-transparent flex flex-col items-center border-none shadow-none'>
+            <CardContent class='w-full flex flex-col items-center justify-center gap-6'>
+                <!--  Logo  -->
+                <NuxtImg
+                    src='https://res.cloudinary.com/dgcyv1ehi/image/upload/v1766403245/astronaut-cartoon_tnp9t4.gif'
+                    alt='crypto horizon login logo'
+                    class='w-44 h-44 rounded-full select-none self-center p-1.5 border-2 border-secondary'
+                    :custom='true'
+                    v-slot='{ src, isLoaded, imgAttrs }'
+                    preload
                 >
+                    <img
+                        v-if='isLoaded'
+                        v-bind='imgAttrs'
+                        :src='src'
+                    >
+                    
+                    <Skeleton
+                        v-else
+                        class='w-44 h-44 rounded-full self-center'
+                    />
+                </NuxtImg>
                 
-                <Skeleton
-                    v-else
-                    class='w-44 h-44 rounded-full self-center'
-                />
-            </NuxtImg>
-            
-            <div class='flex flex-col w-full'>
-                <SignupOtp
-                    v-if='otp_signup'
-                    @otp-step-change='onOtpStepChange'
-                />
+                <div class='flex flex-col w-full'>
+                    <SignupOtp
+                        v-if='otp_signup'
+                        @otp-step-change='onOtpStepChange'
+                    />
+                    
+                    <SignupPassword
+                        v-else
+                        @password-step-change='onPasswordStepChange'
+                    />
+                </div>
                 
-                <SignupPassword
-                    v-else
-                    @password-step-change='onPasswordStepChange'
-                />
-            </div>
-            
-            <div
-                v-if='show_toggle'
-                class='flex flex-col gap-8'
-            >
-                <Field @click='onToggleSignupMode'>
-                    <Button type='button' variant='link'>
-                        <Button variant='ghost' size='icon'>
-                            <NuxtIcon
-                                name='ph:arrows-left-right'
-                                size='18'
-                            />
+                <div
+                    v-if='show_toggle'
+                    class='flex flex-col gap-8'
+                >
+                    <Field @click='onToggleSignupMode'>
+                        <Button type='button' variant='link'>
+                            <Button variant='ghost' size='icon'>
+                                <NuxtIcon
+                                    name='ph:arrows-left-right'
+                                    size='18'
+                                />
+                            </Button>
+                            {{ otp_signup ? 'Use password signup instead' : 'Use OTP signup instead' }}
                         </Button>
-                        {{ otp_signup ? 'Use password signup instead' : 'Use OTP signup instead' }}
-                    </Button>
-                </Field>
-                
-                <FieldSeparator>Or</FieldSeparator>
-                
-                <SignupSocials />
-                
-                <FieldDescription class='text-center'>
-                    By clicking continue, you agree to our <a href='#'>Terms of Service</a>
-                    and <a href='#'>Privacy Policy</a>.
-                </FieldDescription>
-            </div>
-        </CardContent>
-    </Card>
+                    </Field>
+                    
+                    <FieldSeparator>Or</FieldSeparator>
+                    
+                    <SignupSocials />
+                    
+                    <FieldDescription class='text-center'>
+                        By clicking continue, you agree to our <a href='#'>Terms of Service</a>
+                        and <a href='#'>Privacy Policy</a>.
+                    </FieldDescription>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
 </template>
 
 <script setup>
