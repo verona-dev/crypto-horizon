970bc1305a0a1ae1920f0a2860bb55da983f5a68
verona-dev <veronadev@tuta.io>
2026-02-18 15:32:45 +0100

    CR-119 [ACCOUNT] add Login types Create login-oauth server route

---
+++ server/api/supabase/auth/login-oauth.post.js
@@ -0,0 +1,36 @@
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async(event) => {
+    const client = await serverSupabaseClient(event);
+    const body = await readBody(event);
+    const { provider } = body;
+    
+    if(!provider) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'Provider is required',
+        });
+    }
+    
+    try {
+        const { data, error } = await client.auth.signInWithOAuth({
+            provider
+        });
+        
+        if(error) {
+            throw createError({
+                statusCode: error.status || 500,
+                statusMessage: error.message || 'Unexpected error during login',
+            });
+        }
+        
+        return { data, error };
+    } catch(error) {
+        console.error(error);
+        
+        throw createError({
+            statusCode: error.status || 500,
+            statusMessage: error.message || 'Internal Server Error',
+        });
+    }
+});
