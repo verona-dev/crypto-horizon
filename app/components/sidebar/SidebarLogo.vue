d73a31b139d022df471d827746dceae6dbf9f7ca
verona-dev <veronadev@tuta.io>
2026-05-16 20:18:55 +0200

    CR-135 [SIDEBAR] v2.0 Refactor sidebar logo

--- app/components/sidebar/SidebarLogo.vue
+++ app/components/sidebar/SidebarLogo.vue
@@ -2,39 +2,32 @@
     import { useSidebar } from '../ui/sidebar/utils';
     import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '~/components/ui/sidebar';
     const { open, isMobile } = useSidebar();
+    
+    import { Rocket } from "lucide-vue-next"
+
 </script>
 
 <template>
     <SidebarMenu>
-        <SidebarMenuItem :class='{ "h-full" : open }'>
-            <!--  Open  -->
-            <template v-if='open || isMobile'>
-                <SidebarMenuButton class=' full h-full justify-evenly select-none hover:cursor-default hover:bg-transparent active:bg-transparent'>
-                    <img
-                        src='assets/logo/btc-planet.png'
-                        alt='crypto horizon logo'
-                        width='40'
-                        height='40'
-                        preload
-                    />
-                    
-                    <div class='flex flex-col font-great select-none'>
-                        <h5>Crypto</h5>
-                        <h5 class='ml-5'>Horizon</h5>
-                    </div>
-                </SidebarMenuButton>
-            </template>
-            
-            <!--  Close  -->
-            <template v-else>
-                <img
-                    src='assets/logo/btc-planet.png'
-                    alt='crypto horizon logo'
-                    width='40'
-                    height='40'
-                    preload
-                />
-            </template>
+        <SidebarMenuItem>
+            <SidebarMenuButton>
+<!--                <img-->
+<!--                    src='assets/logo/btc-planet.png'-->
+<!--                    alt='crypto horizon logo'-->
+<!--                    width='35'-->
+<!--                    height='35'-->
+<!--                    preload-->
+<!--                />-->
+                
+                <Rocket />
+                <span>Crypto Horizon</span>
+                
+<!--                <div class='flex flex-col font-great select-none'>-->
+<!--                    <h5>Crypto</h5>-->
+<!--                    <h5 class='ml-5'>Horizon</h5>-->
+<!--                </div>-->
+            </SidebarMenuButton>
         </SidebarMenuItem>
     </SidebarMenu>
 </template>
+
