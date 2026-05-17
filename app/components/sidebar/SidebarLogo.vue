afe1f789eb095cd5c6e4c5ad7d9df914da12c065
verona-dev <veronadev@tuta.io>
2026-05-17 20:58:50 +0200

    CR-135 [SIDEBAR] v2.0 Refactor sidebar logo

--- app/components/sidebar/SidebarLogo.vue
+++ app/components/sidebar/SidebarLogo.vue
@@ -1,22 +1,27 @@
 <script setup lang='ts'>
     import { useSidebar } from '../ui/sidebar/utils';
     import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '~/components/ui/sidebar';
-    const { open, isMobile } = useSidebar();
+    const { open } = useSidebar();
     
     import { Rocket } from "lucide-vue-next"
 
 </script>
 
 <template>
-    <SidebarMenu>
-        <SidebarMenuItem>
-            <SidebarMenuButton class='h-full !py-0 !my-0 hover:bg-transparent'>
-                <Rocket />
-                
-                <div class='flex flex-col font-great'>
-                    <span class='text-lg font-great'>Crypto</span>
-                    <span class='text-lg font-great ml-5'>Horizon</span>
-                </div>
+    <SidebarMenu class='!items-center'>
+        <SidebarMenuItem class='h-full w-full'>
+            <SidebarMenuButton
+                :class='{ "h-full hover:bg-transparent" : open }'
+                :tooltip="!open ? 'Home' : undefined"
+            >
+                <NuxtLink to='/' :class='{ "flex items-center" : open }'>
+                    <Rocket :class='{ "w-4 h-4 shrink-0" : !open }' />
+                    
+                    <div v-if='open' class='flex flex-col font-great'>
+                        <span class='text-lg font-great'>Crypto</span>
+                        <span class='text-lg font-great ml-5'>Horizon</span>
+                    </div>
+                </NuxtLink>
             </SidebarMenuButton>
         </SidebarMenuItem>
     </SidebarMenu>
