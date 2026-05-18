648b6fbfb91a3f56ac3243ee12ab5997f6c03f03
verona-dev <veronadev@tuta.io>
2026-05-19 00:38:56 +0200

    CR-136 [ACCOUNT] Register and login split half page Add auth-hero component to the register page

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -71,34 +71,7 @@
             </CardContent>
         </Card>
         
-        <div class='relative hidden lg:flex w-full h-full flex-col items-center justify-center overflow-hidden'>
-            <div class='flex w-full items-center justify-center mr-6'>
-                <TextGlitch
-                    :speed='3.5'
-                    :enable-shadows='true'
-                    text='Crypto Horizon'
-                    :enableOnHover='true'
-                    class='font-great !z-10'
-                />
-            </div>
-            
-            <div class='grid place-content-center'>
-                <IconCloud
-                    :images='imageUrls'
-                    class='cursor-grab !z-10'
-                />
-            </div>
-            
-            <SnowfallBg
-                v-if='dark_mode'
-                color="#e4d9e2"
-                class='absolute inset-0 !min-h-lvh !min-w-full !z-0'
-                :min-radius='0.1'
-                :max-radius='2'
-                :speed='0.2'
-                :quantity='50'
-            />
-        </div>
+        <AuthHero />
     </div>
 </template>
 
@@ -110,37 +83,7 @@
     import RegisterEmail from '@/components/auth/RegisterEmail.vue';
     import LoginOAuth from '@/components/auth/LoginOAuth.vue';
     import { Skeleton } from '@/components/ui/skeleton/index';
-    import { SnowfallBg } from '~/components/ui/snowfall-bg';
-    import { IconCloud } from '~/components/ui/icon-cloud';
-    import TextGlitch from '~/components/ui/text-glitch/TextGlitch.vue';
-    
-    const colorMode = useColorMode();
-    const dark_mode = computed(() => colorMode.value === 'dark');
-    
-    const chains = [
-        "bitcoin",
-        "ethereum",
-        "cardano",
-        "polkadot",
-        "polygon",
-        "solana",
-        "tron",
-        "base",
-        "bnbchain",
-        "arbitrum",
-        "avalanche",
-        "optimism",
-        "stellar",
-        "near",
-        "kava",
-        "hedera",
-        "xrp",
-        "internetcomputer",
-        "litecoin",
-        "dogecoin",
-    ];
-    
-    const imageUrls = chains.map((slug) => `https://cdn.simpleicons.org/${slug}/${slug}`);
+    import AuthHero from '~/components/auth/AuthHero.vue';
     
     const is_otp_default = ref(true);
     const onToggleSignupMode = () => is_otp_default.value = !is_otp_default.value;
