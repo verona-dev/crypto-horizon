a5b05706999b84f6efa6d348b5e517bf69746ab2
verona-dev <veronadev@tuta.io>
2026-05-19 22:51:31 +0200

    CR-134 [COIN] improvements Create chains-cloud component

---
+++ app/components/ChainsCloud.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class='grid place-content-center'>
+        <IconCloud
+            :images='imageUrls'
+            class='cursor-grab !z-10'
+        />
+    </div>
+</template>
+
+<script setup>
+    import { IconCloud } from '~/components/ui/icon-cloud';
+    
+    const chains = [
+        "bitcoin",
+        "ethereum",
+        "cardano",
+        "polkadot",
+        "polygon",
+        "solana",
+        "tron",
+        "base",
+        "bnbchain",
+        "arbitrum",
+        "avalanche",
+        "optimism",
+        "stellar",
+        "near",
+        "kava",
+        "hedera",
+        "xrp",
+        "internetcomputer",
+        "litecoin",
+        "dogecoin",
+    ];
+    
+    const imageUrls = chains.map((slug) => `https://cdn.simpleicons.org/${slug}/${slug}`);
+</script>
