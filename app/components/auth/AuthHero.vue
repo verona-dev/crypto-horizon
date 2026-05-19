02999fccf85d8dfbed0cb159134a5ba093da3ffd
verona-dev <veronadev@tuta.io>
2026-05-19 22:17:08 +0200

    CR-134 [COIN] improvements Auth-hero component remove bg-animation

--- app/components/auth/AuthHero.vue
+++ app/components/auth/AuthHero.vue
@@ -6,7 +6,7 @@
                 :enable-shadows='true'
                 text='Crypto Horizon'
                 :enableOnHover='true'
-                class='font-great !z-10'
+                class='font-great animate-pulse !z-10'
             />
         </div>
         
@@ -16,26 +16,12 @@
                 class='cursor-grab !z-10'
             />
         </div>
-        
-        <SnowfallBg
-            v-if='dark_mode'
-            color="#e4d9e2"
-            class='absolute inset-0 !min-h-lvh !min-w-full !z-0'
-            :min-radius='0.1'
-            :max-radius='2'
-            :speed='0.2'
-            :quantity='50'
-        />
     </div>
 </template>
 
 <script setup>
     import { IconCloud } from '~/components/ui/icon-cloud';
-    import { SnowfallBg } from '~/components/ui/snowfall-bg';
     import TextGlitch from '@/components/ui/text-glitch/TextGlitch.vue';
-    
-    const colorMode = useColorMode();
-    const dark_mode = computed(() => colorMode.value === 'dark');
     
     const chains = [
         "bitcoin",
