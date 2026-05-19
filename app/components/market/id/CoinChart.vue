9aaab217fcc781d62b66cd0dcaeead62046197e9
verona-dev <veronadev@tuta.io>
2026-05-19 20:23:07 +0200

    CR-134 [COIN] improvements Coin chart remove sniper mode background color

--- app/components/market/id/CoinChart.vue
+++ app/components/market/id/CoinChart.vue
@@ -2,7 +2,6 @@
     <Card
         v-if='chart.prices'
         class='coin-chart bg-background flex flex-col gap-6 p-8 border-none shadow-none'
-        :class='{ "bg-popover/50" : sniper_mode }'
     >
         <!--  Tabs  -->
         <div class='tabs-container flex flex-col md:flex-row gap-12 md:gap-0 items-center justify-between'>
