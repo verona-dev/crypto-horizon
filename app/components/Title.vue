1ea1296272cd57d2bccd4aada223c68cbe8f2201
verona-dev <veronadev@tuta.io>
2026-05-21 16:06:41 +0200

    CR-112 [OPTIMISATIONS] improvements v2.0 Sync title component styles with global styles

--- app/components/Title.vue
+++ app/components/Title.vue
@@ -10,7 +10,7 @@
     const tag = computed(() => props.tag ?? props.level);
     
     const styleClasses = {
-        'h1': 'text-7xl xl:text-9xl tracking-widest break-words',
+        'h1': 'text-6xl md:text-7xl xl:text-9xl tracking-widest break-words',
         'h2': 'text-5xl xl:text-7xl',
         'h3': 'text-3xl xl:text-5xl !leading-8',
         'h4': 'text-xl xl:text-3xl',
