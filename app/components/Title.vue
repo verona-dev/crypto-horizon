1108d833b8bd4c9f1b2194a5ad9734b71b539c22
verona-dev <veronadev@tuta.io>
2026-05-21 15:50:32 +0200

    CR-112 [OPTIMISATIONS] improvements v2.0 Create Title component

---
+++ app/components/Title.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { cn } from '@/lib/utils';
+    
+    const props = defineProps({
+        tag: { type: String, default: 'h1' },
+        level: { type: String, default: 'h1' },
+        class: { type: String, default: '' },
+    });
+    
+    const tag = computed(() => props.tag ?? props.level);
+    
+    const styleClasses = {
+        'h1': 'text-7xl xl:text-9xl tracking-widest break-words',
+        'h2': 'text-5xl xl:text-7xl',
+        'h3': 'text-3xl xl:text-5xl !leading-8',
+        'h4': 'text-xl xl:text-3xl',
+        'h5': 'text-xl',
+        'h6': 'text-lg text-left',
+    };
+</script>
+
+<template>
+    <component
+        :is='tag'
+        :class='cn(styleClasses[level], props.class)'
+        v-bind='$attrs'
+    >
+        <slot />
+    </component>
+</template>
