896ee7f6aed924be8bc7a7ab8a429a26721bc629
verona-dev <veronadev@tuta.io>
2026-05-22 16:08:58 +0200

    CR-112 [OPTIMISATIONS] improvements v2.0 Defi page add loading-content component

--- app/pages/defi/index.vue
+++ app/pages/defi/index.vue
@@ -2,7 +2,7 @@
     <LoadingContent v-if='loading' />
     
     <div v-else class='page'>
-        <Title class='page-title'>What is Defi?</Title>
+        <Title level='h3' class='page-title'>What is Defi?</Title>
         
         <GlobalDefi />
     </div>
