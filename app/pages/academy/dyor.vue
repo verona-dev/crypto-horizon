bbd4cea6f8f284f35e8ddd0ecb8a5f06388ae41e
verona-dev <veronadev@tuta.io>
2026-05-22 19:33:06 +0200

    CR-112 [OPTIMISATIONS] improvements v2.0 Add Title component to academy dyor page

--- app/pages/academy/dyor.vue
+++ app/pages/academy/dyor.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class='dyor'>
+    <div class='page'>
+        <Title class='page-title'>Dyor</Title>
         <h6>Coming Soon...</h6>
     </div>
 </template>
 
 <script setup>
+    import Title from '~/components/Title.vue';
+    
     // SEO
     const title = 'DYOR in DeFi: How to Research Crypto & DeFi Projects Safely';
     const description = 'Learn how to Do Your Own Research (DYOR) in DeFi and crypto. Discover tools, red flags, and best practices to evaluate projects and avoid scams.';
