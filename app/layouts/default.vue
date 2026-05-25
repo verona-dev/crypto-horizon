8e359f874b5924bab825bc322e32bd6bca5646df
verona-dev <veronadev@tuta.io>
2026-05-25 02:31:35 +0200

    CR-80 [DESIGN] Design and typography Default layout code cleanup

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,16 +6,11 @@
                 
                 <SidebarInset class='flex justify-between items-center min-h-svh'>
                     <!--
-                    <MarketBar />
                     <Breadcrumb />
                     -->
                     
                     <!--  Mobile Trigger  -->
                     <MobileSidebarTrigger />
-                    
-                    <!--
-                    <h1 v-if='title' class='page-title'>{{ title }}</h1>
-                    -->
                     
                     <slot />
                     
@@ -32,7 +27,6 @@
     import AppSidebar from '~/components/sidebar/AppSidebar.vue';
     // import Breadcrumb from '~/components/Breadcrumb.vue';
     import CoingeckoAttribution from '~/components/CoingeckoAttribution.vue';
-    // import MarketBar from '~/components/market/MarketBar.vue';
     import ScrollToTop from '~/components/ScrollToTop.vue';
     import { SidebarInset, SidebarProvider, MobileSidebarTrigger, } from '~/components/ui/sidebar';
     
