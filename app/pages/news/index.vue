c811650c5fbf21d1dc0fb04a87c853feb31b2f1a
verona-dev <veronadev@tuta.io>
2026-05-31 13:43:13 +0200

    CR-70 [DESIGN] Create theme selector Use theme check composable

--- app/pages/news/index.vue
+++ app/pages/news/index.vue
@@ -3,7 +3,7 @@
         <LoadingContent v-if='loading' />
         
         <div v-else class='page'>
-            <div v-if='!isThemeDark()' class='h-[450px] flex items-center'>
+            <div v-if='!isDarkTheme' class='h-[450px] flex items-center'>
                 <Title :tag='1' :level='2'>{{ page_title }}</Title>
             </div>
             
@@ -184,10 +184,13 @@
     import { ParticleImage } from '~/components/ui/particle-image';
     import { Skeleton } from '~/components/ui/skeleton';
     import titleImage from '~/assets/images/latest-news.png';
-    import { isThemeDark } from '@/utils/styleUtils.js';
     import NewTabIcon from '~/components/NewTabIcon.vue';
     import Title from '~/components/Title.vue';
     import LoadingContent from '@/components/LoadingContent.vue';
+    
+    // Theme
+    import { useTheme } from '~/composables/useTheme.js';
+    const { isDarkTheme } = useTheme();
     
     // Dayjs
     import dayjs from 'dayjs';
