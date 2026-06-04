03f8d86843e8bec83d70725dd0f84946d8d7faff
verona-dev <veronadev@tuta.io>
2026-06-04 21:35:28 +0200

    CR-138 [ACADEMY] Glossary Encyclopedia display item cards

--- app/components/academy/Encyclopedia.vue
+++ app/components/academy/Encyclopedia.vue
@@ -1,19 +1,34 @@
 <template>
-    <div class='glossary'>
-        <Title>this is component</Title>
+    <div class='glossary max-w-5xl flex flex-col gap-8'>
+        <Title>Glossary</Title>
+        
+        <div class='flex flex-col gap-12'>
+            <Item
+                v-for='item in encyclopedia'
+                :key='item.id'
+                variant='muted'
+            >
+                <ItemContent>
+                    <ItemTitle class='text-primary'>{{ item.title }}</ItemTitle>
+                    <ItemDescription class='text-muted-foreground'>{{ item.description }}</ItemDescription>
+                </ItemContent>
+            </Item>
+        </div>
     </div>
 </template>
 
 <script setup>
     import Title from '@/components/Title';
+    import { Item, ItemDescription, ItemContent, ItemTitle } from '~/components/ui/item';
     
     // AcademyStore
+    import { storeToRefs } from 'pinia';
     import { useAcademyStore } from '@/stores/AcademyStore.js';
     const AcademyStore = useAcademyStore();
     const { encyclopedia } = storeToRefs(AcademyStore);
     const { getEncyclopedia } = AcademyStore;
-
+    
     onMounted(async() => {
-       await getEncyclopedia();
+        if(!encyclopedia.value.length) await getEncyclopedia();
     });
 </script>
--- app/pages/academy/index.vue
+++ app/pages/academy/index.vue
@@ -1,13 +1,12 @@
 <template>
     <div class='page'>
-        <Title>Glossary</Title>
         <Encyclopedia />
     </div>
 </template>
 
 <script setup>
     import Encyclopedia from '@/components/academy/Encyclopedia.vue';
-    import Title from '~/components/Title.vue';
+    // import Title from '~/components/Title.vue';
     
     // SEO
     const title = 'Crypto & DeFi Academy: Learn Blockchain & Cryptocurrency';
