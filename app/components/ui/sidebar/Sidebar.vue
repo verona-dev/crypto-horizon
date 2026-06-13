a0f70c3c1699eff9da1cebab406de63e62a58520
verona-dev <veronadev@tuta.io>
2026-06-13 10:34:39 +0200

    CR-106 [MARKET] Trending Card design Code cleanup

--- app/components/ui/sidebar/Sidebar.vue
+++ app/components/ui/sidebar/Sidebar.vue
@@ -1,78 +1,78 @@
 <script setup lang="ts">
-import type { SidebarProps } from "."
-import { cn } from "~/lib/utils"
-import { Sheet, SheetContent } from '~/components/ui/sheet'
-import { SIDEBAR_WIDTH_MOBILE, useSidebar } from "./utils"
-
-defineOptions({
-  inheritAttrs: false,
-})
-
-const props = withDefaults(defineProps<SidebarProps>(), {
-  side: "left",
-  variant: "sidebar",
-  collapsible: "offcanvas",
-})
-
-const { isMobile, state, openMobile, setOpenMobile, setOpen } = useSidebar()
-
-const sidebarRef = ref<HTMLElement | null>(null)
-const handleClickOutside = (event: MouseEvent) => {
-    if (sidebarRef.value && !sidebarRef.value.contains(event.target as Node)) {
-        setOpen(false)
+    import type { SidebarProps } from "."
+    import { cn } from "~/lib/utils"
+    import { Sheet, SheetContent } from '~/components/ui/sheet'
+    import { SIDEBAR_WIDTH_MOBILE, useSidebar } from "./utils"
+    
+    defineOptions({
+        inheritAttrs: false,
+    })
+    
+    const props = withDefaults(defineProps<SidebarProps>(), {
+        side: "left",
+        variant: "sidebar",
+        collapsible: "offcanvas",
+    })
+    
+    const { isMobile, state, openMobile, setOpenMobile, setOpen } = useSidebar()
+    
+    const sidebarRef = ref<HTMLElement | null>(null)
+    const handleClickOutside = (event: MouseEvent) => {
+        if (sidebarRef.value && !sidebarRef.value.contains(event.target as Node)) {
+            setOpen(false)
+        }
     }
-}
-
-onMounted(() => document.addEventListener('click', handleClickOutside));
-onUnmounted(() => document.removeEventListener('click', handleClickOutside));
+    
+    onMounted(() => document.addEventListener('click', handleClickOutside));
+    onUnmounted(() => document.removeEventListener('click', handleClickOutside));
 </script>
 
 <template>
-  <div
-    v-if="collapsible === 'none'"
-    :class="cn('flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground', props.class)"
-    v-bind="$attrs"
-  >
-    <slot />
-  </div>
-
-  <Sheet
-      v-else-if="isMobile"
-      :open="openMobile"
-      v-bind="$attrs"
-      @update:open="setOpenMobile"
-  >
-    <SheetContent
-      data-sidebar="sidebar"
-      data-mobile="true"
-      :side="side"
-      class="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
-      :style="{
+    <div
+        v-if="collapsible === 'none'"
+        :class="cn('flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground', props.class)"
+        v-bind="$attrs"
+    >
+        <slot />
+    </div>
+    
+    <Sheet
+        v-else-if="isMobile"
+        :open="openMobile"
+        v-bind="$attrs"
+        @update:open="setOpenMobile"
+    >
+        <SheetContent
+            data-sidebar="sidebar"
+            data-mobile="true"
+            :side="side"
+            class="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            :style="{
         '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
       }"
-    >
-      <div class="flex h-full w-full flex-col">
-        <slot />
-      </div>
-    </SheetContent>
-  </Sheet>
-
+        >
+            <div class="flex h-full w-full flex-col">
+                <slot />
+            </div>
+        </SheetContent>
+    </Sheet>
+    
     <!--
     Sidebar on hover
     @mouseenter="setOpen(true)"
     @mouseleave="setOpen(false)"
     -->
-  <div
-    ref="sidebarRef"
-    v-else class="group peer hidden md:block"
-    :data-state="state"
-    :data-collapsible="state === 'collapsed' ? collapsible : ''"
-    :data-variant="variant"
-    :data-side="side"
-  >
-    <!-- This is what handles the sidebar gap on desktop  -->
     <div
-      :class="cn(
+        ref="sidebarRef"
+        v-else class="group peer hidden md:block"
+        :data-state="state"
+        :data-collapsible="state === 'collapsed' ? collapsible : ''"
+        :data-variant="variant"
+        :data-side="side"
+    >
+        <!-- This is what handles the sidebar gap on desktop  -->
+        <div
+            :class="cn(
         'relative w-(--sidebar-width) bg-transparent transition-[width] duration-600 ease-in-out',
         'group-data-[collapsible=offcanvas]:w-0',
         'group-data-[side=right]:rotate-180',
@@ -80,9 +80,9 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside));
           ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]'
           : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon)',
       )"
-    />
-    <div
-      :class="cn(
+        />
+        <div
+            :class="cn(
         'fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-600 ease-in-out md:flex',
         side === 'left'
           ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
@@ -93,14 +93,14 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside));
           : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
         props.class,
       )"
-      v-bind="$attrs"
-    >
-      <div
-        data-sidebar="sidebar"
-        class="bg-sidebar group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm"
-      >
-        <slot />
-      </div>
+            v-bind="$attrs"
+        >
+            <div
+                data-sidebar="sidebar"
+                class="bg-sidebar group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm"
+            >
+                <slot />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
