91571dee328a99bd893ca776e7b6052a54c383de
verona-dev <veronadev@tuta.io>
2026-06-14 22:19:26 +0200

    CR-75 [DEFI] Create GlobalDefi dashboard Adjust style and layout

--- app/components/market/GlobalDefi.vue
+++ app/components/market/GlobalDefi.vue
@@ -1,37 +1,28 @@
 <template>
     <div
         v-if='globalDefi'
-        class='flex flex-col gap-24 max-w-5xl'
+        class='flex flex-col gap-24 max-w-6xl w-full'
     >
         <div class='flex flex-col gap-8'>
-            <Title class='self-center'>Global DeFi Stats</Title>
+            <Title class='self-center'>Global DeFi</Title>
             
             <!--  Defi Dominance -->
-            <Card class='item-container border-primary/25 justify-between !w-full'>
-                <Title :tag='3' :level='5' class='mt-0'>Defi Dominance</Title>
-                
-                <Progress
-                    v-if='defi_dominance_ratio'
-                    v-model='defi_dominance_ratio'
-                    class='h-3'
-                />
-                
-                <Title :tag='3' :level='3' class='!my-0 text-primary'>{{ defi_dominance_ratio_percent }}</Title>
-            </Card>
-            
-            <div class='flex flex-wrap justify-center gap-8'>
-                <!--  Market Cap  -->
-                <Card v-if='defi_market_cap' class='item-container flex-1 border-primary/25'>
-                    <MazCircularProgressBar
-                        :percentage='100'
-                        :duration='2000'
-                    >
-                        <template #default>
-                            <Title :tag='2' :level='5'>{{ market_cap_compact }}</Title>
-                        </template>
-                    </MazCircularProgressBar>
+            <Card class='flex flex-col items-evenly justify-center p-12 gap-16 border-primary/25 !w-full'>
+                <CardHeader>
+                    <Title :tag='3' class='mt-0'>Defi Dominance</Title>
                     
-                    <CardContent class='label-container'>
+                    <Progress
+                        v-if='defi_dominance_ratio'
+                        v-model='defi_dominance_ratio'
+                        class='h-3'
+                    />
+                    
+                    <Title :tag='3' :level='3' class='!my-0 text-primary'>{{ defi_dominance_ratio_percent }}</Title>
+                </CardHeader>
+                
+                <div class='flex justify-evenly'>
+                    <!--  Market Cap  -->
+                    <CardContent v-if='defi_market_cap' class='w-86 flex flex-col items-center justify-center border-primary/25'>
                         <div class='flex items-center gap-1.5'>
                             <Title :tag='3' :level='5'>Defi {{ glossary.market_cap.label }}</Title>
                             
@@ -43,8 +34,74 @@
                             </HoverCard>
                         </div>
                         
+                        <MazCircularProgressBar
+                            :percentage='100'
+                            :duration='2000'
+                        >
+                            <template #default>
+                                <Title :tag='2' :level='5'>{{ market_cap_compact }}</Title>
+                            </template>
+                        </MazCircularProgressBar>
+                        
                         <span class='mt-2 text-muted-foreground'>{{ market_cap_value }}</span>
                     </CardContent>
+                    
+                    <!--  Volume 24h  -->
+                    <CardContent v-if='trading_volume_24h' class='w-86 flex flex-col items-center justify-center border-primary/25'>
+                        <div class='flex items-center gap-1.5'>
+                            <Title :tag='3' :level='5' class='!min-w-fit'>{{ glossary.volume.label }}</Title>
+                            
+                            <HoverCard
+                                :openDelay='200'
+                                class='flex'
+                            >
+                                <HoverCardTrigger>
+                                    <InfoIcon size='20' />
+                                </HoverCardTrigger>
+                                <HoverCardContent>{{ glossary.volume.description }}</HoverCardContent>
+                            </HoverCard>
+                        </div>
+                        
+                        <MazCircularProgressBar
+                            :percentage='100'
+                            :duration='3000'
+                        >
+                            <template #default>
+                                <h5>{{ trading_volume_compact }}</h5>
+                            </template>
+                        </MazCircularProgressBar>
+                        
+                        <span class='mt-2 text-muted-foreground'>{{ trading_volume_value }}</span>
+                    </CardContent>
+                </div>
+            </Card>
+            
+            <div class='flex flex-wrap justify-center gap-8'>
+                <!--  Market Cap  -->
+                <Card v-if='defi_market_cap' class='item-container flex-1 border-primary/25'>
+                    <CardContent class='label-container'>
+                        <div class='flex items-center gap-1.5'>
+                            <Title :tag='3' :level='5'>Defi {{ glossary.market_cap.label }}</Title>
+                            
+                            <HoverCard :openDelay='200'>
+                                <HoverCardTrigger>
+                                    <InfoIcon size='20' />
+                                </HoverCardTrigger>
+                                <HoverCardContent>{{ glossary.market_cap.description }}</HoverCardContent>
+                            </HoverCard>
+                        </div>
+                    
+                    </CardContent>
+                    <MazCircularProgressBar
+                        :percentage='100'
+                        :duration='2000'
+                    >
+                        <template #default>
+                            <Title :tag='2' :level='5'>{{ market_cap_compact }}</Title>
+                        </template>
+                    </MazCircularProgressBar>
+                    <span class='mt-2 text-muted-foreground'>{{ market_cap_value }}</span>
+                
                 </Card>
                 
                 <!--  Volume 24h  -->
@@ -80,11 +137,11 @@
         </div>
         
         <div class='flex flex-col gap-8'>
-            <Title class='self-center'>Top Coin Stats</Title>
+            <!--            <Title class='self-center'>Top Defi Coin</Title>-->
             
             <!--  Top coin dominance -->
             <Card class='item-container border-primary/25 justify-between !w-full'>
-                <Title :tag='3' :level='5' class='mt-0'>Top Coin Dominance</Title>
+                <Title :tag='3' class='mt-0'>Top Coin Dominance</Title>
                 
                 <Title :tag='3' :level='6' class='!my-0 uppercase'>{{ top_coin_name }}</Title>
                 
@@ -95,9 +152,12 @@
             <div class='flex flex-wrap justify-center gap-8'>
                 <!--  Defi to Eth ratio  -->
                 <Card class='item-container flex-1 border-primary/25'>
-                    <Title :tag='3' :level='5' class='mt-0'>Defi to Eth ratio</Title>
-                    
                     <MazCircularProgressBar :percentage="defi_to_eth_ratio"  suffix="%" />
+                    <CardContent>
+                        <div class='flex items-center gap-1.5'>
+                            <Title :tag='3' :level='5' class='mt-0'>Defi to Eth ratio</Title>
+                        </div>
+                    </CardContent>
                 </Card>
                 
                 <!--  Eth Market Cap  -->
@@ -134,7 +194,7 @@
 <script setup>
     import { formatNumber } from '~/utils/formatUtils.js';
     import Title from '~/components/Title.vue';
-    import { Card, CardContent } from '~/components/ui/card';
+    import { Card, CardHeader, CardContent, CardFooter } from '~/components/ui/card';
     import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card';
     import InfoIcon from '~/components/InfoIcon.vue';
     import glossary from '~/assets/data/market/glossary.json';
@@ -189,28 +249,3 @@
         style: 'percent',
     }));
 </script>
-
-<style scoped>
-    .item-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 12px;
-        padding: 48px !important;
-        
-        @media (min-width: 768px) {
-            width: 200px;
-        }
-        
-        /* Desktop */
-        @media (min-width: 1024px) {
-            width: 250px;
-        }
-        
-        .label-container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-    }
-</style>
