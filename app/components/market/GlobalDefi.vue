7f626bef53482842c1abf57b5896913d5ded33ac
verona-dev <veronadev@tuta.io>
2026-06-14 19:36:25 +0200

    CR-75 [DEFI] Create GlobalDefi dashboard Adjust style and layout

--- app/components/market/GlobalDefi.vue
+++ app/components/market/GlobalDefi.vue
@@ -1,97 +1,13 @@
 <template>
     <div
         v-if='globalDefi'
-        class='flex flex-col gap-8'
+        class='flex flex-col gap-24 w-full'
     >
-        <div class='flex flex-wrap justify-center gap-8'>
-            <!--  Market Cap  -->
-            <Card v-if='defi_market_cap' class='item-container border-primary/25'>
-                <MazCircularProgressBar
-                    :percentage='100'
-                    :duration='2000'
-                >
-                    <template #default>
-                        <Title :tag='2' :level='5'>{{ market_cap_compact }}</Title>
-                    </template>
-                </MazCircularProgressBar>
-                
-                <CardContent class='label-container'>
-                    <div class='flex items-center gap-1.5'>
-                        <Title :tag='3' :level='5'>Defi {{ glossary.market_cap.label }}</Title>
-                        
-                        <HoverCard :openDelay='200'>
-                            <HoverCardTrigger>
-                                <InfoIcon size='20' />
-                            </HoverCardTrigger>
-                            <HoverCardContent>{{ glossary.market_cap.description }}</HoverCardContent>
-                        </HoverCard>
-                    </div>
-                    
-                    <span class='mt-2 text-muted-foreground'>{{ market_cap_value }}</span>
-                </CardContent>
-            </Card>
+        <div class='flex flex-col gap-8'>
+            <Title class='self-center'>Global DeFi Stats</Title>
             
-            <!--  Volume 24h  -->
-            <Card v-if='trading_volume_24h' class='item-container border-primary/25'>
-                <MazCircularProgressBar
-                    :percentage='100'
-                    :duration='3000'
-                >
-                    <template #default>
-                        <h5>{{ trading_volume_compact }}</h5>
-                    </template>
-                </MazCircularProgressBar>
-                
-                <div class='label-container'>
-                    <div class='flex items-center'>
-                        <Title :tag='3' :level='5' class='!min-w-full'>{{ glossary.volume.label }}</Title>
-                        
-                        <HoverCard
-                            :openDelay='200'
-                            class='flex'
-                        >
-                            <HoverCardTrigger>
-                                <InfoIcon size='20' />
-                            </HoverCardTrigger>
-                            <HoverCardContent>{{ glossary.volume.description }}</HoverCardContent>
-                        </HoverCard>
-                    </div>
-                    
-                    <span class='mt-2 text-muted-foreground'>{{ trading_volume_value }}</span>
-                </div>
-            </Card>
-            
-            <!--  Eth Market Cap  -->
-            <Card v-if='defi_market_cap' class='item-container border-primary/25'>
-                <MazCircularProgressBar
-                    :percentage='100'
-                    :duration='2000'
-                >
-                    <template #default>
-                        <Title :tag='2' :level='5'>{{ eth_market_cap_compact }}</Title>
-                    </template>
-                </MazCircularProgressBar>
-                
-                <CardContent class='label-container'>
-                    <div class='flex items-center gap-1.5'>
-                        <Title :tag='3' :level='5'>ETH {{ glossary.market_cap.label }}</Title>
-                        
-                        <HoverCard :openDelay='200'>
-                            <HoverCardTrigger>
-                                <InfoIcon size='20' />
-                            </HoverCardTrigger>
-                            <HoverCardContent>{{ glossary.market_cap.description }}</HoverCardContent>
-                        </HoverCard>
-                    </div>
-                    
-                    <span class='mt-2 text-muted-foreground'>{{ eth_market_cap_value }}</span>
-                </CardContent>
-            </Card>
-        </div>
-        
-        <div class='flex flex-wrap justify-center gap-8'>
             <!--  Defi Dominance -->
-            <Card class='item-container border-primary/25 justify-between'>
+            <Card class='item-container border-primary/25 justify-between !w-full'>
                 <Title :tag='3' :level='5' class='mt-0'>Defi Dominance</Title>
                 
                 <Progress
@@ -103,15 +19,71 @@
                 <Title :tag='3' :level='3' class='!my-0 text-primary'>{{ defi_dominance_ratio_percent }}</Title>
             </Card>
             
-            <!--  Defi to Eth ratio  -->
-            <Card class='item-container border-primary/25'>
-                <Title :tag='3' :level='5' class='mt-0'>Defi to Eth ratio</Title>
+            <div class='flex flex-wrap justify-center gap-8'>
+                <!--  Market Cap  -->
+                <Card v-if='defi_market_cap' class='item-container flex-1 border-primary/25'>
+                    <MazCircularProgressBar
+                        :percentage='100'
+                        :duration='2000'
+                    >
+                        <template #default>
+                            <Title :tag='2' :level='5'>{{ market_cap_compact }}</Title>
+                        </template>
+                    </MazCircularProgressBar>
+                    
+                    <CardContent class='label-container'>
+                        <div class='flex items-center gap-1.5'>
+                            <Title :tag='3' :level='5'>Defi {{ glossary.market_cap.label }}</Title>
+                            
+                            <HoverCard :openDelay='200'>
+                                <HoverCardTrigger>
+                                    <InfoIcon size='20' />
+                                </HoverCardTrigger>
+                                <HoverCardContent>{{ glossary.market_cap.description }}</HoverCardContent>
+                            </HoverCard>
+                        </div>
+                        
+                        <span class='mt-2 text-muted-foreground'>{{ market_cap_value }}</span>
+                    </CardContent>
+                </Card>
                 
-                <MazCircularProgressBar :percentage="defi_to_eth_ratio"  suffix="%" />
-            </Card>
+                <!--  Volume 24h  -->
+                <Card v-if='trading_volume_24h' class='item-container flex-1 border-primary/25'>
+                    <MazCircularProgressBar
+                        :percentage='100'
+                        :duration='3000'
+                    >
+                        <template #default>
+                            <h5>{{ trading_volume_compact }}</h5>
+                        </template>
+                    </MazCircularProgressBar>
+                    
+                    <div class='label-container'>
+                        <div class='flex items-center'>
+                            <Title :tag='3' :level='5' class='!min-w-full'>{{ glossary.volume.label }}</Title>
+                            
+                            <HoverCard
+                                :openDelay='200'
+                                class='flex'
+                            >
+                                <HoverCardTrigger>
+                                    <InfoIcon size='20' />
+                                </HoverCardTrigger>
+                                <HoverCardContent>{{ glossary.volume.description }}</HoverCardContent>
+                            </HoverCard>
+                        </div>
+                        
+                        <span class='mt-2 text-muted-foreground'>{{ trading_volume_value }}</span>
+                    </div>
+                </Card>
+            </div>
+        </div>
+        
+        <div class='flex flex-col gap-8'>
+            <Title class='self-center'>Top Coin Stats</Title>
             
             <!--  Top coin dominance -->
-            <Card class='item-container border-primary/25 justify-between'>
+            <Card class='item-container border-primary/25 justify-between !w-full'>
                 <Title :tag='3' :level='5' class='mt-0'>Top Coin Dominance</Title>
                 
                 <Title :tag='3' :level='6' class='!my-0 uppercase'>{{ top_coin_name }}</Title>
@@ -119,6 +91,42 @@
                 <Title :tag='3' :level='3' class='!my-0 text-primary'>{{ top_coin_defi_dominance }}</Title>
             
             </Card>
+            
+            <div class='flex flex-wrap justify-center gap-8'>
+                <!--  Defi to Eth ratio  -->
+                <Card class='item-container flex-1 border-primary/25'>
+                    <Title :tag='3' :level='5' class='mt-0'>Defi to Eth ratio</Title>
+                    
+                    <MazCircularProgressBar :percentage="defi_to_eth_ratio"  suffix="%" />
+                </Card>
+                
+                <!--  Eth Market Cap  -->
+                <Card v-if='defi_market_cap' class='item-container flex-1 border-primary/25'>
+                    <MazCircularProgressBar
+                        :percentage='100'
+                        :duration='2000'
+                    >
+                        <template #default>
+                            <Title :tag='2' :level='5'>{{ eth_market_cap_compact }}</Title>
+                        </template>
+                    </MazCircularProgressBar>
+                    
+                    <CardContent class='label-container'>
+                        <div class='flex items-center gap-1.5'>
+                            <Title :tag='3' :level='5'>ETH {{ glossary.market_cap.label }}</Title>
+                            
+                            <HoverCard :openDelay='200'>
+                                <HoverCardTrigger>
+                                    <InfoIcon size='20' />
+                                </HoverCardTrigger>
+                                <HoverCardContent>{{ glossary.market_cap.description }}</HoverCardContent>
+                            </HoverCard>
+                        </div>
+                        
+                        <span class='mt-2 text-muted-foreground'>{{ eth_market_cap_value }}</span>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     </div>
 </template>
@@ -174,14 +182,8 @@
     // Defi to Eth ratio
     const defi_to_eth = computed(() => globalDefi.value?.defi_to_eth_ratio);
     const defi_to_eth_ratio = computed(() => Number(defi_to_eth.value));
-    const defi_to_eth_ratio_percent = formatNumber(defi_to_eth.value, {
-        style: 'percent',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
-    }) || 0;
     
-    
-    
+    // Top Coin
     const top_coin_name = computed(() => globalDefi.value?.top_coin_name);
     const top_coin_defi_dominance = computed(() => formatNumber(globalDefi.value?.top_coin_defi_dominance, {
         style: 'percent',
