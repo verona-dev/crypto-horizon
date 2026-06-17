8e9531088212e134f07d291e4bffcf2b5feef3b8
verona-dev <veronadev@tuta.io>
2026-06-17 11:35:00 +0200

    CR-143 [MARKET] Add exchanges First working version

--- app/pages/market/exchanges.vue
+++ app/pages/market/exchanges.vue
@@ -1,16 +1,36 @@
 <template>
-    <div class='exchanges'>
+    <LoadingContent v-if='loading' />
+    
+    <div v-else class='page'>
         <h6>Coming Soon...</h6>
+        
+        <div
+            v-for='exchange in exchanges'
+            :key='exchange.id'
+        >
+            <p>{{ exchange.id }}</p>
+            <p>{{ exchange.country }}</p>
+            <p>{{ exchange.description }}</p>
+            <p>{{ exchange.image }}</p>
+            <p>{{ exchange.name }}</p>
+            <p>{{ exchange.trade_volume_24h_btc }}</p>
+            <p>{{ exchange.trust_score }}</p>
+            <p>{{ exchange.trust_score_rank }}</p>
+            <p>{{ exchange.url }}</p>
+            <p>{{ exchange.year_established }}</p>
+        </div>
     </div>
 </template>
 
 <script setup>
+    import LoadingContent from '~/components/LoadingContent.vue';
+    
     // MarketStore
     import { storeToRefs } from 'pinia';
     import { useMarketStore } from '~/stores/MarketStore.js';
     const MarketStore = useMarketStore();
     
-    const { loading } = storeToRefs(MarketStore);
+    const { loading, exchanges } = storeToRefs(MarketStore);
     const { getExchanges } = MarketStore;
     
     // SEO
@@ -18,7 +38,7 @@
     const description = 'Compare crypto exchanges by trading volume, trust score, supported coins, fees, and more. Find the best CEX and DEX platforms for trading Bitcoin, Ethereum, and altcoins.';
     
     onMounted(async() => {
-        await getExchanges();
+        if(!exchanges.value.length) await getExchanges();
     })
     
     definePageMeta({
