d1b57992ca413d95c328276997c1b67373c3b307
verona-dev <veronadev@tuta.io>
2026-06-17 11:23:38 +0200

    CR-143 [MARKET] Add exchanges Add getExchanges method

--- app/pages/market/exchanges.vue
+++ app/pages/market/exchanges.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script setup>
+    // MarketStore
+    import { storeToRefs } from 'pinia';
+    import { useMarketStore } from '~/stores/MarketStore.js';
+    const MarketStore = useMarketStore();
+    
+    const { loading } = storeToRefs(MarketStore);
+    const { getExchanges } = MarketStore;
+    
     // SEO
     const title = 'Crypto Exchanges: Ranked by Volume & Trust Score';
     const description = 'Compare crypto exchanges by trading volume, trust score, supported coins, fees, and more. Find the best CEX and DEX platforms for trading Bitcoin, Ethereum, and altcoins.';
+    
+    onMounted(async() => {
+        await getExchanges();
+    })
     
     definePageMeta({
         layout: 'market',
