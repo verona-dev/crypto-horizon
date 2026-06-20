f7a1378b85eee49db8c2eb7957307ddd06bdde7e
verona-dev <veronadev@tuta.io>
2026-06-20 21:25:53 +0200

    CR-143 [MARKET] Add exchanges Add card component

--- app/pages/market/exchanges.vue
+++ app/pages/market/exchanges.vue
@@ -8,21 +8,36 @@
             v-for='exchange in exchanges'
             :key='exchange.id'
         >
-            <p>{{ exchange.id }}</p>
-            <p>{{ exchange.name }}</p>
-            <p>{{ exchange.country }}</p>
-            <p>{{ exchange.year_established }}</p>
-            <p>{{ exchange.description }}</p>
-            <p>{{ exchange.image }}</p>
-            <p>Trade vol: {{ exchange.trade_volume_24h_btc }}</p>
-            <p>Trust score: {{ exchange.trust_score }}</p>
-            <p>Trust score rank: {{ exchange.trust_score_rank }}</p>
-            <p>{{ exchange.url }}</p>
+            <Card>
+                <CardHeader>
+                    <CardTitle>{{ exchange.name }}</CardTitle>
+                    
+                    <CardDescription>{{ exchange.country }}</CardDescription>
+                    
+                    <CardDescription>
+                        <Button variant="link">
+                            Visit
+                        </Button>
+                    </CardDescription>
+                </CardHeader>
+                
+                <CardContent>
+                    <p>{{ exchange.year_established }}</p>
+                    <p>{{ exchange.description }}</p>
+                    <p>{{ exchange.image }}</p>
+                    <p>Trade vol: {{ exchange.trade_volume_24h_btc }}</p>
+                    <p>Trust score: {{ exchange.trust_score }}</p>
+                    <p>Trust score rank: {{ exchange.trust_score_rank }}</p>
+                    <p>{{ exchange.url }}</p>
+                </CardContent>
+            </Card>
         </div>
     </div>
 </template>
 
 <script setup>
+    import { Button } from '~/components/ui/button';
+    import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '~/components/ui/card';
     import LoadingContent from '~/components/LoadingContent.vue';
     
     // MarketStore
