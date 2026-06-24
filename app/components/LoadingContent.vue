91a9fce7440eb8d9da77768b7e0b9d2785bf8c51
verona-dev <veronadev@tuta.io>
2026-06-24 19:57:10 +0200

    CR-144 [DESIGN] LoadingContent - Use animated text component Update loading-content component

--- app/components/LoadingContent.vue
+++ app/components/LoadingContent.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class='relative w-screen h-full flex justify-center items-center'>
-        <GradientText
-            :colors="['#5227FF', '#FF9FFC', '#B19EEF']"
-            :animation-speed="8"
-        >
-            <h1 class='font-great text-4xl lg:text-6xl xl:text-8xl'>{{ site_name }}</h1>
-        </GradientText>
-        
-        <SnowfallBg
-            v-if='darkThemes'
-            color="#e4d9e2"
-            class='absolute inset-0 !min-h-lvh !min-w-full !z-0'
-            :min-radius='0.1'
-            :max-radius='2'
-            :speed='0.2'
-            :quantity='50'
-        />
+    <div class='w-screen'>
+        <div class='h-full flex flex-col justify-center items-center gap-8 pt-16'>
+            <GradientText
+                :colors='["#5227FF", "#FF9FFC", "#B19EEF"]'
+                :animation-speed='8'
+            >
+                <h1 class='font-great text-4xl lg:text-6xl xl:text-8xl'>{{ site_name }}</h1>
+            </GradientText>
+            
+            <Spinner class='size-28 text-[#B19EEF]' />
+        </div>
     </div>
+    
+    <SnowfallBg
+        v-if='darkThemes'
+        color="#e4d9e2"
+        class='absolute inset-0 !min-h-lvh !min-w-full !z-0'
+        :min-radius='0.1'
+        :max-radius='2'
+        :speed='0.2'
+        :quantity='50'
+    />
 </template>
 
 <script setup>
     import GradientText from '~/components/ui/gradient-text/GradientText.vue';
     import { SnowfallBg } from '~/components/ui/snowfall-bg';
+    import { Spinner } from '@/components/ui/spinner/index.js';
     
     const siteConfig = useSiteConfig();
     const site_name = ref(siteConfig?.name || '');
