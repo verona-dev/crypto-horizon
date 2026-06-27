9a635d3fc2f00f49dfb36c5aa80b1da879057a68
verona-dev <veronadev@tuta.io>
2026-06-27 12:00:08 +0200

    CR-89 [MARKET] Add Public Treasury data Adjust public-treasuries table

--- app/pages/market/public-treasuries.vue
+++ app/pages/market/public-treasuries.vue
@@ -2,7 +2,14 @@
     <div class='page'>
         <LoadingContent v-if='loading' />
         
-        <div v-else class='flex flex-wrap gap-16 justify-center'>
+        <Card
+            v-else
+            class='flex flex-wrap gap-16 justify-center w-full h-full !shadow-2x'
+        >
+            <div class='flex flex-col items-center justify-center gap-8 p-14'>
+                <Title :tag='1' :level='3'>Crypto Treasury Holdings</Title>
+            </div>
+            
             <Table class='font-mono'>
                 <TableCaption>A list of Public treasury entities.</TableCaption>
                 <TableHeader>
@@ -12,7 +19,7 @@
                         <TableHead class='text-center'>Country</TableHead>
                         <TableHead>Type</TableHead>
                         <TableHead>Holdings</TableHead>
-                        <TableHead>Current Value</TableHead>
+                        <TableHead>Total Treasury</TableHead>
                         <TableHead>Unrealised PNL</TableHead>
                         
                         <TableHead class='flex items-center gap-1'>
@@ -29,14 +36,14 @@
                         
                         <TableHead class='text-right'>
                             <div class='flex items-center gap-1'>
-                                <span>{{ glossary.total_value_asset.label }}</span>
+                                <span>{{ glossary.total_value_asset_per_share.label }}</span>
                                 
                                 <HoverCard :open-delay='200' class='flex'>
                                     <HoverCardTrigger>
                                         <InfoIcon />
                                     </HoverCardTrigger>
                                     
-                                    <HoverCardContent>{{ glossary.total_value_asset.description }}</HoverCardContent>
+                                    <HoverCardContent>{{ glossary.total_value_asset_per_share.description }}</HoverCardContent>
                                 </HoverCard>
                             </div>
                         </TableHead>
@@ -49,17 +56,22 @@
                         :key='entity.id'
                     >
                         <TableCell class="font-medium">{{ entity.name || '-' }}</TableCell>
+                        
                         <TableCell>{{ entity.symbol || '-' }}</TableCell>
+                        
+                        <!--  Country  -->
                         <TableCell class='text-center'>
                             <span v-if='entity.country === "NO"'>-</span>
                             <span v-else>{{ entity.country }}</span>
                         </TableCell>
                         
+                        <!--  Type  -->
                         <TableCell>
                             <Badge v-if='entity.type === "company"'>{{ entity.type || '-' }}</Badge>
                             <Badge v-else variant='secondary'>{{ entity.type || '-' }}</Badge>
                         </TableCell>
                         
+                        <!--  Holdings  -->
                         <TableCell class='flex items-center gap-2'>
                             <template
                                 v-for='coin in entity.holdings'
@@ -75,20 +87,28 @@
                             </template>
                         </TableCell>
                         
+                        <!--  Total Treasury Value  -->
                         <TableCell>{{ entity.total_treasury_value_usd || '-' }}</TableCell>
+                        
+                        <!--  Unrealised PNL  -->
                         <TableCell>{{ entity.unrealized_pnl || '-' }}</TableCell>
+                        
+                        <!--  nNav  -->
                         <TableCell>{{ entity.m_nav || '-' }}<span v-if='entity.m_nav'>x</span></TableCell>
+                        
+                        <!--  Total asset value per share  -->
                         <TableCell class="text-right">{{ entity.total_asset_value_per_share_usd || '-' }}</TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
-        </div>
+        </Card>
     </div>
 </template>
 
 <script setup>
     import { Badge } from '~/components/ui/badge';
     import { Button } from '~/components/ui/button';
+    import { Card } from '~/components/ui/card';
     import { formatNumber } from '~/utils/formatUtils.js';
     import glossary from '~/assets/data/market/glossary.json';
     import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card/index.ts';
