4c46a0368bfdcae9e8fd16d015d9d98043538bcc
verona-dev <veronadev@tuta.io>
2026-06-30 13:06:45 +0200

    CR-89 [MARKET] Add Public Treasury data Create coin-treasury component

---
+++ app/components/market/id/CoinTreasury.vue
@@ -0,0 +1,17 @@
+<template>
+    <TreasuriesTable v-if='coinTreasury' :data='coinTreasury.companies' class='font-mono w-[97%] mx-auto' />
+</template>
+
+<script setup>
+    import TreasuriesTable from '~/components/market/TreasuriesTable.vue';
+    
+    const props = defineProps({
+        coin: {
+            type: Object,
+            required: true
+        }
+    });
+    
+    const { coin } = toRefs(props);
+    const coinTreasury = computed(() => coin.value?.publicTreasury || null);
+</script>
