afc4594b2d1163f6a2d181baf14337d46b7e6e1c
verona-dev <veronadev@tuta.io>
2026-07-04 13:27:16 +0200

    CR-89 [MARKET] Add Public Treasury data Fix loading store

--- app/stores/LoadingStore.js
+++ app/stores/LoadingStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 
-export const useLoadingStore = () => defineStore('LoadingStore', {
+export const useLoadingStore = defineStore('LoadingStore', {
     state: () => ({
         loading: false,
     }),
