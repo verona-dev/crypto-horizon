d1cdb306f3c132c91ae53f9bd51f517d6ece9c6b
verona-dev <veronadev@tuta.io>
2026-07-05 20:09:04 +0200

    CR-146 [MARKET] Exchanges - create single page Add page title

--- app/pages/market/exchanges.vue
+++ app/pages/market/exchanges.vue
@@ -2,98 +2,102 @@
     <div class='page'>
         <PageLoadingSpinner v-if='loading' />
         
-        <div v-else class='flex flex-wrap gap-16 justify-center'>
-            <div
-                v-for='exchange in exchanges'
-                :key='exchange.id'
-            >
-                <Card class='w-150 min-h-90 p-4 !rounded-xl border-secondary/25 hover:border-foreground/15 flex flex-col justify-between'>
-                    <CardHeader>
-                        <div class='flex justify-between'>
-                            <div class='flex items-center gap-3'>
-                                <NuxtImg
-                                    :src='exchange.image'
-                                    width='54'
-                                    alt='exchange logo'
-                                />
-                                
-                                <!--  Name + Rank + Country  -->
-                                <div class='flex flex-col'>
-                                    <div class='flex items-center gap-3'>
-                                        <Title :tag='4' class='text-2xl xl:text-3xl font-semibold leading-none tracking-tight'>{{ exchange.name }}</Title>
-                                        <p class='text-muted-foreground text-xl mt-0.5'>#{{ exchange.trust_score_rank }}</p>
+        <div v-else class='flex flex-col flex-wrap gap-16 justify-center items-center'>
+            <Title :tag='1' :level='3'>Top Crypto Exchanges Ranked by Trust Score</Title>
+            
+            <div class='flex flex-wrap gap-16 justify-center'>
+                <div
+                    v-for='exchange in exchanges'
+                    :key='exchange.id'
+                >
+                    <Card class='w-150 min-h-90 p-4 !rounded-xl border-secondary/25 hover:border-foreground/15 flex flex-col justify-between'>
+                        <CardHeader>
+                            <div class='flex justify-between'>
+                                <div class='flex items-center gap-3'>
+                                    <NuxtImg
+                                        :src='exchange.image'
+                                        width='54'
+                                        alt='exchange logo'
+                                    />
+                                    
+                                    <!--  Name + Rank + Country  -->
+                                    <div class='flex flex-col'>
+                                        <div class='flex items-center gap-3'>
+                                            <Title :tag='4' class='text-2xl xl:text-3xl font-semibold leading-none tracking-tight'>{{ exchange.name }}</Title>
+                                            <p class='text-muted-foreground text-xl mt-0.5'>#{{ exchange.trust_score_rank }}</p>
+                                        </div>
+                                        <CardDescription class='ml-0.5'>{{ exchange.country }} since {{ exchange.year_established }}</CardDescription>
                                     </div>
-                                    <CardDescription class='ml-0.5'>{{ exchange.country }} since {{ exchange.year_established }}</CardDescription>
                                 </div>
+                                
+                                <!--  Visit  -->
+                                <NuxtLink
+                                    :to='exchange.url'
+                                    target='_blank'
+                                    aria-label='exchange link'
+                                    class='mt-1.5'
+                                >
+                                    <Button variant='link'>
+                                        Visit
+                                        
+                                        <NuxtIcon
+                                            name='ph:arrow-up-right-light'
+                                            size='16'
+                                            class='bg-primary'
+                                        />
+                                    </Button>
+                                </NuxtLink>
+                            </div>
+                        </CardHeader>
+                        
+                        <CardContent class='my-4 flex items-center justify-between'>
+                            <!--  Trade Volume 24h  -->
+                            <div class='flex-1 flex flex-col gap-1.5'>
+                                <CardDescription>Trade Volume 24h in BTC</CardDescription>
+                                
+                                
+                                <Badge variant='outline' class='py-1.5 px-3.5 shadow-lg w-fit' :class='trustScoreStyle(exchange.trust_score)'>
+                                    <div class='flex items-center gap-2'>
+                                        <NuxtIcon
+                                            name='logos:bitcoin'
+                                            class='mb-0.5'
+                                            size='24'
+                                        />
+                                        
+                                        <Title :tag='5' class='font-semibold leading-none tracking-tight'>
+                                            {{
+                                                formatNumber(exchange.trade_volume_24h_btc, {
+                                                    style: 'decimal',
+                                                    maximumFractionDigits: 3
+                                                })
+                                            }}
+                                        </Title>
+                                    </div>
+                                </Badge>
                             </div>
                             
-                            <!--  Visit  -->
-                            <NuxtLink
-                                :to='exchange.url'
-                                target='_blank'
-                                aria-label='exchange link'
-                                class='mt-1.5'
-                            >
-                                <Button variant='link'>
-                                    Visit
-                                    
-                                    <NuxtIcon
-                                        name='ph:arrow-up-right-light'
-                                        size='16'
-                                        class='bg-primary'
-                                    />
-                                </Button>
-                            </NuxtLink>
-                        </div>
-                    </CardHeader>
-                    
-                    <CardContent class='my-4 flex items-center justify-between'>
-                        <!--  Trade Volume 24h  -->
-                        <div class='flex-1 flex flex-col gap-1.5'>
-                            <CardDescription>Trade Volume 24h in BTC</CardDescription>
-                            
-                            
-                            <Badge variant='outline' class='py-1.5 px-3.5 shadow-lg w-fit' :class='trustScoreStyle(exchange.trust_score)'>
-                                <div class='flex items-center gap-2'>
-                                    <NuxtIcon
-                                        name='logos:bitcoin'
-                                        class='mb-0.5'
-                                        size='24'
-                                    />
-                                    
-                                    <Title :tag='5' class='font-semibold leading-none tracking-tight'>
-                                        {{
-                                            formatNumber(exchange.trade_volume_24h_btc, {
-                                                style: 'decimal',
-                                                maximumFractionDigits: 3
-                                            })
-                                        }}
+                            <!--  Trust Score  -->
+                            <div class='flex-1 text-center flex flex-col items-center gap-1.5'>
+                                <CardDescription>Trust Score</CardDescription>
+                                
+                                <Badge variant='outline' class='py-1.5 px-3.5 shadow-lg' :class='trustScoreStyle(exchange.trust_score)'>
+                                    <Title
+                                        :tag='5'
+                                        class='font-semibold leading-none tracking-tight'
+                                        :class='trustScoreStyle(exchange.trust_score)'
+                                    >
+                                        {{ exchange.trust_score }}/10
                                     </Title>
-                                </div>
-                            </Badge>
-                        </div>
+                                </Badge>
+                            </div>
+                        </CardContent>
                         
-                        <!--  Trust Score  -->
-                        <div class='flex-1 text-center flex flex-col items-center gap-1.5'>
-                            <CardDescription>Trust Score</CardDescription>
-                            
-                            <Badge variant='outline' class='py-1.5 px-3.5 shadow-lg' :class='trustScoreStyle(exchange.trust_score)'>
-                                <Title
-                                    :tag='5'
-                                    class='font-semibold leading-none tracking-tight'
-                                    :class='trustScoreStyle(exchange.trust_score)'
-                                >
-                                    {{ exchange.trust_score }}/10
-                                </Title>
-                            </Badge>
-                        </div>
-                    </CardContent>
-                    
-                    <!--  Description  -->
-                    <CardContent>
-                        <p>{{ exchange.description }}</p>
-                    </CardContent>
-                </Card>
+                        <!--  Description  -->
+                        <CardContent>
+                            <p>{{ exchange.description }}</p>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     </div>
