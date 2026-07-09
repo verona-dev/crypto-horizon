1351b97c7b37ac15a4dee8487fb7ccc6ad10cf4a
verona-dev <veronadev@tuta.io>
2026-07-09 14:39:52 +0200

    CR-146 [MARKET] Exchanges - create single page Add card

--- app/pages/market/exchanges/[id]/index.vue
+++ app/pages/market/exchanges/[id]/index.vue
@@ -1,18 +1,54 @@
 <template>
     <div class='page'>
-        this is single exchange page
-        
         <PageLoadingSpinner v-if='loading' />
         
         <template v-else>
-            {{ exchange.id }}
+            <Card>
+                <CardHeader>
+                    <Title :tag='4'>What is {{ exchange?.name }} exchange?</Title>
+                    <CardDescription>{{ exchange?.description }}</CardDescription>
+                </CardHeader>
+                
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+            </Card>
+            
+            <div class='flex flex-col'>
+<!--                <p>{{ exchange?.name }}</p>-->
+                <p>{{ exchange?.year_established }}</p>
+                <p>{{ exchange?.country }}</p>
+<!--                <p>{{ exchange?.description }}</p>-->
+                <p>{{ exchange?.url }}</p>
+                <p>{{ exchange?.image }}</p>
+                
+                <p>{{ exchange?.facebook_url }}</p>
+                <p>{{ exchange?.reddit_url }}</p>
+                <p>{{ exchange?.telegram_url }}</p>
+                <p>{{ exchange?.other_url_1 }}</p>
+                <p>{{ exchange?.other_url_2 }}</p>
+                <p>{{ exchange?.twitter_handle }}</p>
+                
+                <p>{{ exchange?.centralized }}</p>
+                
+                <p>{{ exchange?.public_notice }}</p>
+                <p>{{ exchange?.alert_notice }}</p>
+                
+                <p>{{ exchange?.trust_score }}</p>
+                <p>{{ exchange?.trust_score_rank }}</p>
+                
+                <p>{{ exchange?.coins }}</p>
+                <p>{{ exchange?.pairs }}</p>
+                <p>{{ exchange?.trade_volume_24h_btc }}</p>
+            </div>
         </template>
     </div>
 </template>
 
 <script setup>
-    import { Card  } from '~/components/ui/card';
+    import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
     import PageLoadingSpinner from '@/components/PageLoadingSpinner.vue';
+    import Title from '~/components/Title.vue';
     
     // Router
     const route = useRoute();
