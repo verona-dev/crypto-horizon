d51f1fdd223eddd9893f51a93155b711ae22dfd8
verona-dev <veronadev@tuta.io>
2026-07-14 19:50:29 +0200

    CR-148 [DEFI] Add Defillama Api Create get-defillama-protocols method

--- app/stores/DefiStore.js
+++ app/stores/DefiStore.js
@@ -1,10 +1,12 @@
 import { defineStore } from 'pinia';
 import { useFetchCoingecko } from '~/composables/apiCoingecko';
+import { useFetchDefillama } from '~/composables/apiDefillama';
 import { useLoadingStore } from '@/stores/LoadingStore.js';
 
 export const useDefiStore = defineStore('DefiStore', {
     state: () => ({
         globalDefi: {},
+        protocols: null,
     }),
     
     actions: {
@@ -17,6 +19,24 @@ export const useDefiStore = defineStore('DefiStore', {
                 
                 if(response && response.data) {
                     this.globalDefi = response.data;
+                }
+            } catch(error) {
+                console.error(error);
+            } finally {
+                LoadingStore.setLoading(false);
+            }
+        },
+        
+        async getDefillamaProtocols() {
+            const LoadingStore = useLoadingStore();
+            LoadingStore.setLoading(true);
+            
+            try {
+                const response = await useFetchDefillama('protocols');
+                
+                if(response) {
+                    this.protocols = response;
+                    console.log(JSON.parse(JSON.stringify(this.protocols)));
                 }
             } catch(error) {
                 console.error(error);
