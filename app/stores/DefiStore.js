1b8e545402a2b33a2851f7d2232e00331798fb95
verona-dev <veronadev@tuta.io>
2026-07-14 19:15:15 +0200

    CR-148 [DEFI] Add Defillama Api Create coingecko global-defi method

--- app/stores/DefiStore.js
+++ app/stores/DefiStore.js
@@ -1,9 +1,28 @@
 import { defineStore } from 'pinia';
+import { useFetchCoingecko } from '~/composables/apiCoingecko';
+import { useLoadingStore } from '@/stores/LoadingStore.js';
 
 export const useDefiStore = defineStore('DefiStore', {
     state: () => ({
         globalDefi: {},
     }),
     
-    actions: {},
+    actions: {
+        async getCoingeckoGlobalDefi() {
+            const LoadingStore = useLoadingStore();
+            LoadingStore.setLoading(true);
+            
+            try {
+                const response = await useFetchCoingecko('global/decentralized_finance_defi');
+                
+                if(response && response.data) {
+                    this.globalDefi = response.data;
+                }
+            } catch(error) {
+                console.error(error);
+            } finally {
+                LoadingStore.setLoading(false);
+            }
+        },
+    },
 });
