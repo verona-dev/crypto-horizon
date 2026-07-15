c665cd9ae1eba89c4efc243b205695cbf51e0fdd
verona-dev <veronadev@tuta.io>
2026-07-15 17:14:15 +0200

    CR-151 [DEFI] Add Protocols Adjust protocols page

--- app/pages/defi/protocols.vue
+++ app/pages/defi/protocols.vue
@@ -1,33 +1,27 @@
 <template>
     <div class='page'>
+        <!--
         <PageLoadingSpinner v-if='loading' />
+        -->
         
-        <template v-else>
-            <div>
-<!--                <Title>Defi Protocols</Title>-->
-                
-                <ProtocolsTable :protocols='protocols'/>
-            </div>
-        </template>
+        <ProtocolsTable />
     </div>
 </template>
 
 <script setup>
-    import PageLoadingSpinner from '~/components/PageLoadingSpinner.vue';
+    // import PageLoadingSpinner from '~/components/PageLoadingSpinner.vue';
     import ProtocolsTable from '~/components/defi/ProtocolsTable.vue';
-    import Title from '~/components/Title.vue';
     
     // LoadingStore
     import { storeToRefs } from 'pinia';
-    import { useLoadingStore } from '~/stores/LoadingStore.js';
-    const LoadingStore = useLoadingStore();
-    const { loading } = storeToRefs(LoadingStore);
+    // import { useLoadingStore } from '~/stores/LoadingStore.js';
+    // const LoadingStore = useLoadingStore();
+    // const { loading } = storeToRefs(LoadingStore);
     
     // DefiStore
     import { useDefiStore } from '~/stores/DefiStore.js';
     const DefiStore = useDefiStore();
     const { getDefillamaProtocols } = DefiStore;
-    const { protocols } = storeToRefs(DefiStore);
     
     // SEO
     const title = 'DeFi Platforms: Lending, DEXs & Yield Aggregators';
