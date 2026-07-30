37e81e1916cc49f97522e8c179ef1d3b857ec32b
verona-dev <veronadev@tuta.io>
2026-07-30 19:33:07 +0200

    CR-153 [DEFI] Add single protocol page Add protocol website pixel-card

--- app/pages/defi/protocols/[id]/index.vue
+++ app/pages/defi/protocols/[id]/index.vue
@@ -2,7 +2,7 @@
     <div class='page w-full'>
         <!--        <PageLoadingSpinner v-if='loading' />-->
         
-        <div class='flex items-center border w-full justify-between'>
+        <div class='flex flex-col xl:flex-row items-center w-full justify-evenly'>
             <Card class='w-2/5 p-6 flex flex-col gap-8'>
                 <CardHeader>
                     <div class='flex items-center gap-4 self-start'>
@@ -37,14 +37,47 @@
                     <CardDescription v-if='protocol.description'>{{ protocol.description }}</CardDescription>
                 </CardHeader>
                 
-                <CardContent>
-                    Category:
-                    <Badge
-                        v-if='protocol.category' variant='outline'
-                        class='py-2 px-3 shadow-lg bg-muted text-foreground/75 text-sm'
-                    >
-                        {{ protocol.category }}
-                    </Badge>
+                <CardContent class='flex flex-col'>
+                    <div class='flex items-center gap-2'>
+                        <span>Category:</span>
+                        <Badge
+                            v-if='protocol.category' variant='outline'
+                            class='py-2 px-3 shadow-lg text-sm'
+                        >
+                            {{ protocol.category }}
+                        </Badge>
+                    </div>
+                    
+                    <div>
+                        <!--   Links  -->
+                        <div class='w-full flex gap-8'>
+                            <!--   Website  -->
+                            <template v-if='protocol.url'>
+                                <NuxtLink
+                                    :to='protocol.url'
+                                    target='_blank'
+                                    class='flex-1 h-40'
+                                    aria-label='platform website link'
+                                    external
+                                >
+                                    <PixelCard
+                                        variant='blue'
+                                        class='flex flex-row items-center !h-full !w-full !flex-1 hover:border-blue-sky/50'
+                                    >
+                                        <CardContent class='!w-full !justify-center !flex !flex-col !items-center gap-4 pt-6 ml-1'>
+                                            <NuxtIcon
+                                                name='ph:house-line-fill'
+                                                size='28'
+                                            />
+                                            
+                                            <CardDescription>{{ protocol.url }}</CardDescription>
+                                        </CardContent>
+                                    </PixelCard>
+                                </NuxtLink>
+                            </template>
+                        </div>
+                    </div>
+                    
                 </CardContent>
             </Card>
         </div>
@@ -57,7 +90,12 @@
     import { Card, CardContent, CardDescription, CardHeader, CardFooter } from '~/components/ui/card';
     import { formatNumber } from '~/utils/formatUtils.js';
     import glossary from '~/assets/data/market/glossary.json';
+    import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card';
+    import InfoIcon from '~/components/InfoIcon.vue';
+    import NewTabIcon from '~/components/NewTabIcon.vue';
     import PageLoadingSpinner from '@/components/PageLoadingSpinner.vue';
+    import PixelCard from '~/components/ui/pixel-card/PixelCard.vue';
+    import { Table, TableBody, TableCell, TableRow } from '~/components/ui/table/index.ts';
     import Title from '~/components/Title.vue';
     
     // LoadingStore
