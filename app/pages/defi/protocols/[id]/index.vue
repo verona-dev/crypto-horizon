3d4037622f3ffab6f8daa5f4eb6c10b8d6b367bc
verona-dev <veronadev@tuta.io>
2026-08-06 21:37:09 +0200

    CR-153 [DEFI] Add single protocol page Add hacks table

--- app/pages/defi/protocols/[id]/index.vue
+++ app/pages/defi/protocols/[id]/index.vue
@@ -5,46 +5,142 @@
         <template v-else>
             <div v-if='computed_protocol?.id' class='flex flex-col gap-6'>
                 
-                <!--  Hallmarks Timeline  -->
-                <Card v-if='computed_hallmarks?.length' class='w-full h-fit p-6 flex flex-col gap-6'>
-                    <CardHeader>
-                        <Title :tag='2' :level='4'>{{ glossary.hallmarks.label }}</Title>
-                        <CardDescription>{{ glossary.hallmarks.description }}</CardDescription>
-                    </CardHeader>
-                    
-                    <CardContent>
-                        <Stepper
-                            orientation='vertical'
-                            class='flex max-w-md flex-col gap-10'
-                        >
-                            <StepperItem
-                                v-for='(step, index) in computed_hallmarks'
-                                :key='index'
-                                class='relative flex w-full items-start gap-6'
-                                :step='computed_protocol.hallmarks.length - index'
-                                :state='"completed"'
+                <!--  Hallmarks + Hacks  -->
+                <Card v-if='computed_hallmarks?.length' class='w-full h-fit p-6 flex items-center gap-4'>
+                    <!--  Hallmarks Timeline  -->
+                    <div class='w-1/3 flex flex-col gap-6 border border-secondary'>
+                        <CardHeader>
+                            <Title :tag='2' :level='4'>{{ glossary.hallmarks.label }}</Title>
+                            <CardDescription>{{ glossary.hallmarks.description }}</CardDescription>
+                        </CardHeader>
+                        
+                        <CardContent>
+                            <Stepper
+                                orientation='vertical'
+                                class='flex max-w-md flex-col gap-10'
                             >
-                                <StepperSeparator
-                                    v-if='index !== computed_protocol.hallmarks?.length - 1'
-                                    class='absolute left-[19px] top-[44px] block h-[85%] w-0.5 shrink-0 rounded-full !bg-primary/25'
-                                />
-                                
-                                <div class='z-10 rounded-full shrink-0 flex items-center justify-center size-10 bg-transparent text-primary/75 ring-2 ring-primary/25 ring-offset-2 ring-offset-background'>
-                                    <NuxtIcon name='ph:calendar-blank' size='18' class='text-primary' />
-                                </div>
-                                
-                                <div class='flex flex-col gap-1'>
-                                    <StepperTitle class='text-sm lg:text-base font-semibold transition text-muted-foreground'>
-                                        {{ formatDate(step[0]) }}
-                                    </StepperTitle>
+                                <StepperItem
+                                    v-for='(step, index) in computed_hallmarks'
+                                    :key='index'
+                                    class='relative flex w-full items-start gap-6'
+                                    :step='computed_protocol.hallmarks.length - index'
+                                    :state='"completed"'
+                                >
+                                    <StepperSeparator
+                                        v-if='index !== computed_protocol.hallmarks?.length - 1'
+                                        class='absolute left-[19px] top-[44px] block h-[85%] w-0.5 shrink-0 rounded-full !bg-primary/25'
+                                    />
                                     
-                                    <StepperDescription class='sr-only text-xs lg:text-sm text-foreground transition md:not-sr-only'>
-                                        {{ step[1] }}
-                                    </StepperDescription>
-                                </div>
-                            </StepperItem>
-                        </Stepper>
-                    </CardContent>
+                                    <div
+                                        class='z-10 rounded-full shrink-0 flex items-center justify-center size-10 bg-transparent text-primary/75 ring-2 ring-primary/25 ring-offset-2 ring-offset-background'
+                                    >
+                                        <NuxtIcon name='ph:calendar-blank' size='18' class='text-primary' />
+                                    </div>
+                                    
+                                    <div class='flex flex-col gap-1'>
+                                        <StepperTitle
+                                            class='text-sm lg:text-base font-semibold transition text-muted-foreground'
+                                        >
+                                            {{ formatDate(step[0]) }}
+                                        </StepperTitle>
+                                        
+                                        <StepperDescription
+                                            class='sr-only text-xs lg:text-sm text-foreground transition md:not-sr-only'
+                                        >
+                                            {{ step[1] }}
+                                        </StepperDescription>
+                                    </div>
+                                </StepperItem>
+                            </Stepper>
+                        </CardContent>
+                    </div>
+                    
+                    <!--  Hacks  -->
+                    <div class='w-1/3 flex flex-col gap-6 border border-secondary'>
+                        <CardHeader>
+                            <Title :tag='2' :level='4'>{{ glossary.hacks.label }}</Title>
+                            <CardDescription>{{ glossary.hacks.description }}</CardDescription>
+                        </CardHeader>
+                        
+                        <CardContent>
+                            <Stepper orientation='vertical' class='flex max-w-md flex-col gap-10'>
+                                <StepperItem
+                                    v-for='(hack, index) in protocol.hacks'
+                                    :key='index'
+                                    class='relative flex w-full items-start gap-6'
+                                    :step='protocol.hacks.length - index'
+                                    :state='"completed"'
+                                >
+                                    <StepperSeparator
+                                        v-if='index !== protocol.hacks?.length - 1'
+                                        class='absolute left-[19px] top-[44px] block h-[85%] w-0.5 shrink-0 rounded-full !bg-primary/25'
+                                    />
+                                    
+                                    <div class='z-10 rounded-full shrink-0 flex items-center justify-center size-10 bg-transparent text-primary/75 ring-2 ring-primary/25 ring-offset-2 ring-offset-background'>
+                                        <NuxtIcon name='ph:calendar-blank' size='18' class='text-primary' />
+                                    </div>
+                                    
+                                    <div class='flex flex-col gap-1'>
+                                        <StepperTitle class='text-sm lg:text-base font-semibold transition'>
+                                            {{ hack.name }} Hack
+                                        </StepperTitle>
+                                        
+                                        <StepperDescription class='sr-only text-xs lg:text-sm text-foreground transition md:not-sr-only'>
+                                            <Table class='!w-100 border border-primary'>
+                                                <TableBody class='!mt-6 !w-fit'>
+                                                    <TableRow v-if='hack.date' class='!w-fit'>
+                                                        <TableHead>Date</TableHead>
+                                                        <TableCell class='text-sm !w-fit'>{{ formatDate(hack.date) }}</TableCell>
+                                                    </TableRow>
+                                                    <TableRow v-if='hack.technique' class='!w-fit'>
+                                                        <TableHead>Technique</TableHead>
+                                                        <TableCell class='text-sm !w-fit'>{{ hack.technique }}</TableCell>
+                                                    </TableRow>
+                                                    
+                                                    <TableRow v-if='hack.amount'>
+                                                        <TableHead>Amount</TableHead>
+                                                        <TableCell class='text-sm'>{{ formatNumber(hack.amount) }}</TableCell>
+                                                    </TableRow>
+                                                    
+                                                    <TableRow v-if='hack.returnedFunds'>
+                                                        <TableHead>Returned</TableHead>
+                                                        <TableCell class='text-sm'>{{ formatNumber(hack.returnedFunds) }}</TableCell>
+                                                    </TableRow>
+                                                    
+                                                    <template v-if='hack.chain?.length'>
+                                                        <TableRow>
+                                                            <TableHead>Chain</TableHead>
+                                                            <TableCell class='text-sm'>
+                                                                <template v-for='chain in hack.chain' :key='chain' >
+                                                                    {{ chain }}
+                                                                    <span v-if='hack.chain.length > 1'>,</span>
+                                                                </template>
+                                                            </TableCell>
+                                                        </TableRow>
+                                                    </template>
+                                                    
+                                                    <TableRow v-if='hack.language'>
+                                                        <TableHead>Language</TableHead>
+                                                        <TableCell class='text-sm'>{{ hack.language }}</TableCell>
+                                                    </TableRow>
+                                                    
+                                                    <TableRow>
+                                                        <TableHead class='!w-fit'>Bridge Hacked</TableHead>
+                                                        <TableCell class='text-sm'>{{ hack.bridgeHack ? 'YES' : 'NO' }}</TableCell>
+                                                    </TableRow>
+                                                    
+                                                    <TableRow v-if='hack.source'>
+                                                        <TableHead>Source</TableHead>
+                                                        <TableCell class='text-sm'>{{ hack.source }}</TableCell>
+                                                    </TableRow>
+                                                </TableBody>
+                                            </Table>
+                                        </StepperDescription>
+                                    </div>
+                                </StepperItem>
+                            </Stepper>
+                        </CardContent>
+                    </div>
                 </Card>
                 
                 <div class='flex flex-col 2xl:flex-row w-full gap-6'>
